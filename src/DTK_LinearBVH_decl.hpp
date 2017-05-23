387122f7a5eff322cb46a406601dc49338b0c5bc
Damien L-G <dalg24@gmail.com>
2017-05-23 15:47:43 +0000

    Fix comment in BVH declaration to be picked up by Doxygen

--- src/DTK_LinearBVH_decl.hpp
+++ src/DTK_LinearBVH_decl.hpp
@@ -51,7 +51,7 @@ struct BVH
 
     Kokkos::View<Node *, DeviceType> _leaf_nodes;
     Kokkos::View<Node *, DeviceType> _internal_nodes;
-    /*
+    /**
      * Array of indices that sort the boxes used to construct the hierarchy.
      * The leaf nodes are ordered so we need these to identify objects that
      * meet a predicate.
