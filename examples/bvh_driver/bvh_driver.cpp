2974c4eea09e01ba473b81825a2027b22d144f78
Damien L-G <dalg24@gmail.com>
2018-03-12 21:01:44 +0000

    Cleanup example bvh driver to measure performance

--- examples/bvh_driver/bvh_driver.cpp
+++ examples/bvh_driver/bvh_driver.cpp
@@ -8,58 +8,15 @@
  *                                                                          *
  * SPDX-License-Identifier: BSD-3-Clause                                    *
  ****************************************************************************/
-#include <Teuchos_CommandLineProcessor.hpp>
-#include <Teuchos_StandardCatchMacros.hpp>
-
-#include <Kokkos_DefaultNode.hpp>
-
-#include <DTK_ConfigDefs.hpp>
 
 #include <DTK_LinearBVH.hpp>
 
-#include <cmath>
-#include <functional>
+#include <Kokkos_DefaultNode.hpp>
+#include <Teuchos_CommandLineProcessor.hpp>
+#include <Teuchos_StandardCatchMacros.hpp>
+
+#include <chrono>
 #include <random>
-
-namespace details = DataTransferKit::Details;
-
-std::vector<std::array<double, 3>>
-make_stuctured_cloud( double Lx, double Ly, double Lz, int nx, int ny, int nz )
-{
-    std::vector<std::array<double, 3>> cloud( nx * ny * nz );
-    std::function<int( int, int, int )> ind = [nx, ny]( int i, int j, int k ) {
-        return i + j * nx + k * ( nx * ny );
-    };
-    double x, y, z;
-    for ( int i = 0; i < nx; ++i )
-        for ( int j = 0; j < ny; ++j )
-            for ( int k = 0; k < nz; ++k )
-            {
-                x = i * Lx / ( nx - 1 );
-                y = j * Ly / ( ny - 1 );
-                z = k * Lz / ( nz - 1 );
-                cloud[ind( i, j, k )] = {{x, y, z}};
-            }
-    return cloud;
-}
-
-std::vector<std::array<double, 3>> make_random_cloud( double Lx, double Ly,
-                                                      double Lz, int n )
-{
-    std::vector<std::array<double, 3>> cloud( n );
-    std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution_x( 0.0, Lx );
-    std::uniform_real_distribution<double> distribution_y( 0.0, Ly );
-    std::uniform_real_distribution<double> distribution_z( 0.0, Lz );
-    for ( int i = 0; i < n; ++i )
-    {
-        double x = distribution_x( generator );
-        double y = distribution_y( generator );
-        double z = distribution_z( generator );
-        cloud[i] = {{x, y, z}};
-    }
-    return cloud;
-}
 
 template <class NO>
 int main_( Teuchos::CommandLineProcessor &clp, int argc, char *argv[] )
@@ -67,21 +24,11 @@ int main_( Teuchos::CommandLineProcessor &clp, int argc, char *argv[] )
     using DeviceType = typename NO::device_type;
     using ExecutionSpace = typename DeviceType::execution_space;
 
-    double Lx = 100.0;
-    double Ly = 100.0;
-    double Lz = 100.0;
-    int nx = 11;
-    int ny = 11;
-    int nz = 11;
-    int n_points = 100;
-    std::string mode = "radius";
+    int n_values = 50000;
+    int n_queries = 20000;
 
-    clp.setOption( "nx", &nx, "source mesh points in x-direction." );
-    clp.setOption( "ny", &ny, "source mesh points in y-direction." );
-    clp.setOption( "nz", &nz, "source mesh points in z-direction." );
-    clp.setOption( "N", &n_points,
-                   "number of target mesh points (distributed randomly)." );
-    clp.setOption( "mode", &mode, "mode: (knn | radius)" );
+    clp.setOption( "values", &n_values, "number of indexable values (source)" );
+    clp.setOption( "queries", &n_queries, "number of queries (target)" );
 
     clp.recogniseAllOptions( true );
     switch ( clp.parse( argc, argv ) )
@@ -95,110 +42,86 @@ int main_( Teuchos::CommandLineProcessor &clp, int argc, char *argv[] )
         break;
     }
 
-    // contruct a cloud of points (nodes of a structured grid)
-    auto cloud = make_stuctured_cloud( Lx, Ly, Lz, nx, ny, nz );
-    int n = cloud.size();
+    Kokkos::View<DataTransferKit::Point *, DeviceType> random_points(
+        "random_points" );
+    {
+        auto n = std::max( n_values, n_queries );
+        Kokkos::resize( random_points, n );
+
+        auto random_points_host = Kokkos::create_mirror_view( random_points );
+
+        std::uniform_real_distribution<double> distribution( 0., n );
+        std::default_random_engine generator;
+        auto random = [&distribution, &generator]() {
+            return distribution( generator );
+        };
+        for ( int i = 0; i < n; ++i )
+            random_points_host( i ) = {{random(), random(), random()}};
+        Kokkos::deep_copy( random_points, random_points_host );
+    }
 
     Kokkos::View<DataTransferKit::Box *, DeviceType> bounding_boxes(
-        "bounding_boxes", n );
-    auto bounding_boxes_host = Kokkos::create_mirror_view( bounding_boxes );
-    // build bounding volume hierarchy
-    for ( int i = 0; i < n; ++i )
-    {
-        auto const &point = cloud[i];
-        double x = std::get<0>( point );
-        double y = std::get<1>( point );
-        double z = std::get<2>( point );
-        bounding_boxes_host[i] = {{{x, y, z}}, {{x, y, z}}};
-    }
-    Kokkos::deep_copy( bounding_boxes, bounding_boxes_host );
+        "bounding_boxes", n_values );
+    Kokkos::parallel_for( Kokkos::RangePolicy<ExecutionSpace>( 0, n_values ),
+                          KOKKOS_LAMBDA( int i ) {
+                              double const x = random_points( i )[0];
+                              double const y = random_points( i )[1];
+                              double const z = random_points( i )[2];
+                              bounding_boxes( i ) = {
+                                  {{x - .5, y - .5, z - .5}},
+                                  {{x + .5, y + .5, z + .5}}};
+                          } );
+    Kokkos::fence();
 
+    std::ostream &os = std::cout;
+
+    auto start = std::chrono::high_resolution_clock::now();
     DataTransferKit::BVH<DeviceType> bvh( bounding_boxes );
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    os << "construction " << elapsed_seconds.count() << "\n";
 
-    // random points for radius search and kNN queries
-    auto queries = make_random_cloud( Lx, Ly, Lz, n_points );
-    Kokkos::View<double * [3], ExecutionSpace> point_coords( "point_coords",
-                                                             n_points );
-
-    auto point_coords_host = Kokkos::create_mirror_view( point_coords );
-    for ( int i = 0; i < n_points; ++i )
     {
-        auto const &point = queries[i];
-        point_coords_host( i, 0 ) = std::get<0>( point );
-        point_coords_host( i, 1 ) = std::get<1>( point );
-        point_coords_host( i, 2 ) = std::get<2>( point );
-    }
-    Kokkos::deep_copy( point_coords, point_coords_host );
-
-    std::default_random_engine generator;
-
-    if ( mode == "knn" )
-    {
-        Kokkos::View<int *, ExecutionSpace> k( "distribution_k", n_points );
-        auto k_host = Kokkos::create_mirror_view( k );
-
-        // use random number k of for the kNN search
-        int max_k = std::floor( sqrt( nx * nx + ny * ny + nz * nz ) );
-        std::uniform_int_distribution<int> distribution_k( 1, max_k );
-        for ( int i = 0; i < n_points; ++i )
-        {
-            k_host[i] = distribution_k( generator );
-        }
-        Kokkos::deep_copy( k, k_host );
-
-        Kokkos::View<details::Nearest<DataTransferKit::Point> *, DeviceType>
-            nearest_queries( "nearest_queries", n_points );
+        Kokkos::View<
+            DataTransferKit::Details::Nearest<DataTransferKit::Point> *,
+            DeviceType>
+            queries( "queries", n_queries );
         Kokkos::parallel_for(
-            DTK_MARK_REGION( "register_nearest_queries" ),
-            Kokkos::RangePolicy<ExecutionSpace>( 0, n_points ),
+            Kokkos::RangePolicy<ExecutionSpace>( 0, n_queries ),
             KOKKOS_LAMBDA( int i ) {
-                nearest_queries( i ) = details::nearest<DataTransferKit::Point>(
-                    {{point_coords( i, 0 ), point_coords( i, 1 ),
-                      point_coords( i, 2 )}},
-                    k( i ) );
+                queries( i ) =
+                    DataTransferKit::Details::nearest<DataTransferKit::Point>(
+                        random_points( i ), 10 );
             } );
         Kokkos::fence();
 
-        // do the search
-        Kokkos::View<int *, DeviceType> offset_nearest( "offset_nearest" );
-        Kokkos::View<int *, DeviceType> indices_nearest( "indices_nearest" );
-        bvh.query( nearest_queries, indices_nearest, offset_nearest );
+        Kokkos::View<int *, DeviceType> offset( "offset" );
+        Kokkos::View<int *, DeviceType> indices( "indices" );
+        start = std::chrono::high_resolution_clock::now();
+        bvh.query( queries, indices, offset );
+        end = std::chrono::high_resolution_clock::now();
+        elapsed_seconds = end - start;
+        os << "knn " << elapsed_seconds.count() << "\n";
     }
-    else if ( mode == "radius" )
+
     {
-        Kokkos::View<double *, ExecutionSpace> radii( "radii", n_points );
-        auto radii_host = Kokkos::create_mirror_view( radii );
-
-        // use random radius for the search
-        // set the limit of approximately 100 points by
-        // solving n_points*pi*r^2/(Lx^2 + Ly^2 + Lz^2) <= 100
-        const int approx_points = 100;
-        double max_radius = sqrt(
-            approx_points * ( Lx * Lx + Ly * Ly + Lz * Lz ) / ( n * M_PI ) );
-        std::uniform_real_distribution<double> distribution_radius(
-            0.0, max_radius );
-        for ( int i = 0; i < n_points; ++i )
-        {
-            radii_host[i] = distribution_radius( generator );
-        }
-        Kokkos::deep_copy( radii, radii_host );
-
-        Kokkos::View<details::Within *, DeviceType> within_queries(
-            "within_queries", n_points );
+        Kokkos::View<DataTransferKit::Details::Within *, DeviceType> queries(
+            "queries", n_queries );
         Kokkos::parallel_for(
-            DTK_MARK_REGION( "register_within_queries" ),
-            Kokkos::RangePolicy<ExecutionSpace>( 0, n_points ),
+            Kokkos::RangePolicy<ExecutionSpace>( 0, n_queries ),
             KOKKOS_LAMBDA( int i ) {
-                within_queries( i ) = details::within(
-                    {{point_coords( i, 0 ), point_coords( i, 1 ),
-                      point_coords( i, 2 )}},
-                    radii( i ) );
+                queries( i ) =
+                    DataTransferKit::Details::within( random_points( i ), 5 );
             } );
         Kokkos::fence();
 
-        Kokkos::View<int *, DeviceType> offset_within( "offset_within" );
-        Kokkos::View<int *, DeviceType> indices_within( "indices_within" );
-        bvh.query( within_queries, indices_within, offset_within );
+        Kokkos::View<int *, DeviceType> offset( "offset" );
+        Kokkos::View<int *, DeviceType> indices( "indices" );
+        start = std::chrono::high_resolution_clock::now();
+        bvh.query( queries, indices, offset );
+        end = std::chrono::high_resolution_clock::now();
+        elapsed_seconds = end - start;
+        os << "radius " << elapsed_seconds.count() << "\n";
     }
 
     return 0;
