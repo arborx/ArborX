27dabadee2643178b09240ebe4ec1bf246f265a5
Damien L-G <dalg24@gmail.com>
2018-05-12 15:36:56 +0000

    KOKKOS_HAVE -> KOKKOS_ENABLE in Search package

--- examples/bvh_driver/bvh_driver.cpp
+++ examples/bvh_driver/bvh_driver.cpp
@@ -198,7 +198,7 @@ int main( int argc, char *argv[] )
         }
         else if ( node == "serial" )
         {
-#ifdef KOKKOS_HAVE_SERIAL
+#ifdef KOKKOS_ENABLE_SERIAL
             typedef Kokkos::Compat::KokkosSerialWrapperNode Node;
             main_<Node>( clp, argc, argv );
 #else
@@ -207,7 +207,7 @@ int main( int argc, char *argv[] )
         }
         else if ( node == "openmp" )
         {
-#ifdef KOKKOS_HAVE_OPENMP
+#ifdef KOKKOS_ENABLE_OPENMP
             typedef Kokkos::Compat::KokkosOpenMPWrapperNode Node;
             main_<Node>( clp, argc, argv );
 #else
@@ -216,7 +216,7 @@ int main( int argc, char *argv[] )
         }
         else if ( node == "cuda" )
         {
-#ifdef KOKKOS_HAVE_CUDA
+#ifdef KOKKOS_ENABLE_CUDA
             typedef Kokkos::Compat::KokkosCudaWrapperNode Node;
             main_<Node>( clp, argc, argv );
 #else
