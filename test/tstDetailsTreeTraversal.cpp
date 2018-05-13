3f3d45c9983fbedac639649530f86891f2c0780f
Damien L-G <dalg24@gmail.com>
2018-05-13 05:41:25 +0000

    Add tests to inspect the heap as elements are pushed/popped

--- test/tstDetailsTreeTraversal.cpp
+++ test/tstDetailsTreeTraversal.cpp
@@ -58,3 +58,60 @@ TEUCHOS_UNIT_TEST( LinearBVH, priority_queue )
     queue.pop();
     TEST_ASSERT( queue.empty() );
 }
+
+TEUCHOS_UNIT_TEST( LinearBVH, push_heap )
+{
+    // Here checking against the example of binary heap insertion from
+    // https://en.wikipedia.org/wiki/Binary_heap#Insert
+    DataTransferKit::Details::PriorityQueue<int> queue;
+    auto heap = reinterpret_cast<int *>( &queue );
+
+    queue.push( 11 );
+    queue.push( 5 );
+    queue.push( 8 );
+    queue.push( 3 );
+    queue.push( 4 );
+    TEST_EQUALITY( queue.top(), 11 );
+    TEST_EQUALITY( heap[0], 11 );
+    TEST_EQUALITY( heap[1], 5 );
+    TEST_EQUALITY( heap[2], 8 );
+    TEST_EQUALITY( heap[3], 3 );
+    TEST_EQUALITY( heap[4], 4 );
+
+    queue.push( 15 );
+
+    TEST_EQUALITY( queue.top(), 15 );
+    TEST_EQUALITY( heap[0], 15 );
+    TEST_EQUALITY( heap[1], 5 );
+    TEST_EQUALITY( heap[2], 11 );
+    TEST_EQUALITY( heap[3], 3 );
+    TEST_EQUALITY( heap[4], 4 );
+    TEST_EQUALITY( heap[5], 8 );
+}
+
+TEUCHOS_UNIT_TEST( LinearBVH, pop_heap )
+{
+    // See https://en.wikipedia.org/wiki/Binary_heap#Extract
+    DataTransferKit::Details::PriorityQueue<int> queue;
+    auto heap = reinterpret_cast<int *>( &queue );
+
+    queue.push( 11 );
+    queue.push( 5 );
+    queue.push( 8 );
+    queue.push( 3 );
+    queue.push( 4 );
+    TEST_EQUALITY( queue.top(), 11 );
+    TEST_EQUALITY( heap[0], 11 );
+    TEST_EQUALITY( heap[1], 5 );
+    TEST_EQUALITY( heap[2], 8 );
+    TEST_EQUALITY( heap[3], 3 );
+    TEST_EQUALITY( heap[4], 4 );
+
+    queue.pop();
+
+    TEST_EQUALITY( queue.top(), 8 );
+    TEST_EQUALITY( heap[0], 8 );
+    TEST_EQUALITY( heap[1], 5 );
+    TEST_EQUALITY( heap[2], 4 );
+    TEST_EQUALITY( heap[3], 3 );
+}
