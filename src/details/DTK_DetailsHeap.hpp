e37ebd3cf70901c2c263d819437bf8f9255cf4e9
Damien L-G <dalg24@gmail.com>
2018-08-30 21:27:03 +0000

    Add pushHeap() and popHeap() to get ready to move heap operations out of PriorityQueue

---
+++ src/details/DTK_DetailsHeap.hpp
@@ -0,0 +1,98 @@
+/****************************************************************************
+ * Copyright (c) 2012-2018 by the DataTransferKit authors                   *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the DataTransferKit library. DataTransferKit is     *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+#ifndef DTK_DETAILS_HEAP_HPP
+#define DTK_DETAILS_HEAP_HPP
+
+#include <Kokkos_Macros.hpp>
+
+#include <iterator> // iterator_traits
+#include <utility>  // move
+
+namespace DataTransferKit
+{
+namespace Details
+{
+
+template <typename RandomIterator, typename DistanceType, typename ValueType,
+          typename Compare>
+KOKKOS_INLINE_FUNCTION void __bubbleUp( RandomIterator first, DistanceType pos,
+                                        DistanceType top, ValueType val,
+                                        Compare comp )
+{
+    DistanceType parent = ( pos - 1 ) / 2;
+    while ( pos > top && comp( *( first + parent ), val ) )
+    {
+        *( first + pos ) = std::move( *( first + parent ) );
+        pos = parent;
+        parent = ( pos - 1 ) / 2;
+    }
+    *( first + pos ) = std::move( val );
+}
+
+template <typename RandomIterator, typename Compare>
+KOKKOS_INLINE_FUNCTION void pushHeap( RandomIterator first, RandomIterator last,
+                                      Compare comp )
+{
+    using DistanceType =
+        typename std::iterator_traits<RandomIterator>::difference_type;
+    using ValueType = typename std::iterator_traits<RandomIterator>::value_type;
+    if ( last - first > 1 )
+    {
+        ValueType value = std::move( *( last - 1 ) );
+        __bubbleUp( first, DistanceType( ( last - first ) - 1 ),
+                    DistanceType( 0 ), std::move( value ), comp );
+    }
+}
+
+template <typename RandomIterator, typename DistanceType, typename ValueType,
+          typename Compare>
+KOKKOS_INLINE_FUNCTION void __bubbleDown( RandomIterator first,
+                                          DistanceType pos, DistanceType len,
+                                          ValueType val, Compare comp )
+{
+    DistanceType child = 2 * pos + 1;
+    // if right child exists and compares greater than left child
+    if ( child + 1 < len && comp( *( first + child ), *( first + child + 1 ) ) )
+        ++child;
+
+    while ( child < len && comp( val, *( first + child ) ) )
+    {
+        *( first + pos ) = std::move( *( first + child ) );
+        pos = child;
+        child = 2 * pos + 1;
+        if ( child + 1 < len &&
+             comp( *( first + child ), *( first + child + 1 ) ) )
+            ++child;
+    }
+    *( first + pos ) = std::move( val );
+}
+
+template <typename RandomIterator, typename Compare>
+KOKKOS_INLINE_FUNCTION void popHeap( RandomIterator first, RandomIterator last,
+                                     Compare comp )
+{
+    using DistanceType =
+        typename std::iterator_traits<RandomIterator>::difference_type;
+    using ValueType = typename std::iterator_traits<RandomIterator>::value_type;
+    if ( last - first > 1 )
+    {
+        ValueType value = std::move( *first );
+        __bubbleDown( first, DistanceType( 0 ),
+                      DistanceType( ( last - first ) - 1 ),
+                      std::move( *( last - 1 ) ), comp );
+        *( last - 1 ) = std::move( value );
+    }
+}
+
+} // namespace Details
+} // namespace DataTransferKit
+
+#endif
--- test/tstDetailsTreeTraversal.cpp
+++ test/tstDetailsTreeTraversal.cpp
@@ -15,6 +15,8 @@
 
 #include <Teuchos_UnitTestHarness.hpp>
 
+#include <Kokkos_Array.hpp>
+
 #include <random>
 
 namespace dtk = DataTransferKit::Details;
@@ -215,48 +217,32 @@ void check_heap( PriorityQueue const &queue,
         TEST_EQUALITY( heap[i], heap_ref[i] );
 }
 
-TEUCHOS_UNIT_TEST( LinearBVH, push_heap )
+TEUCHOS_UNIT_TEST( HeapOperations, push_heap )
 {
     // Here checking against the example of binary heap insertion from
     // https://en.wikipedia.org/wiki/Binary_heap#Insert
-    DataTransferKit::Details::PriorityQueue<int> queue;
-
-    queue.push( 11 );
-    queue.push( 5 );
-    queue.push( 8 );
-    queue.push( 3 );
-    queue.push( 4 );
-
-    TEST_EQUALITY( queue.top(), 11 );
-    check_heap( queue, {11, 5, 8, 3, 4}, success, out );
-
-    queue.push( 15 );
-
-    TEST_EQUALITY( queue.top(), 15 );
-    check_heap( queue, {15, 5, 11, 3, 4, 8}, success, out );
+    Kokkos::Array<int, 6> a = {11, 5, 8, 3, 4, 15};
+    TEST_ASSERT( dtk::isHeap( a.data(), a.data() + 5, dtk::Less<int>() ) );
+    TEST_ASSERT( !dtk::isHeap( a.data(), a.data() + 6, dtk::Less<int>() ) );
+    dtk::pushHeap( a.data(), a.data() + 6, dtk::Less<int>() );
+    TEST_ASSERT( dtk::isHeap( a.data(), a.data() + 6, dtk::Less<int>() ) );
+    Kokkos::Array<int, 6> ref = {15, 5, 11, 3, 4, 8};
+    TEST_COMPARE_ARRAYS( a, ref );
 }
 
-TEUCHOS_UNIT_TEST( LinearBVH, pop_heap )
+TEUCHOS_UNIT_TEST( HeapOperations, pop_heap )
 {
     // See https://en.wikipedia.org/wiki/Binary_heap#Extract
-    DataTransferKit::Details::PriorityQueue<int> queue;
-
-    queue.push( 11 );
-    queue.push( 5 );
-    queue.push( 8 );
-    queue.push( 3 );
-    queue.push( 4 );
-
-    TEST_EQUALITY( queue.top(), 11 );
-    check_heap( queue, {11, 5, 8, 3, 4}, success, out );
-
-    queue.pop();
-
-    TEST_EQUALITY( queue.top(), 8 );
-    check_heap( queue, {8, 5, 4, 3}, success, out );
+    Kokkos::Array<int, 5> a = {11, 5, 8, 3, 4};
+    TEST_ASSERT( dtk::isHeap( a.data(), a.data() + 5, dtk::Less<int>() ) );
+    dtk::popHeap( a.data(), a.data() + 5, dtk::Less<int>() );
+    TEST_ASSERT( dtk::isHeap( a.data(), a.data() + 4, dtk::Less<int>() ) );
+    TEST_ASSERT( !dtk::isHeap( a.data(), a.data() + 5, dtk::Less<int>() ) );
+    Kokkos::Array<int, 5> ref = {8, 5, 4, 3, 11};
+    TEST_COMPARE_ARRAYS( a, ref );
 }
 
-TEUCHOS_UNIT_TEST( LinearBVH, heap )
+TEUCHOS_UNIT_TEST( HeapOperations, max_heap )
 {
     // Here attempting to reproduce examples code from cppreference.com for
     // std::push_heap() and std::pop_heap().  It turns out that calling
@@ -267,37 +253,51 @@ TEUCHOS_UNIT_TEST( LinearBVH, heap )
     // on implementation.
     // Nevertheless, I thought it helps understanding how the algorithm works so
     // I decided to keep it as a unit test.
-    DataTransferKit::Details::PriorityQueue<int> queue;
+    Kokkos::Array<int, 7> a = {3, 1, 4, 1, 5, 9, 6};
+    Kokkos::Array<int, 7> ref;
 
-    queue.push( 3 );
-    check_heap( queue, {3}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 0, dtk::Less<int>() );
+    ref = {3, 1, 4, 1, 5, 9, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 1 );
-    check_heap( queue, {3, 1}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 1, dtk::Less<int>() );
+    ref = {3, 1, 4, 1, 5, 9, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 4 );
-    check_heap( queue, {4, 1, 3}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 2, dtk::Less<int>() );
+    ref = {3, 1, 4, 1, 5, 9, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 1 );
-    check_heap( queue, {4, 1, 3, 1}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 3, dtk::Less<int>() );
+    ref = {4, 1, 3, 1, 5, 9, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 5 );
-    check_heap( queue, {5, 4, 3, 1, 1}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 4, dtk::Less<int>() );
+    ref = {4, 1, 3, 1, 5, 9, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 9 );
-    check_heap( queue, {9, 4, 5, 1, 1, 3}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 5, dtk::Less<int>() );
+    ref = {5, 4, 3, 1, 1, 9, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.pop();
-    check_heap( queue, {5, 4, 3, 1, 1}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 6, dtk::Less<int>() );
+    ref = {9, 4, 5, 1, 1, 3, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 9 );
-    check_heap( queue, {9, 4, 5, 1, 1, 3}, success, out );
+    dtk::popHeap( a.data(), a.data() + 6, dtk::Less<int>() );
+    ref = {5, 4, 3, 1, 1, 9, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 6 );
-    check_heap( queue, {9, 4, 6, 1, 1, 3, 5}, success, out );
+    dtk::pushHeap( a.data(), a.data() + 6, dtk::Less<int>() );
+    ref = {9, 4, 5, 1, 1, 3, 6};
+    TEST_COMPARE_ARRAYS( a, ref );
+
+    dtk::pushHeap( a.data(), a.data() + 7, dtk::Less<int>() );
+    ref = {9, 4, 6, 1, 1, 3, 5};
+    TEST_COMPARE_ARRAYS( a, ref );
 }
 
-TEUCHOS_UNIT_TEST( LinearBVH, min_heap )
+TEUCHOS_UNIT_TEST( HeapOperations, min_heap )
 {
     // Here reproducing the example of a binary min heap from Wikipedia and then
     // popping all elements one by one.
@@ -305,63 +305,54 @@ TEUCHOS_UNIT_TEST( LinearBVH, min_heap )
     // exposed by other unit tests in this file, partly because they were too
     // trivial.  The bug was only showing with real kNN search problems when
     // comparing results from BoundaryVolumeHierarchy with boost::rtree.
-    struct Greater
+    Kokkos::Array<int, 9> a = {1, 2, 3, 17, 19, 36, 7, 25, 100};
+    Kokkos::Array<int, 9> ref;
+    for ( int i = 0; i < 9; ++i )
     {
-      public:
-        KOKKOS_FUNCTION bool operator()( int x, int y ) const { return x > y; }
-    };
-    DataTransferKit::Details::PriorityQueue<int, Greater> queue;
+        dtk::pushHeap( a.data(), a.data() + i, dtk::Greater<int>() );
+        ref = {1, 2, 3, 17, 19, 36, 7, 25, 100};
+        TEST_COMPARE_ARRAYS( a, ref );
+    }
 
-    queue.push( 1 );
-    check_heap( queue, {1}, success, out );
+    dtk::popHeap( a.data(), a.data() + 9, dtk::Greater<int>() );
+    ref = {2, 17, 3, 25, 19, 36, 7, 100, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 2 );
-    check_heap( queue, {1, 2}, success, out );
+    dtk::popHeap( a.data(), a.data() + 8, dtk::Greater<int>() );
+    ref = {3, 17, 7, 25, 19, 36, 100, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 3 );
-    check_heap( queue, {1, 2, 3}, success, out );
+    dtk::popHeap( a.data(), a.data() + 7, dtk::Greater<int>() );
+    ref = {7, 17, 36, 25, 19, 100, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 17 );
-    check_heap( queue, {1, 2, 3, 17}, success, out );
+    dtk::popHeap( a.data(), a.data() + 6, dtk::Greater<int>() );
+    ref = {17, 19, 36, 25, 100, 7, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 19 );
-    check_heap( queue, {1, 2, 3, 17, 19}, success, out );
+    dtk::popHeap( a.data(), a.data() + 5, dtk::Greater<int>() );
+    ref = {19, 25, 36, 100, 17, 7, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 36 );
-    check_heap( queue, {1, 2, 3, 17, 19, 36}, success, out );
+    dtk::popHeap( a.data(), a.data() + 4, dtk::Greater<int>() );
+    ref = {25, 100, 36, 19, 17, 7, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 7 );
-    check_heap( queue, {1, 2, 3, 17, 19, 36, 7}, success, out );
+    dtk::popHeap( a.data(), a.data() + 3, dtk::Greater<int>() );
+    ref = {36, 100, 25, 19, 17, 7, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 25 );
-    check_heap( queue, {1, 2, 3, 17, 19, 36, 7, 25}, success, out );
+    dtk::popHeap( a.data(), a.data() + 2, dtk::Greater<int>() );
+    ref = {100, 36, 25, 19, 17, 7, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.push( 100 );
-    check_heap( queue, {1, 2, 3, 17, 19, 36, 7, 25, 100}, success, out );
+    dtk::popHeap( a.data(), a.data() + 1, dtk::Greater<int>() );
+    ref = {100, 36, 25, 19, 17, 7, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 
-    queue.pop();
-    check_heap( queue, {2, 17, 3, 25, 19, 36, 7, 100}, success, out );
-
-    queue.pop();
-    check_heap( queue, {3, 17, 7, 25, 19, 36, 100}, success, out );
-
-    queue.pop();
-    check_heap( queue, {7, 17, 36, 25, 19, 100}, success, out );
-
-    queue.pop();
-    check_heap( queue, {17, 19, 36, 25, 100}, success, out );
-
-    queue.pop();
-    check_heap( queue, {19, 25, 36, 100}, success, out );
-
-    queue.pop();
-    check_heap( queue, {25, 100, 36}, success, out );
-
-    queue.pop();
-    check_heap( queue, {36, 100}, success, out );
-
-    queue.pop();
-    check_heap( queue, {100}, success, out );
+    dtk::popHeap( a.data(), a.data() + 0, dtk::Greater<int>() );
+    ref = {100, 36, 25, 19, 17, 7, 3, 2, 1};
+    TEST_COMPARE_ARRAYS( a, ref );
 }
 
 TEUCHOS_UNIT_TEST( LinearBVH, pop_push )
