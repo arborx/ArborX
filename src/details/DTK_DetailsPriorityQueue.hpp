f62469d506aa730208555bf27891077594abc913
Damien L-G <dalg24@gmail.com>
2018-08-31 02:49:22 +0000

    Template PriorityQueue on container type and call external heap operations

--- src/details/DTK_DetailsPriorityQueue.hpp
+++ src/details/DTK_DetailsPriorityQueue.hpp
@@ -11,11 +11,14 @@
 #ifndef DTK_DETAILS_PRIORITY_QUEUE_HPP
 #define DTK_DETAILS_PRIORITY_QUEUE_HPP
 
+#include <DTK_DetailsContainers.hpp>
+#include <DTK_DetailsHeap.hpp>
+
 #include <Kokkos_Macros.hpp>
 
-#include <cassert>
-#include <cstdlib>
-#include <utility>
+#include <cstddef>     // ptrdiff_t
+#include <type_traits> // is_same
+#include <utility>     // move, forward
 
 namespace DataTransferKit
 {
@@ -26,120 +29,102 @@ template <typename T>
 struct Less
 {
   public:
-    KOKKOS_INLINE_FUNCTION bool operator()( T const &x, T const &y ) const
+    KOKKOS_INLINE_FUNCTION bool operator()( T const &lhs, T const &rhs ) const
     {
-        return x < y;
+        return lhs < rhs;
     }
 };
 
-template <typename T, typename Compare = Less<T>>
+template <typename T>
+struct Greater
+{
+  public:
+    KOKKOS_INLINE_FUNCTION bool operator()( T const &lhs, T const &rhs ) const
+    {
+        return lhs > rhs;
+    }
+};
+
+template <typename T, typename Compare = Less<T>,
+          typename Container = Vector<T, 256>>
 class PriorityQueue
 {
   public:
-    using IndexType = std::ptrdiff_t;
-    using ValueType = T;
-    using ValueCompare = Compare;
+    using container_type = Container;
+    using value_compare = Compare;
+    using value_type = typename Container::value_type;
+    using size_type = typename Container::size_type;
+    using reference = typename Container::reference;
+    using const_reference = typename Container::const_reference;
+    static_assert( std::is_same<value_type, T>::value,
+                   "Template parameter T in PriorityQueue is not the same as "
+                   "the type of the elements stored by the underlying "
+                   "container Container::value_type" );
 
     KOKKOS_FUNCTION PriorityQueue() = default;
 
-    KOKKOS_INLINE_FUNCTION bool empty() const { return _size == 0; }
-
-    KOKKOS_INLINE_FUNCTION IndexType size() const { return _size; }
-
-    KOKKOS_INLINE_FUNCTION void clear() { _size = 0; }
-
-    KOKKOS_INLINE_FUNCTION ValueCompare value_comp() const { return _compare; }
-
-    template <typename... Args>
-    KOKKOS_INLINE_FUNCTION void push( Args &&... args )
+    KOKKOS_FUNCTION PriorityQueue( Container const &c )
+        : _c( c )
     {
-        // ensure the heap is not already full
-        assert( _size < _max_size );
-
-        // add the element to the bottom level of the heap
-        IndexType pos = _size;
-        T elem{std::forward<Args>( args )...};
-
-        // perform up-heap operation
-        while ( pos > 0 )
-        {
-            // compare the added element with its parent
-            // if they are in correct order, stop
-            // if not, swap them and continue
-            IndexType const parent = ( pos - 1 ) / 2;
-            if ( !_compare( _heap[parent], elem ) )
-                break;
-            _heap[pos] = _heap[parent];
-            pos = parent;
-        }
-
-        _heap[pos] = elem;
-
-        // update the size of the heap
-        ++_size;
+        assert( _c.empty() ||
+                isHeap( _c.data(), _c.data() + _c.size(), _compare ) );
     }
 
+    // Capacity
+    KOKKOS_INLINE_FUNCTION bool empty() const { return _c.empty(); }
+    KOKKOS_INLINE_FUNCTION size_type size() const { return _c.size(); }
+
+    // Element access
+    KOKKOS_INLINE_FUNCTION reference top() { return _c.front(); }
+    KOKKOS_INLINE_FUNCTION const_reference top() const { return _c.front(); }
+
+    // Modifiers
+    KOKKOS_INLINE_FUNCTION void push( value_type const &value )
+    {
+        _c.pushBack( value );
+        pushHeap( _c.data(), _c.data() + _c.size(), _compare );
+    }
+    KOKKOS_INLINE_FUNCTION void push( value_type &&value )
+    {
+        _c.pushBack( std::move( value ) );
+        pushHeap( _c.data(), _c.data() + _c.size(), _compare );
+    }
+    template <class... Args>
+    KOKKOS_INLINE_FUNCTION void emplace( Args &&... args )
+    {
+        _c.emplaceBack( std::forward<Args>( args )... );
+        pushHeap( _c.data(), _c.data() + _c.size(), _compare );
+    }
     KOKKOS_INLINE_FUNCTION void pop()
     {
-        // ensure that the heap is not empty
-        assert( _size > 0 );
-
-        // replace the root with the last element on the last level
-        IndexType pos = 0;
-
-        // perform down-heap operation
-        while ( true )
-        {
-            // compare the new root with its children
-            // if they are in the correct order, stop
-            // if not, swap the element with one of its children and continue
-            IndexType const left_child = 2 * pos + 1;
-            IndexType const right_child = 2 * pos + 2;
-            IndexType next_pos = _size - 1;
-            for ( IndexType child : {left_child, right_child} )
-                if ( child < _size - 1 &&
-                     _compare( _heap[next_pos], _heap[child] ) )
-                    next_pos = child;
-            if ( next_pos == _size - 1 )
-                break;
-            _heap[pos] = _heap[next_pos];
-            pos = next_pos;
-        }
-
-        _heap[pos] = _heap[_size - 1];
-
-        // update the size of the heap
-        _size--;
+        popHeap( _c.data(), _c.data() + _c.size(), _compare );
+        _c.popBack();
     }
-
     // in TreeTraversal::nearestQuery, pop() is often followed by push which is
     // an opportunity for doing a single bubble-down operation instead of paying
     // for both one bubble-down and one bubble-up
     template <typename... Args>
-    KOKKOS_INLINE_FUNCTION void pop_push( Args &&... args )
+    KOKKOS_INLINE_FUNCTION void popPush( Args &&... args )
     {
-        assert( _size < _max_size );
-
-        // size will be decremented by pop()
-        _size++;
-
-        // put the new element at the bottom level
-        _heap[_size - 1] = T{std::forward<Args>( args )...};
-
-        // replace the root with that new element and bubble it down
-        pop();
+        assert( _c.size() > 0 );
+        __bubbleDown( _c.data(), std::ptrdiff_t( 0 ),
+                      std::ptrdiff_t( _c.size() ),
+                      T{std::forward<Args>( args )...}, _compare );
     }
 
-    KOKKOS_INLINE_FUNCTION T const &top() const
+    // Accessors that shouldn't be there but that are convenient in
+    // TreeTraversal::nearestQuery()
+    KOKKOS_INLINE_FUNCTION typename Container::pointer data()
     {
-        assert( _size > 0 );
-        return _heap[0];
+        return _c.data();
+    }
+    KOKKOS_INLINE_FUNCTION value_compare const &valueComp() const
+    {
+        return _compare;
     }
 
   private:
-    static IndexType constexpr _max_size = 256;
-    T _heap[_max_size];
-    IndexType _size = 0;
+    Container _c;
     Compare _compare;
 };
 
