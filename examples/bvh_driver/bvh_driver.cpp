fcae3c4b496ff8529bb1492695accc64092b6acb
Damien L-G <dalg24@gmail.com>
2018-09-04 20:44:42 +0000

    Turn bvh driver into benchmark

--- examples/bvh_driver/bvh_driver.cpp
+++ examples/bvh_driver/bvh_driver.cpp
@@ -13,79 +13,34 @@
 
 #include <Kokkos_DefaultNode.hpp>
 #include <Teuchos_CommandLineProcessor.hpp>
-#include <Teuchos_StandardCatchMacros.hpp>
+
+#include <benchmark/benchmark.h>
 
 #include <chrono>
 #include <cmath> // cbrt
 #include <random>
 
-template <class NO>
-int main_( Teuchos::CommandLineProcessor &clp, int argc, char *argv[] )
+template <typename DeviceType>
+Kokkos::View<DataTransferKit::Box *, DeviceType> constructBoxes( int n_values )
 {
-    using DeviceType = typename NO::device_type;
-    using ExecutionSpace = typename DeviceType::execution_space;
-
-    int n_values = 50000;
-    int n_queries = 20000;
-    int n_neighbors = 10;
-    int buffer_size = 0;
-    bool perform_knn_search = true;
-    bool perform_radius_search = true;
-
-    clp.setOption( "values", &n_values, "number of indexable values (source)" );
-    clp.setOption( "queries", &n_queries, "number of queries (target)" );
-    clp.setOption( "neighbors", &n_neighbors,
-                   "desired number of results per query" );
-    clp.setOption( "buffer", &buffer_size,
-                   "size for buffer optimization in radius search" );
-    clp.setOption( "perform-knn-search", "do-not-perform-knn-search",
-                   &perform_knn_search,
-                   "whether or not to perform kNN search" );
-    clp.setOption( "perform-radius-search", "do-not-perform-radius-search",
-                   &perform_radius_search,
-                   "whether or not to perform radius search" );
-
-    clp.recogniseAllOptions( true );
-    switch ( clp.parse( argc, argv ) )
-    {
-    case Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED:
-        return EXIT_SUCCESS;
-    case Teuchos::CommandLineProcessor::PARSE_ERROR:
-    case Teuchos::CommandLineProcessor::PARSE_UNRECOGNIZED_OPTION:
-        return EXIT_FAILURE;
-    case Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL:
-        break;
-    }
-
     Kokkos::View<DataTransferKit::Point *, DeviceType> random_points(
-        "random_points" );
-    {
-        // Random points are "reused" between building the tree and performing
-        // queries.  You may change it if you have a problem with it.  These
-        // don't really need to be stored in the 1st place.  What is needed is
-        // indexable objects/values (here boxes) to build a tree and queries
-        // (here kNN and radius searches) with mean to control the amount of
-        // work per query as the problem size varies.
-        auto n = std::max( n_values, n_queries );
-        Kokkos::resize( random_points, n );
+        Kokkos::ViewAllocateWithoutInitializing( "random_points" ), n_values );
+    auto random_points_host = Kokkos::create_mirror_view( random_points );
+    // Generate random points uniformely distributed within a box.  The edge
+    // length of the box chosen such that object density (here objects will be
+    // boxes 2x2x2 centered around a random point) will remain constant as
+    // problem size is changed.
+    auto const a = std::cbrt( n_values );
+    std::uniform_real_distribution<double> distribution( -a, +a );
+    std::default_random_engine generator;
+    auto random = [&distribution, &generator]() {
+        return distribution( generator );
+    };
+    for ( int i = 0; i < n_values; ++i )
+        random_points_host( i ) = {{random(), random(), random()}};
+    Kokkos::deep_copy( random_points, random_points_host );
 
-        auto random_points_host = Kokkos::create_mirror_view( random_points );
-
-        // Generate random points uniformely distributed within a box.  The
-        // edge length of the box chosen such that object density (here objects
-        // will be boxes 2x2x2 centered around a random point) will remain
-        // constant as problem size is changed.
-        auto const a = std::cbrt( n_values );
-        std::uniform_real_distribution<double> distribution( -a, +a );
-        std::default_random_engine generator;
-        auto random = [&distribution, &generator]() {
-            return distribution( generator );
-        };
-        for ( int i = 0; i < n; ++i )
-            random_points_host( i ) = {{random(), random(), random()}};
-        Kokkos::deep_copy( random_points, random_points_host );
-    }
-
+    using ExecutionSpace = typename DeviceType::execution_space;
     Kokkos::View<DataTransferKit::Box *, DeviceType> bounding_boxes(
         Kokkos::ViewAllocateWithoutInitializing( "bounding_boxes" ), n_values );
     Kokkos::parallel_for( "bvh_driver:construct_bounding_boxes",
@@ -99,145 +54,229 @@ int main_( Teuchos::CommandLineProcessor &clp, int argc, char *argv[] )
                                   {{x + 1., y + 1., z + 1.}}};
                           } );
     Kokkos::fence();
-
-    std::ostream &os = std::cout;
-
-    auto start = std::chrono::high_resolution_clock::now();
-    DataTransferKit::BVH<DeviceType> bvh( bounding_boxes );
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    os << "construction " << elapsed_seconds.count() << "\n";
-
-    if ( perform_knn_search )
-    {
-        Kokkos::View<DataTransferKit::Nearest<DataTransferKit::Point> *,
-                     DeviceType>
-            queries( Kokkos::ViewAllocateWithoutInitializing( "queries" ),
-                     n_queries );
-        Kokkos::parallel_for(
-            "bvh_driver:setup_knn_search_queries",
-            Kokkos::RangePolicy<ExecutionSpace>( 0, n_queries ),
-            KOKKOS_LAMBDA( int i ) {
-                queries( i ) = DataTransferKit::nearest<DataTransferKit::Point>(
-                    random_points( i ), n_neighbors );
-            } );
-        Kokkos::fence();
-
-        Kokkos::View<int *, DeviceType> offset( "offset" );
-        Kokkos::View<int *, DeviceType> indices( "indices" );
-
-        start = std::chrono::high_resolution_clock::now();
-        bvh.query( queries, indices, offset );
-        end = std::chrono::high_resolution_clock::now();
-
-        elapsed_seconds = end - start;
-        os << "knn " << elapsed_seconds.count() << "\n";
-    }
-
-    if ( perform_radius_search )
-    {
-        Kokkos::View<DataTransferKit::Within *, DeviceType> queries(
-            Kokkos::ViewAllocateWithoutInitializing( "queries" ), n_queries );
-        // radius chosen in order to control the number of results per query
-        // NOTE: minus "1+sqrt(3)/2 \approx 1.37" matches the size of the boxes
-        // inserted into the tree (mid-point between half-edge and
-        // half-diagonal)
-        double const r = 2. * std::cbrt( static_cast<double>( n_neighbors ) *
-                                         3. / ( 4. * M_PI ) ) -
-                         ( 1. + std::sqrt( 3. ) ) / 2.;
-        Kokkos::parallel_for(
-            "bvh_driver:setup_radius_search_queries",
-            Kokkos::RangePolicy<ExecutionSpace>( 0, n_queries ),
-            KOKKOS_LAMBDA( int i ) {
-                queries( i ) = DataTransferKit::within( random_points( i ), r );
-            } );
-        Kokkos::fence();
-
-        Kokkos::View<int *, DeviceType> offset( "offset" );
-        Kokkos::View<int *, DeviceType> indices( "indices" );
-
-        start = std::chrono::high_resolution_clock::now();
-        bvh.query( queries, indices, offset, buffer_size );
-        end = std::chrono::high_resolution_clock::now();
-
-        elapsed_seconds = end - start;
-        os << "radius " << elapsed_seconds.count() << "\n";
-    }
-
-    return 0;
+    return bounding_boxes;
 }
+
+template <typename DeviceType>
+Kokkos::View<DataTransferKit::Nearest<DataTransferKit::Point> *, DeviceType>
+makeNearestQueries( int n_values, int n_queries, int n_neighbors )
+{
+    Kokkos::View<DataTransferKit::Point *, DeviceType> random_points(
+        Kokkos::ViewAllocateWithoutInitializing( "random_points" ), n_queries );
+    auto random_points_host = Kokkos::create_mirror_view( random_points );
+    auto const a = std::cbrt( n_values );
+    std::uniform_real_distribution<double> distribution( -a, +a );
+    std::default_random_engine generator;
+    auto random = [&distribution, &generator]() {
+        return distribution( generator );
+    };
+    for ( int i = 0; i < n_queries; ++i )
+        random_points_host( i ) = {{random(), random(), random()}};
+    Kokkos::deep_copy( random_points, random_points_host );
+
+    Kokkos::View<DataTransferKit::Nearest<DataTransferKit::Point> *, DeviceType>
+        queries( Kokkos::ViewAllocateWithoutInitializing( "queries" ),
+                 n_queries );
+    using ExecutionSpace = typename DeviceType::execution_space;
+    Kokkos::parallel_for(
+        "bvh_driver:setup_knn_search_queries",
+        Kokkos::RangePolicy<ExecutionSpace>( 0, n_queries ),
+        KOKKOS_LAMBDA( int i ) {
+            queries( i ) = DataTransferKit::nearest<DataTransferKit::Point>(
+                random_points( i ), n_neighbors );
+        } );
+    Kokkos::fence();
+    return queries;
+}
+
+template <typename DeviceType>
+Kokkos::View<DataTransferKit::Within *, DeviceType>
+makeSpatialQueries( int n_values, int n_queries, int n_neighbors )
+{
+    Kokkos::View<DataTransferKit::Point *, DeviceType> random_points(
+        Kokkos::ViewAllocateWithoutInitializing( "random_points" ), n_queries );
+    auto random_points_host = Kokkos::create_mirror_view( random_points );
+    auto const a = std::cbrt( n_values );
+    std::uniform_real_distribution<double> distribution( -a, +a );
+    std::default_random_engine generator;
+    auto random = [&distribution, &generator]() {
+        return distribution( generator );
+    };
+    for ( int i = 0; i < n_queries; ++i )
+        random_points_host( i ) = {{random(), random(), random()}};
+    Kokkos::deep_copy( random_points, random_points_host );
+
+    Kokkos::View<DataTransferKit::Within *, DeviceType> queries(
+        Kokkos::ViewAllocateWithoutInitializing( "queries" ), n_queries );
+    // radius chosen in order to control the number of results per query
+    // NOTE: minus "1+sqrt(3)/2 \approx 1.37" matches the size of the boxes
+    // inserted into the tree (mid-point between half-edge and half-diagonal)
+    double const r = 2. * std::cbrt( static_cast<double>( n_neighbors ) * 3. /
+                                     ( 4. * M_PI ) ) -
+                     ( 1. + std::sqrt( 3. ) ) / 2.;
+    using ExecutionSpace = typename DeviceType::execution_space;
+    Kokkos::parallel_for( "bvh_driver:setup_radius_search_queries",
+                          Kokkos::RangePolicy<ExecutionSpace>( 0, n_queries ),
+                          KOKKOS_LAMBDA( int i ) {
+                              queries( i ) = DataTransferKit::within(
+                                  random_points( i ), r );
+                          } );
+    Kokkos::fence();
+    return queries;
+}
+
+template <class DeviceType>
+void BM_construction( benchmark::State &state )
+{
+    int const n_values = state.range( 0 );
+    auto bounding_boxes = constructBoxes<DeviceType>( n_values );
+
+    for ( auto _ : state )
+    {
+        auto const start = std::chrono::high_resolution_clock::now();
+        DataTransferKit::BVH<DeviceType> bvh( bounding_boxes );
+        auto const end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        state.SetIterationTime( elapsed_seconds.count() );
+    }
+}
+
+template <class DeviceType>
+void BM_knn_search( benchmark::State &state )
+{
+    int const n_values = state.range( 0 );
+    int const n_queries = state.range( 1 );
+    int const n_neighbors = state.range( 2 );
+
+    DataTransferKit::BVH<DeviceType> bvh(
+        constructBoxes<DeviceType>( n_values ) );
+    auto const queries =
+        makeNearestQueries<DeviceType>( n_values, n_queries, n_neighbors );
+
+    for ( auto _ : state )
+    {
+        Kokkos::View<int *, DeviceType> offset( "offset" );
+        Kokkos::View<int *, DeviceType> indices( "indices" );
+        auto const start = std::chrono::high_resolution_clock::now();
+        bvh.query( queries, indices, offset );
+        auto const end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        state.SetIterationTime( elapsed_seconds.count() );
+    }
+}
+
+template <class DeviceType>
+void BM_radius_search( benchmark::State &state )
+{
+    int const n_values = state.range( 0 );
+    int const n_queries = state.range( 1 );
+    int const n_neighbors = state.range( 2 );
+    int const buffer_size = state.range( 3 );
+
+    DataTransferKit::BVH<DeviceType> bvh(
+        constructBoxes<DeviceType>( n_values ) );
+    auto const queries =
+        makeSpatialQueries<DeviceType>( n_values, n_queries, n_neighbors );
+
+    for ( auto _ : state )
+    {
+        Kokkos::View<int *, DeviceType> offset( "offset" );
+        Kokkos::View<int *, DeviceType> indices( "indices" );
+        auto const start = std::chrono::high_resolution_clock::now();
+        bvh.query( queries, indices, offset, buffer_size );
+        auto const end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        state.SetIterationTime( elapsed_seconds.count() );
+    }
+}
+
+class KokkosScopeGuard
+{
+  public:
+    KokkosScopeGuard( int &argc, char *argv[] )
+    {
+        Kokkos::initialize( argc, argv );
+    }
+    ~KokkosScopeGuard() { Kokkos::finalize(); }
+};
+
+#define REGISTER_BENCHMARK( DeviceType )                                       \
+    BENCHMARK_TEMPLATE( BM_construction, DeviceType )                          \
+        ->Arg( n_values )                                                      \
+        ->UseManualTime()                                                      \
+        ->Unit( benchmark::kMicrosecond );                                     \
+    BENCHMARK_TEMPLATE( BM_knn_search, DeviceType )                            \
+        ->Args( {n_values, n_queries, n_neighbors} )                           \
+        ->UseManualTime()                                                      \
+        ->Unit( benchmark::kMicrosecond );                                     \
+    BENCHMARK_TEMPLATE( BM_radius_search, DeviceType )                         \
+        ->Args( {n_values, n_queries, n_neighbors, buffer_size} )              \
+        ->UseManualTime()                                                      \
+        ->Unit( benchmark::kMicrosecond );
 
 int main( int argc, char *argv[] )
 {
-    Kokkos::initialize( argc, argv );
+    KokkosScopeGuard guard( argc, argv );
 
-    bool success = true;
-    bool verbose = true;
+    bool const throw_exceptions = false;
+    bool const recognise_all_options = false;
+    Teuchos::CommandLineProcessor clp( throw_exceptions,
+                                       recognise_all_options );
+    int n_values = 50000;
+    int n_queries = 20000;
+    int n_neighbors = 10;
+    int buffer_size = 0;
+    clp.setOption( "values", &n_values, "number of indexable values (source)" );
+    clp.setOption( "queries", &n_queries, "number of queries (target)" );
+    clp.setOption( "neighbors", &n_neighbors,
+                   "desired number of results per query" );
+    clp.setOption( "buffer", &buffer_size,
+                   "size for buffer optimization in radius search" );
 
-    try
+    switch ( clp.parse( argc, argv, NULL ) )
     {
-        const bool throwExceptions = false;
-
-        Teuchos::CommandLineProcessor clp( throwExceptions );
-
-        std::string node = "";
-        clp.setOption( "node", &node, "node type (serial | openmp | cuda)" );
-
-        clp.recogniseAllOptions( false );
-        switch ( clp.parse( argc, argv, NULL ) )
-        {
-        case Teuchos::CommandLineProcessor::PARSE_ERROR:
-            success = false;
-        case Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED:
-        case Teuchos::CommandLineProcessor::PARSE_UNRECOGNIZED_OPTION:
-        case Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL:
-            break;
-        }
-
-        if ( !success )
-        {
-            // do nothing, just skip other if clauses
-        }
-        else if ( node == "" )
-        {
-            typedef KokkosClassic::DefaultNode::DefaultNodeType Node;
-            main_<Node>( clp, argc, argv );
-        }
-        else if ( node == "serial" )
-        {
-#ifdef KOKKOS_ENABLE_SERIAL
-            typedef Kokkos::Compat::KokkosSerialWrapperNode Node;
-            main_<Node>( clp, argc, argv );
-#else
-            throw std::runtime_error( "Serial node type is disabled" );
-#endif
-        }
-        else if ( node == "openmp" )
-        {
-#ifdef KOKKOS_ENABLE_OPENMP
-            typedef Kokkos::Compat::KokkosOpenMPWrapperNode Node;
-            main_<Node>( clp, argc, argv );
-#else
-            throw std::runtime_error( "OpenMP node type is disabled" );
-#endif
-        }
-        else if ( node == "cuda" )
-        {
-#ifdef KOKKOS_ENABLE_CUDA
-            typedef Kokkos::Compat::KokkosCudaWrapperNode Node;
-            main_<Node>( clp, argc, argv );
-#else
-            throw std::runtime_error( "CUDA node type is disabled" );
-#endif
-        }
-        else
-        {
-            throw std::runtime_error( "Unrecognized node type" );
-        }
+    case Teuchos::CommandLineProcessor::PARSE_ERROR:
+        return EXIT_FAILURE;
+    case Teuchos::CommandLineProcessor::PARSE_UNRECOGNIZED_OPTION:
+    case Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED:
+        clp.printHelpMessage( "benchmark", std::cout );
+    case Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL:
+        break;
     }
-    TEUCHOS_STANDARD_CATCH_STATEMENTS( verbose, std::cerr, success );
 
-    Kokkos::finalize();
+    // FIXME benchmark::Initialize() calls exit(0) when `--help` and
+    // Kokkos::finalize() is never called
+    benchmark::Initialize( &argc, argv );
 
-    return ( success ? EXIT_SUCCESS : EXIT_FAILURE );
+    // Throw if an option is not recognised
+    clp.throwExceptions( true );
+    clp.recogniseAllOptions( true );
+    switch ( clp.parse( argc, argv, NULL ) )
+    {
+    case Teuchos::CommandLineProcessor::PARSE_UNRECOGNIZED_OPTION:
+    case Teuchos::CommandLineProcessor::PARSE_ERROR:
+        return EXIT_FAILURE;
+    case Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED:
+    case Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL:
+        break;
+    }
+
+#ifdef KOKKOS_ENABLE_SERIAL
+    using Serial = Kokkos::Compat::KokkosSerialWrapperNode::device_type;
+    REGISTER_BENCHMARK( Serial );
+#endif
+
+#ifdef KOKKOS_ENABLE_OPENMP
+    using OpenMP = Kokkos::Compat::KokkosOpenMPWrapperNode::device_type;
+    REGISTER_BENCHMARK( OpenMP );
+#endif
+
+#ifdef KOKKOS_ENABLE_CUDA
+    using Cuda = Kokkos::Compat::KokkosCudaWrapperNode::device_type;
+    REGISTER_BENCHMARK( Cuda );
+#endif
+
+    benchmark::RunSpecifiedBenchmarks();
+
+    return EXIT_SUCCESS;
 }
