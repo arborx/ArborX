3d846c45a315f4dae32bd6c42b038815f971d118
Andrey Prokopenko <prokopenkoav@ornl.gov>
2019-04-09 12:02:11 -0400

    Updating license headers and adding LICENSE file

--- test/tstDetailsDistributedSearchTreeImpl.cpp
+++ test/tstDetailsDistributedSearchTreeImpl.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
- * Copyright (c) 2012-2019 by the DataTransferKit authors                   *
+ * Copyright (c) 2012-2019 by the ArborX authors                            *
  * All rights reserved.                                                     *
  *                                                                          *
- * This file is part of the DataTransferKit library. DataTransferKit is     *
+ * This file is part of the ArborX library. ArborX is                       *
  * distributed under a BSD 3-clause license. For the licensing terms see    *
  * the LICENSE file in the top-level directory.                             *
  *                                                                          *
