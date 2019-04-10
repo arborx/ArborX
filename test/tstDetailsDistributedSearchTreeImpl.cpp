0606a81806db73891a58f801548f54b6f0a5603f
Damien L-G <dalg24@gmail.com>
2019-04-10 02:46:18 +0000

    Template create_layout_right_mirror_view test case on DeviceType

--- test/tstDetailsDistributedSearchTreeImpl.cpp
+++ test/tstDetailsDistributedSearchTreeImpl.cpp
@@ -162,7 +162,8 @@ inline void checkNewViewWasAllocated( View1 const &v1, View2 const &v2 )
     BOOST_TEST( v1.dimension_7() == v2.dimension_7() );
 }
 
-BOOST_AUTO_TEST_CASE( create_layout_right_mirror_view )
+BOOST_AUTO_TEST_CASE_TEMPLATE( create_layout_right_mirror_view, DeviceType,
+                               ARBORX_DEVICE_TYPES )
 {
     using ArborX::Details::create_layout_right_mirror_view;
     using Kokkos::ALL;
@@ -172,18 +173,22 @@ BOOST_AUTO_TEST_CASE( create_layout_right_mirror_view )
     using Kokkos::subview;
     using Kokkos::View;
 
+    if ( !Kokkos::Impl::SpaceAccessibility<
+             Kokkos::HostSpace, typename DeviceType::memory_space>::accessible )
+        return;
+
     // rank-1 and not strided -> do not allocate
-    View<int *, LayoutLeft> u( "u", 255 );
+    View<int *, LayoutLeft, DeviceType> u( "u", 255 );
     auto u_h = create_layout_right_mirror_view( u );
     checkViewWasNotAllocated( u, u_h );
 
     // right layout -> do not allocate
-    View<int **, LayoutRight> v( "v", 2, 3 );
+    View<int **, LayoutRight, DeviceType> v( "v", 2, 3 );
     auto v_h = create_layout_right_mirror_view( v );
     checkViewWasNotAllocated( v, v_h );
 
     // left layout and rank > 1 -> allocate
-    View<int **, LayoutLeft> w( "w", 4, 5 );
+    View<int **, LayoutLeft, DeviceType> w( "w", 4, 5 );
     auto w_h = create_layout_right_mirror_view( w );
     checkNewViewWasAllocated( w, w_h );
 
