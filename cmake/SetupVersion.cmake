6f519b635e11f231daaecaf3e596c396b7ee3d9d
Daniel Arndt <arndtd@ornl.gov>
2019-06-12 08:55:03 -0400

    Directly configure ArborX_Version.hpp

--- cmake/SetupVersion.cmake
+++ cmake/SetupVersion.cmake
@@ -1,15 +1,29 @@
+##/****************************************************************************
+## * Copyright (c) 2012-2019 by the ArborX authors                            *
+## * All rights reserved.                                                     *
+## *                                                                          *
+## * This file is part of the ArborX library. ArborX is                       *
+## * distributed under a BSD 3-clause license. For the licensing terms see    *
+## * the LICENSE file in the top-level directory.                             *
+## *                                                                          *
+## * SPDX-License-Identifier: BSD-3-Clause                                    *
+## ****************************************************************************/
+
+# This CMake script makes sure to store the current git hash in
+# ArborX_Version.hpp each time we recomplile. It is important that this script
+# is called by through a target created by add_custom_target so that it is
+# always considered to be out-of-date.
+
 IF(EXISTS ${SOURCE_DIR}/.git)
-    FIND_PACKAGE(Git)
-    IF(GIT_FOUND)
-        EXECUTE_PROCESS(
-            COMMAND           ${GIT_EXECUTABLE} log --pretty=format:%h -n 1
-            OUTPUT_VARIABLE   ARBORX_GIT_COMMIT_HASH
-            )
+  FIND_PACKAGE(Git QUIET)
+  IF(GIT_FOUND)
+    EXECUTE_PROCESS(
+      COMMAND          ${GIT_EXECUTABLE} log --pretty=format:%h -n 1
+      OUTPUT_VARIABLE  ARBORX_GIT_COMMIT_HASH)
     ENDIF()
 ENDIF()
 MESSAGE("ArborX hash = '${ARBORX_GIT_COMMIT_HASH}'")
-MESSAGE(STATUS "ARBORX_ENABLE_MPI: ${ARBORX_ENABLE_MPI}")
 
-configure_file(${SOURCE_DIR}/src/ArborX_Config.hpp.in
-               ${BINARY_DIR}/include/ArborX_Config.hpp)
+configure_file(${SOURCE_DIR}/src/ArborX_Version.hpp.in
+               ${BINARY_DIR}/include/ArborX_Version.hpp)
 
