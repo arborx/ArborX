d2beb5c474366c8142645451eed01242fe9376ec
Daniel Arndt <arndtd@ornl.gov>
2019-08-29 14:29:52 -0400

    Fix compiling with clang+CUDA

---
+++ test/BoostTest_CUDA_clang_workarounds.hpp
@@ -0,0 +1,11 @@
+#ifndef BOOSTTEST_CUDA_CLANG_WORKAROUNDS_HPP
+#define BOOSTTEST_CUDA_CLANG_WORKAROUNDS_HPP
+
+#include <boost/type_traits/add_const.hpp>
+#include <boost/type_traits/add_pointer.hpp>
+
+#ifndef BOOST_STATIC_ASSERT
+#define BOOST_STATIC_ASSERT(m) static_assert(m, "");
+#endif
+
+#endif
