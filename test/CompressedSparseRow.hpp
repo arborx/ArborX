e18249dc435eba92202b74fb6bab705cb3ba6f31
Daniel Arndt <arndtd@ornl.gov>
2019-10-08 15:02:34 -0400

    Fix compiling with XL (tests)

--- test/CompressedSparseRow.hpp
+++ test/CompressedSparseRow.hpp
@@ -14,6 +14,45 @@
 
 #include "VectorOfTuples.hpp"
 
+namespace std_cxx14
+{
+template <size_t... Ints>
+struct index_sequence
+{
+  using type = index_sequence;
+  using value_type = size_t;
+  static constexpr std::size_t size() noexcept { return sizeof...(Ints); }
+};
+
+namespace internal
+{
+template <class Sequence1, class Sequence2>
+struct merge_and_renumber;
+
+template <size_t... I1, size_t... I2>
+struct merge_and_renumber<index_sequence<I1...>, index_sequence<I2...>>
+    : index_sequence<I1..., (sizeof...(I1) + I2)...>
+{
+};
+} // namespace internal
+
+template <std::size_t N>
+struct make_index_sequence : internal::merge_and_renumber<
+                                 typename make_index_sequence<N / 2>::type,
+                                 typename make_index_sequence<N - N / 2>::type>
+{
+};
+
+template <>
+struct make_index_sequence<0> : index_sequence<>
+{
+};
+template <>
+struct make_index_sequence<1> : index_sequence<0>
+{
+};
+} // namespace std_cxx14
+
 namespace ext
 {
 
@@ -22,15 +61,16 @@ namespace ext
 // std::index_sequence type with Offset, Offset+1, Offset+2, Offset+Count-1 as
 // Ints.
 template <std::size_t Offset, std::size_t... Ints>
-constexpr std::index_sequence<(Offset + Ints)...>
-add_offset(std::index_sequence<Ints...>)
+constexpr std_cxx14::index_sequence<(Offset + Ints)...>
+add_offset(std_cxx14::index_sequence<Ints...>)
 {
   return {};
 }
 
 template <std::size_t Offset, std::size_t Count>
 struct make_index_sequence_with_offset
-    : public decltype(add_offset<Offset>(std::make_index_sequence<Count>{}))
+    : public decltype(
+          add_offset<Offset>(std_cxx14::make_index_sequence<Count>{}))
 {
 };
 
@@ -40,7 +80,7 @@ struct make_index_sequence_with_offset
 // The size is assumed to be the same for all these arrays. The return type here
 // is std::size_t.
 template <typename Tuple, std::size_t... Is>
-auto getSizeOfArraysInTuple(Tuple const &t, std::index_sequence<Is...>)
+auto getSizeOfArraysInTuple(Tuple const &t, std_cxx14::index_sequence<Is...>)
 {
   return Details::getSizeOfArrays(std::get<Is>(t)...);
 }
@@ -82,7 +122,7 @@ auto getNumberOfRows(TupleOfArrays const &t)
 // a std::vector of std::tuples and sorts this vector.
 template <typename TupleOfArrays, std::size_t... Is>
 auto subsetAndSort(TupleOfArrays const &t, std::size_t first, std::size_t last,
-                   std::index_sequence<Is...>)
+                   std_cxx14::index_sequence<Is...>)
 {
   auto out = subsetToVectorOfTuples(first, last, std::get<Is>(t)...);
   std::sort(std::begin(out), std::end(out));
