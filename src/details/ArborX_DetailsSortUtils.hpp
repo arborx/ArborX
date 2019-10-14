28d1f22bad8c59e1d2addb9aff36c93b415c4492
Andrey Prokopenko <prokopenkoav@ornl.gov>
2019-10-14 15:05:26 -0400

    Workaround for Clang+CUDA

--- src/details/ArborX_DetailsSortUtils.hpp
+++ src/details/ArborX_DetailsSortUtils.hpp
@@ -20,9 +20,32 @@
 #include <Kokkos_View.hpp>
 
 #if defined(KOKKOS_ENABLE_CUDA)
+#if (KOKKOS_COMPILER_CLANG < 900)
+// Clang of version less than 9.0 cannot compile Thrust, failing with errors
+// like this:
+//    <snip>/thrust/system/cuda/detail/core/agent_launcher.h:557:11:
+//    error: use of undeclared identifier 'va_printf'
+// Defining _CubLog here allows us to avoid that code path, however disabling
+// some debugging diagnostics
+//
+// If _CubLog is already defined, we save it into ARBORX_CubLog_save, and
+// restore it at the end
+#ifdef _CubLog
+#define ARBORX_CubLog_save _CubLog
+#endif
+#define _CubLog
 #include <thrust/device_ptr.h>
 #include <thrust/sort.h>
+#undef _CubLog
+#ifdef ARBORX_CubLog_save
+#define _CubLog ARBORX_CubLog_save
+#undef ARBORX_CubLog_save
 #endif
+#else // #if (KOKKOS_COMPILER_CLANG < 900)
+#include <thrust/device_ptr.h>
+#include <thrust/sort.h>
+#endif // #if (KOKKOS_COMPILER_CLANG < 900)
+#endif // #if defined(KOKKOS_ENABLE_CUDA)
 
 namespace ArborX
 {
