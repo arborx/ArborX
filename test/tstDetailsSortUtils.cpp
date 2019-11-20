f3526abdb111e932e83932bfa85855ba6c5f896a
Daniel Arndt <arndtd@ornl.gov>
2019-11-20 13:34:35 -0500

    Unify names and use Unmanged views

--- test/tstDetailsSortUtils.cpp
+++ test/tstDetailsSortUtils.cpp
@@ -49,21 +49,21 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(sort_results, DeviceType, ARBORX_DEVICE_TYPES)
   BOOST_TEST(ranks_.size() == n);
 
   Kokkos::View<int *, DeviceType> ids("query_ids", n);
-  auto ids_host = Kokkos::create_mirror_view(ids);
-  for (int i = 0; i < n; ++i)
-    ids_host(i) = ids_[i];
+  Kokkos::View<int *, Kokkos::HostSpace,
+               Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+      ids_host(ids_.data(), ids_.size());
   Kokkos::deep_copy(ids, ids_host);
 
   Kokkos::View<int *, DeviceType> results("results", n);
-  auto results_host = Kokkos::create_mirror_view(results);
-  for (int i = 0; i < n; ++i)
-    results_host(i) = results_[i];
+  Kokkos::View<int *, Kokkos::HostSpace,
+               Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+      results_host(results_.data(), results_.size());
   Kokkos::deep_copy(results, results_host);
 
   Kokkos::View<int *, DeviceType> ranks("ranks", n);
-  auto ranks_host = Kokkos::create_mirror_view(ranks);
-  for (int i = 0; i < n; ++i)
-    ranks_host(i) = ranks_[i];
+  Kokkos::View<int *, Kokkos::HostSpace,
+               Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+      ranks_host(ranks_.data(), ranks_.size());
   Kokkos::deep_copy(ranks, ranks_host);
 
   ArborX::Details::sortMultipleViews(ids, results, ranks);
@@ -88,25 +88,25 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(sort_results, DeviceType, ARBORX_DEVICE_TYPES)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(sort_results_2d, DeviceType, ARBORX_DEVICE_TYPES)
 {
-  std::array<int, 5> ids{4, 2, 1, 3, 5};
-  Kokkos::View<int *, DeviceType> ids_device("ids", 5);
-  auto ids_host = Kokkos::create_mirror_view(ids_device);
-  for (int i = 0; i < 5; ++i)
-    ids_host[i] = ids[i];
-  Kokkos::deep_copy(ids_device, ids_host);
+  std::array<int, 5> ids_{4, 2, 1, 3, 5};
+  Kokkos::View<int *, DeviceType> ids("ids", 5);
+  Kokkos::View<int *, Kokkos::HostSpace,
+               Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+      ids_host(ids_.data(), ids_.size());
+  Kokkos::deep_copy(ids, ids_host);
 
-  int results_2d[5][2] = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
+  int results_2d_[5][2] = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
   int sorted_results_2d[5][2] = {{5, 6}, {3, 4}, {7, 8}, {1, 2}, {9, 10}};
-  Kokkos::View<int **, DeviceType> results_2d_device("results", 5, 2);
-  auto results_2d_host = Kokkos::create_mirror_view(results_2d_device);
+  Kokkos::View<int **, DeviceType> results_2d("results", 5, 2);
+  auto results_2d_host = Kokkos::create_mirror_view(results_2d);
   for (int i = 0; i < 5; ++i)
     for (int j = 0; j < 2; ++j)
-      results_2d_host(i, j) = results_2d[i][j];
-  Kokkos::deep_copy(results_2d_device, results_2d_host);
+      results_2d_host(i, j) = results_2d_[i][j];
+  Kokkos::deep_copy(results_2d, results_2d_host);
 
-  ArborX::Details::sortMultipleViews(ids_device, results_2d_device);
+  ArborX::Details::sortMultipleViews(ids, results_2d);
 
-  Kokkos::deep_copy(results_2d_host, results_2d_device);
+  Kokkos::deep_copy(results_2d_host, results_2d);
   for (int i = 0; i < 5; ++i)
     for (int j = 0; j < 2; ++j)
       BOOST_TEST(results_2d_host(i, j) == sorted_results_2d[i][j]);
@@ -114,34 +114,34 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(sort_results_2d, DeviceType, ARBORX_DEVICE_TYPES)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(sort_results_3d, DeviceType, ARBORX_DEVICE_TYPES)
 {
-  std::array<int, 5> ids{4, 2, 1, 3, 5};
-  Kokkos::View<int *, DeviceType> ids_device("ids", 5);
-  auto ids_host = Kokkos::create_mirror_view(ids_device);
-  for (int i = 0; i < 5; ++i)
-    ids_host[i] = ids[i];
-  Kokkos::deep_copy(ids_device, ids_host);
+  std::array<int, 5> ids_{4, 2, 1, 3, 5};
+  Kokkos::View<int *, DeviceType> ids("ids", 5);
+  Kokkos::View<int *, Kokkos::HostSpace,
+               Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+      ids_host(ids_.data(), ids_.size());
+  Kokkos::deep_copy(ids, ids_host);
 
-  int results_3d[5][2][2] = {{{1, 2}, {3, 4}},
-                             {{5, 6}, {7, 8}},
-                             {{9, 10}, {11, 12}},
-                             {{13, 14}, {15, 16}},
-                             {{17, 18}, {19, 20}}};
+  int results_3d_[5][2][2] = {{{1, 2}, {3, 4}},
+                              {{5, 6}, {7, 8}},
+                              {{9, 10}, {11, 12}},
+                              {{13, 14}, {15, 16}},
+                              {{17, 18}, {19, 20}}};
   int sorted_results_3d[5][2][2] = {{{9, 10}, {11, 12}},
                                     {{5, 6}, {7, 8}},
                                     {{13, 14}, {15, 16}},
                                     {{1, 2}, {3, 4}},
                                     {{17, 18}, {19, 20}}};
-  Kokkos::View<int ***, DeviceType> results_3d_device("results", 5, 2, 2);
-  auto results_3d_host = Kokkos::create_mirror_view(results_3d_device);
+  Kokkos::View<int ***, DeviceType> results_3d("results", 5, 2, 2);
+  auto results_3d_host = Kokkos::create_mirror_view(results_3d);
   for (int i = 0; i < 5; ++i)
     for (int j = 0; j < 2; ++j)
       for (int k = 0; k < 2; ++k)
-        results_3d_host(i, j, k) = results_3d[i][j][k];
-  Kokkos::deep_copy(results_3d_device, results_3d_host);
+        results_3d_host(i, j, k) = results_3d_[i][j][k];
+  Kokkos::deep_copy(results_3d, results_3d_host);
 
-  ArborX::Details::sortMultipleViews(ids_device, results_3d_device);
+  ArborX::Details::sortMultipleViews(ids, results_3d);
 
-  Kokkos::deep_copy(results_3d_host, results_3d_device);
+  Kokkos::deep_copy(results_3d_host, results_3d);
   for (int i = 0; i < 5; ++i)
     for (int j = 0; j < 2; ++j)
       for (int k = 0; k < 2; ++k)
