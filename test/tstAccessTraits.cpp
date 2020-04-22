ed28acddfa5e82b0351911da4c85a6a94a0eec62
Damien L-G <dalg24@gmail.com>
2020-04-22 17:51:17 -0400

    Drop has_access_traits in test access traits

--- test/tstAccessTraits.cpp
+++ test/tstAccessTraits.cpp
@@ -3,7 +3,6 @@
 #include <Kokkos_Core.hpp>
 
 using ArborX::Details::check_valid_access_traits;
-using ArborX::Details::has_access_traits;
 using ArborX::Traits::PredicatesTag;
 using ArborX::Traits::PrimitivesTag;
 
@@ -23,14 +22,6 @@ struct Access<HasEmptySpecialization, Tag>
 };
 } // namespace Traits
 } // namespace ArborX
-static_assert(has_access_traits<Kokkos::View<double *>, PrimitivesTag>{}, "");
-static_assert(has_access_traits<Kokkos::View<double *>, PredicatesTag>{}, "");
-static_assert(
-    !has_access_traits<HasNoAccessTraitsSpecialization, PrimitivesTag>{}, "");
-static_assert(
-    !has_access_traits<HasNoAccessTraitsSpecialization, PredicatesTag>{}, "");
-static_assert(has_access_traits<HasEmptySpecialization, PrimitivesTag>{}, "");
-static_assert(has_access_traits<HasEmptySpecialization, PredicatesTag>{}, "");
 
 int main()
 {
@@ -38,8 +29,15 @@ int main()
   Kokkos::View<float **> v;
   check_valid_access_traits(PrimitivesTag{}, p);
   check_valid_access_traits(PrimitivesTag{}, v);
+
   using NearestPredicate = decltype(ArborX::nearest(ArborX::Point{}));
   Kokkos::View<NearestPredicate *> q;
   check_valid_access_traits(PredicatesTag{}, q);
+
+  // Uncomment to see error messages
+  // check_valid_access_traits(PrimitivesTag{},
+  // HasNoAccessTraitsSpecialization{});
+
+  // check_valid_access_traits(PrimitivesTag{}, HasEmptySpecialization{});
 }
 
