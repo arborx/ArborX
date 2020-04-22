f759f41ee9df80474b09ca32b09a4d8e99d44f13
Damien L-G <dalg24@gmail.com>
2020-04-22 12:06:56 -0400

    Verify that Kokkos::View specializations of Access::Traits are valid in test

--- test/tstAccessTraits.cpp
+++ test/tstAccessTraits.cpp
@@ -2,9 +2,11 @@
 
 #include <Kokkos_Core.hpp>
 
+using ArborX::Details::check_valid_access_traits;
 using ArborX::Details::has_access_traits;
 using ArborX::Traits::PredicatesTag;
 using ArborX::Traits::PrimitivesTag;
+
 struct HasNoAccessTraitsSpecialization
 {
 };
@@ -36,4 +38,14 @@ static_assert(has_access_traits<HasEmptySpecialization, PrimitivesTag>::value,
 static_assert(has_access_traits<HasEmptySpecialization, PredicatesTag>::value,
               "");
 
-int main() {}
+int main()
+{
+  Kokkos::View<ArborX::Point *> p;
+  Kokkos::View<float **> v;
+  check_valid_access_traits(PrimitivesTag{}, p);
+  check_valid_access_traits(PrimitivesTag{}, v);
+  using NearestPredicate = decltype(ArborX::nearest(ArborX::Point{}));
+  Kokkos::View<NearestPredicate *> q;
+  check_valid_access_traits(PredicatesTag{}, q);
+}
+
