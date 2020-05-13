bf46e30ed5498548f2240a75f76a648c76bc132b
Damien L-G <dalg24@gmail.com>
2020-05-13 11:40:26 -0400

    Add specialization of TreeTraversal for nearest predicates

--- src/details/ArborX_DetailsTreeTraversal.hpp
+++ src/details/ArborX_DetailsTreeTraversal.hpp
@@ -127,6 +127,165 @@ struct TreeTraversal<
   }
 };
 
+// NOTE using tuple as a workaround
+// Error: class template partial specialization contains a template parameter
+// that cannot be deduced
+template <typename BVH, typename Predicates, typename Tuple>
+struct TreeTraversal<
+    BVH, Predicates, Tuple,
+    std::enable_if_t<std::is_same<
+        NearestPredicateTag, typename Traits::Helper<Traits::Access<
+                                 Predicates, Traits::PredicatesTag>>::tag>{}>>
+{
+  BVH bvh_;
+  Predicates predicates_;
+  using Callback = std::tuple_element_t<0, Tuple>;
+  using Buffer = std::tuple_element_t<1, Tuple>;
+  Callback callback_;
+  Buffer buffer_;
+
+  using Access = Traits::Access<Predicates, Traits::PredicatesTag>;
+
+  template <typename ExecutionSpace>
+  TreeTraversal(ExecutionSpace const &space, BVH const &bvh,
+                Predicates const &predicates, Callback const &callback,
+                Buffer const &buffer)
+      : bvh_{bvh}
+      , predicates_{predicates}
+      , callback_{callback}
+      , buffer_{buffer}
+  {
+    Kokkos::parallel_for(
+        ARBORX_MARK_REGION("BVH:nearest_queries"),
+        Kokkos::RangePolicy<ExecutionSpace>(space, 0, Access::size(predicates)),
+        *this);
+  }
+
+  KOKKOS_FUNCTION int operator()(int queryIndex) const
+  {
+    auto const &predicate = Access::get(predicates_, queryIndex);
+    auto const k = getK(predicate);
+    auto const distance = [geometry = getGeometry(predicate),
+                           bvh = bvh_](Node const *node) {
+      return Details::distance(geometry, bvh.getBoundingVolume(node));
+    };
+    auto const buffer = buffer_(queryIndex);
+
+    if (bvh_.empty() || k < 1)
+      return 0;
+
+    if (bvh_.size() == 1)
+    {
+      callback_(queryIndex, 0, distance(bvh_.getRoot()));
+      return 1;
+    }
+
+    // Nodes with a distance that exceed that radius can safely be
+    // discarded. Initialize the radius to infinity and tighten it once k
+    // neighbors have been found.
+    auto radius = KokkosExt::ArithmeticTraits::infinity<float>::value;
+
+    using PairIndexDistance = Kokkos::pair<int, float>;
+    static_assert(
+        std::is_same<typename decltype(buffer)::value_type,
+                     PairIndexDistance>::value,
+        "Type of the elements stored in the buffer passed as argument to "
+        "TreeTraversal::nearestQuery is not right");
+    struct CompareDistance
+    {
+      KOKKOS_INLINE_FUNCTION bool operator()(PairIndexDistance const &lhs,
+                                             PairIndexDistance const &rhs) const
+      {
+        return lhs.second < rhs.second;
+      }
+    };
+    // Use a priority queue for convenience to store the results and
+    // preserve the heap structure internally at all time.  There is no
+    // memory allocation, elements are stored in the buffer passed as an
+    // argument. The farthest leaf node is on top.
+    assert(k == buffer.size());
+    PriorityQueue<PairIndexDistance, CompareDistance,
+                  UnmanagedStaticVector<PairIndexDistance>>
+        heap(UnmanagedStaticVector<PairIndexDistance>(buffer.data(),
+                                                      buffer.size()));
+
+    using PairNodePtrDistance = Kokkos::pair<Node const *, float>;
+    Stack<PairNodePtrDistance> stack;
+    // Do not bother computing the distance to the root node since it is
+    // immediately popped out of the stack and processed.
+    stack.emplace(bvh_.getRoot(), 0.);
+
+    while (!stack.empty())
+    {
+      Node const *node = stack.top().first;
+      auto const node_distance = stack.top().second;
+      stack.pop();
+
+      if (node_distance < radius)
+      {
+        if (node->isLeaf())
+        {
+          int const leaf_index = node->getLeafPermutationIndex();
+          auto const leaf_distance = node_distance;
+          if (heap.size() < k)
+          {
+            // Insert leaf node and update radius if it was the kth
+            // one.
+            heap.push(Kokkos::make_pair(leaf_index, leaf_distance));
+            if (heap.size() == k)
+              radius = heap.top().second;
+          }
+          else
+          {
+            // Replace top element in the heap and update radius.
+            heap.popPush(Kokkos::make_pair(leaf_index, leaf_distance));
+            radius = heap.top().second;
+          }
+        }
+        else
+        {
+          // Insert children into the stack and make sure that the
+          // closest one ends on top.
+          Node const *left_child = bvh_.getNodePtr(node->children.first);
+          Node const *right_child = bvh_.getNodePtr(node->children.second);
+          auto const left_child_distance = distance(left_child);
+          auto const right_child_distance = distance(right_child);
+          if (left_child_distance < right_child_distance)
+          {
+            // NOTE not really sure why but it performed better with
+            // the conditional insertion on the device and without
+            // it on the host (~5% improvement for both)
+#if defined(__CUDA_ARCH__)
+            if (right_child_distance < radius)
+#endif
+              stack.emplace(right_child, right_child_distance);
+            stack.emplace(left_child, left_child_distance);
+          }
+          else
+          {
+#if defined(__CUDA_ARCH__)
+            if (left_child_distance < radius)
+#endif
+              stack.emplace(left_child, left_child_distance);
+            stack.emplace(right_child, right_child_distance);
+          }
+        }
+      }
+    }
+    // Sort the leaf nodes and output the results.
+    // NOTE: Do not try this at home.  Messing with the underlying container
+    // invalidates the state of the PriorityQueue.
+    sortHeap(heap.data(), heap.data() + heap.size(), heap.valueComp());
+    for (decltype(heap.size()) i = 0; i < heap.size(); ++i)
+    {
+      int const leaf_index = (heap.data() + i)->first;
+      auto const leaf_distance = (heap.data() + i)->second;
+      callback_(queryIndex, leaf_index, leaf_distance);
+    }
+    return heap.size();
+  }
+};
+
 template <typename BVH>
 using DeprecatedTreeTraversal = TreeTraversal<BVH, void, void, void>;
 
