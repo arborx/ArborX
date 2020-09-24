1cbebb6aa3cdcf34cea472bcbc2292298e302a5f
Daniel Arndt <arndtd@ornl.gov>
2020-09-24 15:51:13 -0400

    Add test for View prefixes in DistributedSearchTree

--- test/tstKokkosToolsDistributedAnnotations.cpp
+++ test/tstKokkosToolsDistributedAnnotations.cpp
@@ -21,7 +21,7 @@
 #if (KOKKOS_VERSION >= 30200) // callback registriation from within the program
                               // was added in Kokkkos v3.2
 
-BOOST_AUTO_TEST_SUITE(KokkosToolsDistributedAnnotations)
+BOOST_AUTO_TEST_SUITE(DistributedKokkosToolsAnnotations)
 
 namespace tt = boost::test_tools;
 
@@ -37,6 +37,139 @@ BOOST_AUTO_TEST_CASE(is_prefixed_with)
   BOOST_TEST(!isPrefixedWith("Nope::ArborX", "ArborX"));
 }
 
+BOOST_AUTO_TEST_CASE_TEMPLATE(
+    distributed_search_tree_distributed_search_tree_allocations_prefixed,
+    DeviceType, ARBORX_DEVICE_TYPES)
+{
+  Kokkos::Tools::Experimental::set_allocate_data_callback(
+      [](Kokkos::Profiling::SpaceHandle handle, const char *label,
+         void const *ptr, uint64_t size) {
+        std::cout << label << '\n';
+        BOOST_TEST((isPrefixedWith(label, "ArborX::DistributedSearchTree::") ||
+                    isPrefixedWith(label, "ArborX::BVH::") ||
+                    isPrefixedWith(label, "ArborX::Sorting::") ||
+                    isPrefixedWith(label, "Testing::")));
+      });
+
+  { // empty
+    auto tree = makeDistributedSearchTree<DeviceType>(MPI_COMM_WORLD, {});
+  }
+
+  { // one leaf per process
+    auto tree = makeDistributedSearchTree<DeviceType>(
+        MPI_COMM_WORLD, {
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                        });
+  }
+
+  { // two leaves per process
+    auto tree = makeDistributedSearchTree<DeviceType>(
+        MPI_COMM_WORLD, {
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                        });
+  }
+
+  Kokkos::Tools::Experimental::set_allocate_data_callback(nullptr);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(
+    distributed_search_tree_query_allocations_prefixed, DeviceType,
+    ARBORX_DEVICE_TYPES)
+{
+  auto tree = makeDistributedSearchTree<DeviceType>(
+      MPI_COMM_WORLD, {
+                          {{{0, 0, 0}}, {{1, 1, 1}}},
+                          {{{0, 0, 0}}, {{1, 1, 1}}},
+                      });
+
+  Kokkos::Tools::Experimental::set_allocate_data_callback(
+      [](Kokkos::Profiling::SpaceHandle handle, const char *label,
+         void const *ptr, uint64_t size) {
+        std::cout << label << '\n';
+        BOOST_TEST(
+            (isPrefixedWith(label, "ArborX::DistributedSearchTree::query::") ||
+             isPrefixedWith(label, "ArborX::Distributor::") ||
+             isPrefixedWith(label, "ArborX::BVH::query::") ||
+             isPrefixedWith(label, "ArborX::BufferOptimization::") ||
+             isPrefixedWith(label, "ArborX::Sorting::") ||
+             isPrefixedWith(label, "Kokkos::SortImpl::") ||
+             isPrefixedWith(label, "Testing::")));
+      });
+
+  // spatial predicates
+  query(tree, makeIntersectsBoxQueries<DeviceType>({
+                  {{{0, 0, 0}}, {{1, 1, 1}}},
+                  {{{0, 0, 0}}, {{1, 1, 1}}},
+              }));
+
+  // nearest predicates
+  query(tree, makeNearestQueries<DeviceType>({
+                  {{{0, 0, 0}}, 1},
+                  {{{0, 0, 0}}, 2},
+              }));
+
+  Kokkos::Tools::Experimental::set_allocate_data_callback(nullptr);
+}
+
+/*
+BOOST_AUTO_TEST_CASE_TEMPLATE(kernels_prefixed, DeviceType, ARBORX_DEVICE_TYPES)
+{
+  auto const callback = [](char const *label, uint32_t, uint64_t *) {
+    std::cout << label << '\n';
+    BOOST_TEST((isPrefixedWith(label, "ArborX::") ||
+                isPrefixedWith(label, "Kokkos::")));
+  };
+  Kokkos::Tools::Experimental::set_begin_parallel_for_callback(callback);
+  Kokkos::Tools::Experimental::set_begin_parallel_scan_callback(callback);
+  Kokkos::Tools::Experimental::set_begin_parallel_reduce_callback(callback);
+
+  // DistributedSearchTree::DistributedSearchTree
+
+  { // empty
+    auto tree = makeDistributedSearchTree<DeviceType>(MPI_COMM_WORLD, {});
+  }
+
+  { // one leaf
+    auto tree = makeDistributedSearchTree<DeviceType>(
+        MPI_COMM_WORLD, {
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                        });
+  }
+
+  { // two leaves
+    auto tree = makeDistributedSearchTree<DeviceType>(
+        MPI_COMM_WORLD, {
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                        });
+  }
+
+  // DistributedSearchTree::query
+
+  auto tree = makeDistributedSearchTree<DeviceType>(
+      MPI_COMM_WORLD, {
+                          {{{0, 0, 0}}, {{1, 1, 1}}},
+                          {{{0, 0, 0}}, {{1, 1, 1}}},
+                      });
+
+  // spatial predicates
+  query(tree, makeIntersectsBoxQueries<DeviceType>({
+                  {{{0, 0, 0}}, {{1, 1, 1}}},
+                  {{{0, 0, 0}}, {{1, 1, 1}}},
+              }));
+
+  // nearest predicates
+  query(tree, makeNearestQueries<DeviceType>({
+                  {{{0, 0, 0}}, 1},
+                  {{{0, 0, 0}}, 2},
+              }));
+
+  Kokkos::Tools::Experimental::set_begin_parallel_for_callback(nullptr);
+  Kokkos::Tools::Experimental::set_begin_parallel_scan_callback(nullptr);
+  Kokkos::Tools::Experimental::set_begin_parallel_reduce_callback(nullptr);
+}
+
 BOOST_AUTO_TEST_CASE_TEMPLATE(regions_prefixed, DeviceType, ARBORX_DEVICE_TYPES)
 {
   Kokkos::Tools::Experimental::set_push_region_callback([](char const *label) {
@@ -49,6 +182,19 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(regions_prefixed, DeviceType, ARBORX_DEVICE_TYPES)
 
   { // empty
     auto tree = makeDistributedSearchTree<DeviceType>(MPI_COMM_WORLD, {});
+  }
+
+  { // one leaf per process
+    auto tree = makeDistributedSearchTree<DeviceType>(
+        MPI_COMM_WORLD, {{{{0, 0, 0}}, {{1, 1, 1}}}});
+  }
+
+  { // two leaves per process
+    auto tree = makeDistributedSearchTree<DeviceType>(
+        MPI_COMM_WORLD, {
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                            {{{0, 0, 0}}, {{1, 1, 1}}},
+                        });
   }
 
   // DistributedSearchTree::query
@@ -73,6 +219,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(regions_prefixed, DeviceType, ARBORX_DEVICE_TYPES)
 
   Kokkos::Tools::Experimental::set_push_region_callback(nullptr);
 }
+*/
 
 BOOST_AUTO_TEST_SUITE_END()
 
