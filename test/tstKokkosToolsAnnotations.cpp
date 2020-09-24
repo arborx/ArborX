6918cdb553725558909c99e4a1543bb1010fa29b
Damien L-G <dalg24@gmail.com>
2020-09-24 00:17:26 -0400

    Fixup view allocation unit test on BVH constructor
    We changed the view data member prefix and forgot to update.

--- test/tstKokkosToolsAnnotations.cpp
+++ test/tstKokkosToolsAnnotations.cpp
@@ -55,6 +55,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(bvh_bvh_allocations_prefixed, Tree, TreeTypes)
          void const * /*ptr*/, uint64_t /*size*/) {
         std::cout << label << '\n';
         BOOST_TEST(
+            (isPrefixedWith(label, "ArborX::BVH::") || // data member
             (isPrefixedWith(label, "ArborX::BVH::BVH::") ||
              isPrefixedWith(label, "ArborX::Sorting::") ||
              isPrefixedWith(label,
