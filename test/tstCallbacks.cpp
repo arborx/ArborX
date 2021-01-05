d7991ee2a6bb7a61b5fb4f46d9d506ce62d8253c
Andrey Prokopenko <prokopenkoav@ornl.gov>
2021-01-05 09:39:53 -0500

    Add missing copyright headers

--- test/tstCallbacks.cpp
+++ test/tstCallbacks.cpp
@@ -1,3 +1,14 @@
+/****************************************************************************
+ * Copyright (c) 2017-2021 by the ArborX authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ArborX library. ArborX is                       *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
 #include <ArborX_AccessTraits.hpp>
 #include <ArborX_Callbacks.hpp>
 #include <ArborX_Predicates.hpp>
