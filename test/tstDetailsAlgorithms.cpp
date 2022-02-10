c3384d242724f31046f155e5cb4fb5c7601a0bd1
Damien L-G <dalg24@gmail.com>
2022-02-10 13:59:17 -0500

    Drive by changes (typo any[]more and trailing semicolon)

--- test/tstDetailsAlgorithms.cpp
+++ test/tstDetailsAlgorithms.cpp
@@ -15,8 +15,8 @@
 #define BOOST_TEST_MODULE Geometry
 #include <boost/test/unit_test.hpp>
 
-// NOTE: message is not required any more with C++17
-#define STATIC_ASSERT(cond) static_assert(cond, "");
+// NOTE: message is not required anymore with C++17
+#define STATIC_ASSERT(cond) static_assert(cond, "")
 
 using ArborX::Box;
 using ArborX::Point;
