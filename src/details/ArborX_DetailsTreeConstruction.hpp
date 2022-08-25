2a431676d9d7321f414cef738fb823bb5f62431b
Andrey Prokopenko <prokopenkoav@ornl.gov>
2022-08-25 15:04:24 -0400

    Template helper tree construction on Box instead of hardcoding

--- src/details/ArborX_DetailsTreeConstruction.hpp
+++ src/details/ArborX_DetailsTreeConstruction.hpp
@@ -13,7 +13,6 @@
 #define ARBORX_DETAILS_TREE_CONSTRUCTION_HPP
 
 #include <ArborX_AccessTraits.hpp>
-#include <ArborX_Box.hpp>
 #include <ArborX_DetailsAlgorithms.hpp> // expand
 #include <ArborX_DetailsKokkosExtArithmeticTraits.hpp>
 #include <ArborX_DetailsNode.hpp> // makeLeafNode
@@ -30,7 +29,7 @@ namespace Details
 namespace TreeConstruction
 {
 
-template <typename ExecutionSpace, typename Primitives>
+template <typename ExecutionSpace, typename Primitives, typename Box>
 inline void calculateBoundingBoxOfTheScene(ExecutionSpace const &space,
                                            Primitives const &primitives,
                                            Box &scene_bounding_box)
@@ -47,7 +46,7 @@ inline void calculateBoundingBoxOfTheScene(ExecutionSpace const &space,
 }
 
 template <typename ExecutionSpace, typename Primitives,
-          typename SpaceFillingCurve, typename LinearOrdering>
+          typename SpaceFillingCurve, typename Box, typename LinearOrdering>
 inline void projectOntoSpaceFillingCurve(ExecutionSpace const &space,
                                          Primitives const &primitives,
                                          SpaceFillingCurve const &curve,
