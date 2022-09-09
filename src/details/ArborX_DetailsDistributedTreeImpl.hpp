e0d0d79f8b3ab7f96e0decc9fa43187aa5a7a3ec
Daniel Arndt <arndtd@ornl.gov>
2022-09-09 17:34:50 -0400

    Indentation

--- src/details/ArborX_DetailsDistributedTreeImpl.hpp
+++ src/details/ArborX_DetailsDistributedTreeImpl.hpp
@@ -797,12 +797,18 @@ DistributedTreeImpl<DeviceType>::queryDispatch(
     Kokkos::View<int const *, Kokkos::HostSpace> host_offsets(off.data(),
                                                               off.size());
     typename DeviceType::memory_space memory_space;
-    Kokkos::View<int *, DeviceType> destinations(Kokkos::view_alloc(Kokkos::WithoutInitializing, "ArborX::DistributedTree::query::destinations"), dest.size());
+    Kokkos::View<int *, DeviceType> destinations(
+        Kokkos::view_alloc(Kokkos::WithoutInitializing,
+                           "ArborX::DistributedTree::query::destinations"),
+        dest.size());
     Kokkos::deep_copy(space, destinations, host_destinations);
-    Kokkos::View<int *, DeviceType> offsets(Kokkos::view_alloc(Kokkos::WithoutInitializing, "ArborX::DistributedTree::query::offsets"), off.size());
+    Kokkos::View<int *, DeviceType> offsets(
+        Kokkos::view_alloc(Kokkos::WithoutInitializing,
+                           "ArborX::DistributedTree::query::offsets"),
+        off.size());
     Kokkos::deep_copy(space, offsets, host_offsets);
-    auto const n_imports_back = back_distributor.createFromSends(
-        space, destinations, offsets);
+    auto const n_imports_back =
+        back_distributor.createFromSends(space, destinations, offsets);
     KokkosExt::reallocWithoutInitializing(space, out, n_imports_back);
     Kokkos::View<int *, DeviceType> ids(
         "ArborX::DistributedTree::query::nearest::query_ids", 0);
