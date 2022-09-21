86666cbc34044a9c7278ae5ca274c20ab65645f9
Andrey Prokopenko <prokopenkoav@ornl.gov>
2022-09-20 23:54:10 -0400

    Clean up arithmetic traits to always forward to Kokkos

--- src/kokkos_ext/ArborX_DetailsKokkosExtArithmeticTraits.hpp
+++ src/kokkos_ext/ArborX_DetailsKokkosExtArithmeticTraits.hpp
@@ -12,13 +12,11 @@
 #ifndef ARBORX_DETAILS_KOKKOS_EXT_ARITHMETIC_TRAITS_HPP
 #define ARBORX_DETAILS_KOKKOS_EXT_ARITHMETIC_TRAITS_HPP
 
-#include <Kokkos_Macros.hpp>
-#if KOKKOS_VERSION >= 30599
 #include <Kokkos_NumericTraits.hpp>
-namespace KokkosExt
+
+namespace KokkosExt::ArithmeticTraits
 {
-namespace ArithmeticTraits
-{
+
 template <class T>
 using infinity = Kokkos::Experimental::infinity<T>;
 
@@ -30,127 +28,7 @@ using finite_min = Kokkos::Experimental::finite_min<T>;
 
 template <class T>
 using epsilon = Kokkos::Experimental::epsilon<T>;
-} // namespace ArithmeticTraits
-} // namespace KokkosExt
 
-#else
-#include <cfloat>  // DBL_MAX, DBL_EPSILON
-#include <climits> // INT_MAX, INT_MIN
-#include <cmath>   // HUGE_VAL
-#include <type_traits>
-
-namespace KokkosExt
-{
-namespace ArithmeticTraits
-{
-namespace Details
-{
-
-template <typename T>
-struct infinity;
-
-template <>
-struct infinity<float>
-{
-  static constexpr float value = HUGE_VALF;
-};
-
-template <>
-struct infinity<double>
-{
-  static constexpr double value = HUGE_VAL;
-};
-
-template <typename T>
-struct finite_max;
-
-template <>
-struct finite_max<float>
-{
-  static constexpr float value = FLT_MAX;
-};
-
-template <>
-struct finite_max<double>
-{
-  static constexpr double value = DBL_MAX;
-};
-
-template <>
-struct finite_max<int>
-{
-  static constexpr int value = INT_MAX;
-};
-
-template <>
-struct finite_max<long long>
-{
-  static constexpr long long value = LLONG_MAX;
-};
-
-template <typename T>
-struct finite_min;
-
-template <>
-struct finite_min<float>
-{
-  static constexpr float value = -FLT_MAX;
-};
-
-template <>
-struct finite_min<double>
-{
-  static constexpr double value = -DBL_MAX;
-};
-
-template <>
-struct finite_min<int>
-{
-  static constexpr int value = INT_MIN;
-};
-
-template <>
-struct finite_min<long long>
-{
-  static constexpr long long value = LLONG_MIN;
-};
-
-template <typename T>
-struct epsilon;
-
-template <>
-struct epsilon<float>
-{
-  static constexpr float value = FLT_EPSILON;
-};
-
-template <>
-struct epsilon<double>
-{
-  static constexpr double value = DBL_EPSILON;
-};
-
-} // namespace Details
-
-template <class T>
-struct infinity : Details::infinity<std::remove_cv_t<T>>
-{};
-
-template <class T>
-struct finite_max : Details::finite_max<std::remove_cv_t<T>>
-{};
-
-template <class T>
-struct finite_min : Details::finite_min<std::remove_cv_t<T>>
-{};
-
-template <class T>
-struct epsilon : Details::epsilon<std::remove_cv_t<T>>
-{};
-
-} // namespace ArithmeticTraits
-
-} // namespace KokkosExt
-#endif
+} // namespace KokkosExt::ArithmeticTraits
 
 #endif
