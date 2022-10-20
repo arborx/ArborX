cbe4cd207e06646be27d2b96463a1a4439f58ca0
Daniel Arndt <arndtd@ornl.gov>
2022-10-20 14:24:03 +0000

    Use target_compile_definitions for SYCL workaround instead

--- test/tstKokkosToolsAnnotations.cpp
+++ test/tstKokkosToolsAnnotations.cpp
@@ -14,9 +14,6 @@
 
 #include <boost/test/unit_test.hpp>
 
-#ifdef KOKKOS_ENABLE_SYCL // FIXME_SYCL
-#define NANORANGE_NO_STD_FORWARD_DECLARATIONS
-#endif
 #include <regex>
 
 #include "Search_UnitTestHelpers.hpp"
