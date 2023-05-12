d20e5d02ce8547e37578acc1f8c11d01ef523e6a
Daniel Arndt <arndtd@ornl.gov>
2023-05-12 13:04:05 -0400

    Only adjust tolerance in MinimumSpanningTreeGoldenTest for Kokkos::Experimental::SYCL execution space

--- test/tstMinimumSpanningTreeGoldenTest.cpp
+++ test/tstMinimumSpanningTreeGoldenTest.cpp
@@ -133,14 +133,13 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(minimum_spanning_tree_golden_test, DeviceType,
   }
 
   namespace tt = boost::test_tools;
+  float tol = 1.e-8;
 // FIXME_SYCL
 #ifdef KOKKOS_ENABLE_SYCL
-  BOOST_TEST(total_weight[5] == ref_total_weight[5], tt::tolerance(1e-7));
-  BOOST_TEST(total_weight[10] == ref_total_weight[10], tt::tolerance(1e-7));
-  BOOST_TEST(total_weight[15] == ref_total_weight[15], tt::tolerance(1e-7));
-#else
-  BOOST_TEST(total_weight[5] == ref_total_weight[5], tt::tolerance(1e-8));
-  BOOST_TEST(total_weight[10] == ref_total_weight[10], tt::tolerance(1e-8));
-  BOOST_TEST(total_weight[15] == ref_total_weight[15], tt::tolerance(1e-8));
+  if constexpr (std::is_same_v<ExecutionSpace, Kokkos::Experimental::SYCL>)
+    tol = 1.e-7;
 #endif
+  BOOST_TEST(total_weight[5] == ref_total_weight[5], tt::tolerance(tol));
+  BOOST_TEST(total_weight[10] == ref_total_weight[10], tt::tolerance(tol));
+  BOOST_TEST(total_weight[15] == ref_total_weight[15], tt::tolerance(tol));
 }
