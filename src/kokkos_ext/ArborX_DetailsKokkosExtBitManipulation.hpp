bf97ec02b3949f631b36ac67613a6e6adda20023
Andrey Prokopenko <prokopenkoav@ornl.gov>
2023-06-06 16:35:43 -0400

    Add KokkosExt::bit_cast

---
+++ src/kokkos_ext/ArborX_DetailsKokkosExtBitManipulation.hpp
@@ -0,0 +1,41 @@
+/****************************************************************************
+ * Copyright (c) 2023 by the ArborX authors                                 *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ArborX library. ArborX is                       *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef ARBORX_DETAILS_KOKKOS_EXT_BIT_MANIPULATION_HPP
+#define ARBORX_DETAILS_KOKKOS_EXT_BIT_MANIPULATION_HPP
+
+#include <Kokkos_Macros.hpp>
+
+#include <type_traits>
+
+namespace KokkosExt
+{
+
+#if KOKKOS_VERSION >= 40100
+template <class To, class From>
+using bit_cast = Kokkos::bit_cast<To, From>;
+#else
+template <class To, class From>
+KOKKOS_FUNCTION std::enable_if_t<sizeof(To) == sizeof(From) &&
+                                     std::is_trivially_copyable_v<To> &&
+                                     std::is_trivially_copyable_v<From>,
+                                 To>
+bit_cast(From const &from) noexcept
+{
+  To to;
+  memcpy(&to, &from, sizeof(To));
+  return to;
+}
+#endif
+
+} // namespace KokkosExt
+
+#endif
