12105e86effa76970776821e1e1689e643269bcc
Yohann Bosqued <bosq.yohann@gmail.com>
2023-07-28 13:38:57 -0400

    New example program

---
+++ examples/moving_least_squares/moving_least_squares.cpp
@@ -0,0 +1,14 @@
+/****************************************************************************
+ * Copyright (c) 2017-2023 by the ArborX authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ArborX library. ArborX is                       *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#include <ArborX.hpp>
+
+int main(int argc, char *argv[]) { return 0; }
