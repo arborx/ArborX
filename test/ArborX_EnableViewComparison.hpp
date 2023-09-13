797fd65da3e49351bf875f4e8b474e5c3a782529
Yohann Bosqued <bosq.yohann@gmail.com>
2023-09-13 11:22:58 -0400

    Better multi dimensional comparison

--- test/ArborX_EnableViewComparison.hpp
+++ test/ArborX_EnableViewComparison.hpp
@@ -17,63 +17,76 @@
 #include <Kokkos_Core.hpp>
 
 #include "BoostTest_CUDA_clang_workarounds.hpp"
+#include <boost/test/unit_test.hpp>
 #include <boost/test/utils/is_forward_iterable.hpp>
 
-template <typename T>
-struct KokkosViewIterator;
-
-template <typename T, typename... P>
-struct KokkosViewIterator<Kokkos::View<T, P...>>
+template <typename U, typename V>
+void arborxViewCheck(U const &u, V const &v, std::string const &u_name,
+                     std::string const &v_name, double tol = 0.)
 {
-  using self_t = KokkosViewIterator<Kokkos::View<T, P...>>;
-  using view_t = Kokkos::View<T, P...>;
-  using value_t = typename view_t::value_type;
+  static constexpr std::size_t rank = U::rank;
 
-  value_t &operator*()
+  bool same_dim_size = true;
+  for (std::size_t i = 0; i < rank; i++)
   {
-    return view.access(index[0], index[1], index[2], index[3], index[4],
-                       index[5], index[6], index[7]);
+    std::size_t ui = u.extent(i), vi = v.extent(i);
+    BOOST_TEST(ui == vi, u_name << " == " << v_name << " dimension " << i
+                                << " sizes"
+                                << boost::test_tools::tolerance(0.));
+    same_dim_size = (ui == vi) && same_dim_size;
   }
 
-  value_t *operator->() { return &operator*(); }
+  if (!same_dim_size)
+    return;
 
-  self_t &operator++()
+  auto const layout = u.layout();
+  Kokkos::Array<std::size_t, 8> index{0, 0, 0, 0, 0, 0, 0, 0};
+  auto make_index = [&]() {
+    std::stringstream sstr;
+    sstr << "(";
+    for (std::size_t i = 0; i < rank - 1; i++)
+      sstr << index[i] << ", ";
+    sstr << index[rank - 1] << ")";
+    return sstr.str();
+  };
+
+  while (index[0] != layout.dimension[0])
   {
-    index[7]++;
-    auto const layout = view.layout();
+    auto uval = u.access(index[0], index[1], index[2], index[3], index[4],
+                         index[5], index[6], index[7]);
+    auto vval = v.access(index[0], index[1], index[2], index[3], index[4],
+                         index[5], index[6], index[7]);
+    std::string index_str = make_index();
 
-    for (std::size_t i = 7; i > 0; i--)
-      if (index[i] == layout.dimension[i] ||
-          layout.dimension[i] == KOKKOS_INVALID_INDEX)
+    BOOST_TEST(uval == vval, u_name << " == " << v_name << " at " << index_str
+                                    << boost::test_tools::tolerance(tol));
+
+    index[rank - 1]++;
+    for (std::size_t i = rank - 1; i > 0; i--)
+      if (index[i] == layout.dimension[i])
       {
         index[i] = 0;
         index[i - 1]++;
       }
-
-    return *this;
   }
+}
 
-  self_t operator++(int)
-  {
-    self_t old = *this;
-    operator++();
-    return old;
-  }
-
-  static self_t begin(view_t const &view)
-  {
-    return {view, {{0, 0, 0, 0, 0, 0, 0, 0}}};
-  }
-
-  static self_t end(view_t const &view)
-  {
-    auto const layout = view.layout();
-    return {view, {{layout.dimension[0], 0, 0, 0, 0, 0, 0, 0}}};
-  }
-
-  view_t view;
-  Kokkos::Array<std::size_t, 8> index;
-};
+#define ARBORX_MDVIEW_TEST(VIEWA, VIEWB, ...)                                  \
+  [](decltype(VIEWA) const &u, decltype(VIEWB) const &v) {                     \
+    auto view_a = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, u); \
+    auto view_b = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, v); \
+                                                                               \
+    static_assert(decltype(view_a)::rank == decltype(view_b)::rank,            \
+                  "'" #VIEWA "' and '" #VIEWB "' must have the same rank");    \
+                                                                               \
+    std::string view_a_name(#VIEWA);                                           \
+    view_a_name += " (" + view_a.label() + ")";                                \
+                                                                               \
+    std::string view_b_name(#VIEWB);                                           \
+    view_b_name += " (" + view_b.label() + ")";                                \
+                                                                               \
+    arborxViewCheck(view_a, view_b, view_a_name, view_b_name, ##__VA_ARGS__);  \
+  }(VIEWA, VIEWB)
 
 // Enable element-wise comparison for views that are accessible from the host
 namespace boost
@@ -87,8 +100,11 @@ struct is_forward_iterable<Kokkos::View<T, P...>> : public boost::mpl::true_
   // NOTE Prefer static assertion to SFINAE because error message about no
   // operator== for the operands is not as clear.
   static_assert(
-      KokkosExt::is_accessible_from_host<Kokkos::View<T, P...>>::value,
-      "Restricted to host-accessible views");
+      Kokkos::View<T, P...>::rank == 1 &&
+          !std::is_same<typename Kokkos::View<T, P...>::array_layout,
+                        Kokkos::LayoutStride>::value &&
+          KokkosExt::is_accessible_from_host<Kokkos::View<T, P...>>::value,
+      "Restricted to contiguous rank-one host-accessible views");
 };
 
 template <typename T, typename... P>
@@ -96,18 +112,10 @@ struct bt_iterator_traits<Kokkos::View<T, P...>, true>
 {
   using view_type = Kokkos::View<T, P...>;
   using value_type = typename view_type::value_type;
-  using const_iterator = KokkosViewIterator<view_type>;
-
-  static const_iterator begin(view_type const &v)
-  {
-    return const_iterator::begin(v);
-  }
-
-  static const_iterator end(view_type const &v)
-  {
-    return const_iterator::end(v);
-  }
-
+  using const_iterator =
+      typename std::add_pointer<typename view_type::const_value_type>::type;
+  static const_iterator begin(view_type const &v) { return v.data(); }
+  static const_iterator end(view_type const &v) { return v.data() + v.size(); }
   static std::size_t size(view_type const &v) { return v.size(); }
 };
 
--- test/tstInterpDetailsSymmPInvSVD.cpp
+++ test/tstInterpDetailsSymmPInvSVD.cpp
@@ -19,81 +19,48 @@
 namespace tt = boost::test_tools;
 namespace axid = ArborX::Interpolation::Details;
 
-// Checks for equality betweem 2D or 3D views
-template <typename U, typename V>
-void equalityCheck(U const &u, V const &v,
-                   double tol = Kokkos::Experimental::epsilon_v<float>)
+template <typename ES, typename U, typename V>
+void makeCase(ES const &es, std::size_t id, V &src_arr, V &ref_arr,
+              std::size_t n, std::size_t m = 0)
 {
-  auto u_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, u);
-  auto v_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, v);
-  BOOST_TEST(u_host == v_host, tt::tolerance(tol) << tt::per_element());
-}
+  std::string id_string = std::to_string(id);
+  using host_view = typename U::HostMirror;
 
-// Makes a view from a 2D C array
-template <typename U, std::size_t N>
-auto viewHostRef2(double (&a)[N][N])
-{
-  typename U::HostMirror r("r", N, N);
+  host_view src;
+  host_view ref;
+  U inv;
 
-  for (std::size_t i = 0; i < N; i++)
-    for (std::size_t j = 0; j < N; j++)
-      r(i, j) = a[i][j];
+  if constexpr (U::rank == 2)
+  {
+    src = host_view("src" + id_string, n, n);
+    ref = host_view("ref" + id_string, n, n);
+    inv = U("inv" + id_string, n, n);
 
-  return r;
-}
+    for (std::size_t i = 0; i < n; i++)
+      for (std::size_t j = 0; j < n; j++)
+      {
+        src(i, j) = src_arr[i][j];
+        ref(i, j) = ref_arr[i][j];
+      }
+  }
+  else if constexpr (U::rank == 3)
+  {
+    src = host_view("src" + id_string, m, n, n);
+    ref = host_view("ref" + id_string, m, n, n);
+    inv = U("inv" + id_string, m, n, n);
 
-// Makes a view from a 3D C array
-template <typename U, std::size_t M, std::size_t N>
-auto viewHostRef3(double (&a)[M][N][N])
-{
-  typename U::HostMirror r("r", M, N, N);
+    for (std::size_t i = 0; i < m; i++)
+      for (std::size_t j = 0; j < n; j++)
+        for (std::size_t k = 0; k < n; k++)
+        {
+          src(i, j, k) = src_arr[i][j][k];
+          ref(i, j, k) = ref_arr[i][j][k];
+        }
+  }
 
-  for (std::size_t i = 0; i < M; i++)
-    for (std::size_t j = 0; j < N; j++)
-      for (std::size_t k = 0; k < N; k++)
-        r(i, j, k) = a[i][j][k];
-
-  return r;
-}
-
-// Computes the pseudo-inverse from a single matrix
-template <typename ES, typename U, std::size_t N>
-auto makePseudoInverse(ES const &es, typename U::HostMirror const &a)
-{
-  U inv("inv", N, N);
-  Kokkos::deep_copy(es, inv, a);
+  Kokkos::deep_copy(es, inv, src);
   axid::symmetricPseudoInverseSVD(es, inv);
-  return inv;
-}
-
-// Computes the pseudo-inverse from a list of matrices
-template <typename ES, typename U, std::size_t M, std::size_t N>
-auto makePseudoInverseList(ES const &es, typename U::HostMirror const &a)
-{
-  U inv("inv", M, N, N);
-  Kokkos::deep_copy(es, inv, a);
-  axid::symmetricPseudoInverseSVD(es, inv);
-  return inv;
-}
-
-// Makes the case for a single matrix
-template <typename ES, typename U, std::size_t N>
-void makeCase(ES const &es, double (&src)[N][N], double (&ref)[N][N])
-{
-  auto rsrc = viewHostRef2<U, N>(src);
-  auto rref = viewHostRef2<U, N>(ref);
-  auto inv = makePseudoInverse<ES, U, N>(es, rsrc);
-  equalityCheck(rref, inv);
-}
-
-// Makes the case for a list of matrices
-template <typename ES, typename U, std::size_t M, std::size_t N>
-void makeCaseList(ES const &es, double (&src)[M][N][N], double (&ref)[M][N][N])
-{
-  auto rsrc = viewHostRef3<U, M, N>(src);
-  auto rref = viewHostRef3<U, M, N>(ref);
-  auto inv = makePseudoInverseList<ES, U, M, N>(es, rsrc);
-  equalityCheck(rref, inv);
+  ARBORX_MDVIEW_TEST(ref, inv, Kokkos::Experimental::epsilon_v<float>);
 }
 
 // Pseudo-inverses were computed using numpy's "linalg.pinv" solver and
@@ -108,23 +75,23 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm2, DeviceType, ARBORX_DEVICE_TYPES)
 
   double mat0[2][2] = {{1, 2}, {2, 3}};
   double inv0[2][2] = {{-3, 2}, {2, -1}};
-  makeCase<ExecutionSpace, view_t, 2>(space, mat0, inv0);
+  makeCase<ExecutionSpace, view_t, double[2][2]>(space, 0, mat0, inv0, 2);
 
   double mat1[2][2] = {{4, 0}, {0, 4}};
   double inv1[2][2] = {{1 / 4., 0}, {0, 1 / 4.}};
-  makeCase<ExecutionSpace, view_t, 2>(space, mat1, inv1);
+  makeCase<ExecutionSpace, view_t, double[2][2]>(space, 1, mat1, inv1, 2);
 
   double mat2[2][2] = {{0, 5}, {5, 0}};
   double inv2[2][2] = {{0, 1 / 5.}, {1 / 5., 0}};
-  makeCase<ExecutionSpace, view_t, 2>(space, mat2, inv2);
+  makeCase<ExecutionSpace, view_t, double[2][2]>(space, 2, mat2, inv2, 2);
 
   double mat3[2][2] = {{2, 2}, {2, 2}};
   double inv3[2][2] = {{1 / 8., 1 / 8.}, {1 / 8., 1 / 8.}};
-  makeCase<ExecutionSpace, view_t, 2>(space, mat3, inv3);
+  makeCase<ExecutionSpace, view_t, double[2][2]>(space, 3, mat3, inv3, 2);
 
   double mat4[2][2] = {{1, -3}, {-3, 1}};
   double inv4[2][2] = {{-1 / 8., -3 / 8.}, {-3 / 8., -1 / 8.}};
-  makeCase<ExecutionSpace, view_t, 2>(space, mat4, inv4);
+  makeCase<ExecutionSpace, view_t, double[2][2]>(space, 4, mat4, inv4, 2);
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm3, DeviceType, ARBORX_DEVICE_TYPES)
@@ -138,19 +105,19 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm3, DeviceType, ARBORX_DEVICE_TYPES)
   double inv0[3][3] = {{-1 / 18., 7 / 18., 2 / 18.},
                        {7 / 18., -13 / 18., 4 / 18.},
                        {2 / 18., 4 / 18., -4 / 18.}};
-  makeCase<ExecutionSpace, view_t, 3>(space, mat0, inv0);
+  makeCase<ExecutionSpace, view_t, double[3][3]>(space, 0, mat0, inv0, 3);
 
   double mat1[3][3] = {{0, 1, 2}, {1, 2, 3}, {2, 3, 4}};
   double inv1[3][3] = {{-5 / 6., -1 / 6., 3 / 6.},
                        {-1 / 6., 0, 1 / 6.},
                        {3 / 6., 1 / 6., -1 / 6.}};
-  makeCase<ExecutionSpace, view_t, 3>(space, mat1, inv1);
+  makeCase<ExecutionSpace, view_t, double[3][3]>(space, 1, mat1, inv1, 3);
 
   double mat2[3][3] = {{5, 5, 5}, {5, 5, 5}, {5, 5, 5}};
   double inv2[3][3] = {{1 / 45., 1 / 45., 1 / 45.},
                        {1 / 45., 1 / 45., 1 / 45.},
                        {1 / 45., 1 / 45., 1 / 45.}};
-  makeCase<ExecutionSpace, view_t, 3>(space, mat2, inv2);
+  makeCase<ExecutionSpace, view_t, double[3][3]>(space, 2, mat2, inv2, 3);
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm128, DeviceType,
@@ -172,7 +139,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm128, DeviceType,
       mat[i][j] = -2;
       inv[i][j] = 1 / (128 * 128 * -2.);
     }
-  makeCase<ExecutionSpace, view_t, 128>(space, mat, inv);
+  makeCase<ExecutionSpace, view_t, double[128][128]>(space, 0, mat, inv, 128);
 
   // Case for invertible 128x128 matrix
 }
@@ -187,11 +154,11 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_scalar_like, DeviceType,
 
   double mat0[1][1] = {{2}};
   double inv0[1][1] = {{1 / 2.}};
-  makeCase<ExecutionSpace, view_t, 1>(space, mat0, inv0);
+  makeCase<ExecutionSpace, view_t, double[1][1]>(space, 0, mat0, inv0, 1);
 
   double mat1[1][1] = {{0}};
   double inv1[1][1] = {{0}};
-  makeCase<ExecutionSpace, view_t, 1>(space, mat1, inv1);
+  makeCase<ExecutionSpace, view_t, double[1][1]>(space, 1, mat1, inv1, 1);
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_empty, DeviceType, ARBORX_DEVICE_TYPES)
@@ -224,7 +191,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm2_list, DeviceType,
                          {{0, 1 / 5.}, {1 / 5., 0}},
                          {{1 / 8., 1 / 8.}, {1 / 8., 1 / 8.}},
                          {{-1 / 8., -3 / 8.}, {-3 / 8., -1 / 8.}}};
-  makeCaseList<ExecutionSpace, view_t, 5, 2>(space, mat, inv);
+  makeCase<ExecutionSpace, view_t, double[5][2][2]>(space, 0, mat, inv, 2, 5);
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm3_list, DeviceType,
@@ -247,5 +214,5 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(pseudo_inv_symm3_list, DeviceType,
                          {{1 / 45., 1 / 45., 1 / 45.},
                           {1 / 45., 1 / 45., 1 / 45.},
                           {1 / 45., 1 / 45., 1 / 45.}}};
-  makeCaseList<ExecutionSpace, view_t, 3, 3>(space, mat, inv);
+  makeCase<ExecutionSpace, view_t, double[3][3][3]>(space, 0, mat, inv, 3, 3);
 }
