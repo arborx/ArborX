c8b4302947192f9df3a5c3a2e46c5c6551b7f4e7
Andrey Prokopenko <prokopenkoav@ornl.gov>
2024-02-12 16:34:46 -0500

    Switch to using Kokkos::Experimental::sort_by_key

--- src/cluster/ArborX_Dendrogram.hpp
+++ src/cluster/ArborX_Dendrogram.hpp
@@ -67,7 +67,7 @@ struct Dendrogram
     splitEdges(exec_space, edges, unweighted_edges, _parent_heights);
 
     Kokkos::Profiling::pushRegion("ArborX::Dendrogram::sort_edges");
-    KokkosExt::sortByKey(exec_space, _parent_heights, unweighted_edges);
+    KokkosExt::sort_by_key(exec_space, _parent_heights, unweighted_edges);
     Kokkos::Profiling::popRegion();
 
     using ConstEdges =
