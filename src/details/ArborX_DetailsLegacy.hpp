0bc3cab4736c76f8e3b043c7a50a6cde1fc145e8
Jared Crean <jcrean01@gmail.com>
2024-06-06 20:30:26 -0600

    Work around spurious "missing return statement" warnings for NVCC and Intel compilers (#1102)
    Work around spurious "missing return statement" warning
    
    NVCC < 11.5 and classic Intel < 2021.07 produce spurious warning about
    missing return statement in `else` branch of the `if constexpr`. This
    works around it.
    
    Co-authored-by: Jared Crean <jccrean@sandia.gov>

--- src/details/ArborX_DetailsLegacy.hpp
+++ src/details/ArborX_DetailsLegacy.hpp
@@ -52,6 +52,12 @@ public:
       expand(bounding_volume, Access::get(_primitives, i));
       return value_type{bounding_volume, (index_type)i};
     }
+#if (defined(KOKKOS_COMPILER_NVCC) && (KOKKOS_COMPILER_NVCC < 1150)) ||        \
+    (defined(KOKKOS_COMPILER_INTEL) && (KOKKOS_COMPILER_INTEL <= 2021))
+    // FIXME_NVCC, FIXME_INTEL: workaround for spurios "missing return
+    // statement at end of non-void function" warning
+    return value_type{};
+#endif
   }
 
   KOKKOS_FUNCTION
