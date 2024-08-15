c1c9ec81d6164fb7da3a2b13148d11e45cb5ceef
Andrey Prokopenko <prokopenkoav@ornl.gov>
2024-08-15 17:53:53 -0400

    Add tests

--- test/tstDetailsKokkosExtKernelStdAlgorithms.cpp
+++ test/tstDetailsKokkosExtKernelStdAlgorithms.cpp
@@ -27,6 +27,10 @@
 
 namespace tt = boost::test_tools;
 
+template <typename T>
+using UnmanagedView = Kokkos::View<T *, Kokkos::HostSpace,
+                                   Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+
 BOOST_AUTO_TEST_CASE_TEMPLATE(nth_element, DeviceType, ARBORX_DEVICE_TYPES)
 {
   using ExecutionSpace = typename DeviceType::execution_space;
@@ -41,13 +45,10 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(nth_element, DeviceType, ARBORX_DEVICE_TYPES)
   {
     int const n = v_ref.size();
 
-    Kokkos::View<float *, DeviceType> v("v", n);
-    Kokkos::deep_copy(
-        space, v,
-        Kokkos::View<float *, Kokkos::HostSpace,
-                     Kokkos::MemoryTraits<Kokkos::Unmanaged>>(v_ref.data(), n));
+    Kokkos::View<float *, DeviceType> v("Testing::v", n);
+    Kokkos::deep_copy(space, v, UnmanagedView<float>(v_ref.data(), n));
 
-    Kokkos::View<float *, DeviceType> nth("nth", n);
+    Kokkos::View<float *, DeviceType> nth("Testing::nth", n);
     for (int i = 0; i < n; ++i)
     {
       auto v_copy = ArborX::Details::KokkosExt::clone(space, v);
@@ -64,4 +65,43 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(nth_element, DeviceType, ARBORX_DEVICE_TYPES)
     std::sort(v_ref.begin(), v_ref.end());
     BOOST_TEST(nth_host == v_ref, tt::per_element());
   }
+}
+
+template <typename DeviceType>
+int upperBound(std::vector<float> const &v_host, float x)
+{
+  typename DeviceType::execution_space space;
+
+  auto const n = v_host.size();
+  Kokkos::View<float *, DeviceType> v("Testing::v", n);
+
+  Kokkos::deep_copy(space, v, UnmanagedView<float const>(v_host.data(), n));
+
+  int result;
+  Kokkos::parallel_reduce(
+      Kokkos::RangePolicy(space, 0, 1),
+      KOKKOS_LAMBDA(int, int &update) {
+        update =
+            ArborX::Details::KokkosExt::upper_bound(v.data(), v.data() + n, x) -
+            v.data();
+      },
+      result);
+
+  return result;
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(upper_bound, DeviceType, ARBORX_DEVICE_TYPES)
+{
+  BOOST_TEST(upperBound<DeviceType>({}, 0) == 0);
+  BOOST_TEST(upperBound<DeviceType>({0}, -1) == 0);
+  BOOST_TEST(upperBound<DeviceType>({0}, 0) == 1);
+  BOOST_TEST(upperBound<DeviceType>({0}, 1) == 1);
+  BOOST_TEST(upperBound<DeviceType>({1, 1}, 0) == 0);
+  BOOST_TEST(upperBound<DeviceType>({1, 1}, 1) == 2);
+  BOOST_TEST(upperBound<DeviceType>({1, 3, 5}, 1) == 1);
+  BOOST_TEST(upperBound<DeviceType>({1, 3, 5}, 2) == 1);
+  BOOST_TEST(upperBound<DeviceType>({1, 3, 5}, 3) == 2);
+  BOOST_TEST(upperBound<DeviceType>({1, 3, 5}, 4) == 2);
+  BOOST_TEST(upperBound<DeviceType>({1, 3, 5}, 5) == 3);
+  BOOST_TEST(upperBound<DeviceType>({1, 3, 5, 7, 9, 11, 13}, 8) == 4);
 }
