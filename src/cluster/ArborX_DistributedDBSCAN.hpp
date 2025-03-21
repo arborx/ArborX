db652cdab3007d89c58ffb8779452722ae2f978f
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-03-21 08:52:47 -0400

    Implement proper distributed DBSCAN

--- src/cluster/ArborX_DistributedDBSCAN.hpp
+++ src/cluster/ArborX_DistributedDBSCAN.hpp
@@ -12,8 +12,10 @@
 #ifndef ARBORX_DISTRIBUTED_DBSCAN_HPP
 #define ARBORX_DISTRIBUTED_DBSCAN_HPP
 
+#include "kokkos_ext/ArborX_KokkosExtStdAlgorithms.hpp" // FIXME: remove
 #include <ArborX_DBSCAN.hpp>
 #include <detail/ArborX_AccessTraits.hpp>
+#include <detail/ArborX_DistributedDBSCANHelpers.hpp>
 
 #include <Kokkos_Profiling_ScopedRegion.hpp>
 
@@ -24,12 +26,14 @@ namespace ArborX::Experimental
 
 template <typename ExecutionSpace, typename Primitives, typename Coordinate,
           typename Labels>
-void dbscan(MPI_Comm comm, ExecutionSpace const &exec_space,
+void dbscan(MPI_Comm comm, ExecutionSpace const &space,
             Primitives const &primitives, Coordinate eps, int core_min_size,
             Labels &labels,
-            DBSCAN::Parameters const &parameters = DBSCAN::Parameters())
+            DBSCAN::Parameters const &params = DBSCAN::Parameters())
 {
-  Kokkos::Profiling::ScopedRegion guard("ArborX::DistributedDBSCAN");
+  std::string prefix = "ArborX::DistributedDBSCAN";
+  Kokkos::Profiling::ScopedRegion guard(prefix);
+  prefix += "::";
 
   namespace KokkosExt = ArborX::Details::KokkosExt;
 
@@ -42,6 +46,8 @@ void dbscan(MPI_Comm comm, ExecutionSpace const &exec_space,
 
   ARBORX_ASSERT(eps > 0);
   ARBORX_ASSERT(core_min_size >= 2);
+  if (core_min_size > 2)
+    Kokkos::abort("minPts > 2 is not supported yet");
 
   using Point = typename Points::value_type;
   static_assert(GeometryTraits::is_point_v<Point>);
@@ -50,82 +56,108 @@ void dbscan(MPI_Comm comm, ExecutionSpace const &exec_space,
                      Coordinate>);
 
   Points points{primitives}; // NOLINT
-  auto const n = points.size();
+  int const n_local = points.size();
 
-  int comm_size;
-  MPI_Comm_size(comm, &comm_size);
   int comm_rank;
   MPI_Comm_rank(comm, &comm_rank);
 
-  std::vector<int> counts(comm_size);
-  counts[comm_rank] = n;
-  MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, counts.data(), 1, MPI_INT,
-                comm);
-  std::vector<int> offsets(comm_size + 1);
-  offsets[0] = 0;
-  for (int i = 0; i < comm_size; ++i)
-    offsets[i + 1] = counts[i] + offsets[i];
+  // Step 1: receive ghost neighbors
+  Kokkos::View<int *, MemorySpace> ghost_ids(prefix + "ghost_ids", 0);
+  Kokkos::View<Point *, MemorySpace> ghost_points(prefix + "ghost_points", 0);
+  Kokkos::View<int *, MemorySpace> ghost_ranks(prefix + "ghost_ranks", 0);
+  Details::forwardNeighbors(comm, space, points, eps, ghost_points, ghost_ids,
+                            ghost_ranks);
+  int const n_ghost = ghost_points.size();
 
-  auto const total_n = offsets.back();
-  auto rank_offset = offsets[comm_rank];
-  auto slice = Kokkos::make_pair(offsets[comm_rank], offsets[comm_rank + 1]);
+  // Step 2: do local DBSCAN
+  auto local_labels =
+      dbscan(space,
+             Details::UnifiedPoints<Points, decltype(ghost_points)>{
+                 points, ghost_points},
+             eps, core_min_size, params);
 
-#ifdef ARBORX_ENABLE_GPU_AWARE_MPI
-  Kokkos::View<Point *, MemorySpace> send_data(
-      Kokkos::view_alloc(exec_space, Kokkos::WithoutInitializing,
-                         "ArborX::DistributedDBSCAN::send_data"),
-      total_n);
-#else
-  Kokkos::View<Point *, Kokkos::HostSpace> send_data(
-      Kokkos::view_alloc(Kokkos::DefaultHostExecutionSpace{},
-                         Kokkos::WithoutInitializing,
-                         "ArborX::DistributedDBSCAN::send_data"),
-      total_n);
-#endif
-
-  if constexpr (Kokkos::is_view_v<Primitives>)
+  // Step 3: convert local labels to global
+  Kokkos::View<long long *, MemorySpace> rank_offsets(prefix + "rank_offsets",
+                                                      0);
   {
-    Kokkos::deep_copy(exec_space, Kokkos::subview(send_data, slice),
-                      primitives);
+    std::vector<int> counts;
+    std::vector<long long> offsets;
+    Details::computeCountsAndOffsets(comm, (long long)n_local, counts, offsets);
+
+    Kokkos::resize(Kokkos::view_alloc(space, Kokkos::WithoutInitializing),
+                   rank_offsets, offsets.size());
+    Kokkos::deep_copy(
+        space, rank_offsets,
+        Kokkos::View<long long *, Kokkos::HostSpace, Kokkos::MemoryUnmanaged>(
+            offsets.data(), offsets.size()));
   }
-  else
-  {
-#ifdef ARBORX_ENABLE_GPU_AWARE_MPI
-    Kokkos::parallel_for(
-        "ArborX::DistributedDBSCAN::fill_data",
-        Kokkos::RangePolicy(exec_space, 0, n),
-        KOKKOS_LAMBDA(int i) { send_data(rank_offset + i) = points(i); });
-#else
-    Kokkos::View<Point *, MemorySpace> data(
-        Kokkos::view_alloc(exec_space, Kokkos::WithoutInitializing,
-                           "ArborX::DistributedDBSCAN::data"),
-        n);
-    Kokkos::parallel_for(
-        "ArborX::DistributedDBSCAN::fill_data",
-        Kokkos::RangePolicy(exec_space, 0, n),
-        KOKKOS_LAMBDA(int i) { data(i) = points(i); });
-    Kokkos::deep_copy(exec_space, Kokkos::subview(send_data, slice), data);
-#endif
-  }
-  exec_space.fence("ArborX::DistributedDBSCAN (data ready before sending)");
-
-  auto const sc = sizeof(Point);
-  std::for_each(counts.begin(), counts.end(), [sc](auto &x) { x *= sc; });
-  std::for_each(offsets.begin(), offsets.end(), [sc](auto &x) { x *= sc; });
-  MPI_Allgatherv(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, send_data.data(),
-                 counts.data(), offsets.data(), MPI_BYTE, MPI_COMM_WORLD);
-
-  auto data = Kokkos::create_mirror_view_and_copy(exec_space, send_data);
-  Kokkos::resize(send_data, 0); // deallocate
-
-  auto local_labels = dbscan(exec_space, data, eps, core_min_size, parameters);
-
-  Kokkos::resize(Kokkos::view_alloc(exec_space, Kokkos::WithoutInitializing),
-                 labels, n);
+  Kokkos::resize(Kokkos::view_alloc(space, Kokkos::WithoutInitializing), labels,
+                 local_labels.size());
   Kokkos::parallel_for(
-      "ArborX::DistributedDBSCAN::copy_labels",
-      Kokkos::RangePolicy(exec_space, 0, n),
-      KOKKOS_LAMBDA(int i) { labels(i) = local_labels(rank_offset + i); });
+      prefix + "convert_labels",
+      Kokkos::RangePolicy(space, 0, local_labels.size()), KOKKOS_LAMBDA(int i) {
+        auto label = local_labels(i);
+        if (label == -1)
+        {
+          labels(i) = -1;
+          return;
+        }
+
+        if (label < n_local)
+          labels(i) = rank_offsets(comm_rank) + label;
+        else
+          labels(i) = rank_offsets(ghost_ranks(label - n_local)) +
+                      ghost_ids(label - n_local);
+      });
+  Kokkos::resize(local_labels, 0); // free space
+
+  // Step 4: pack and communicate results back to owning ranks
+  Kokkos::View<long long *, MemorySpace> ghost_labels(
+      Kokkos::view_alloc(space, Kokkos::WithoutInitializing,
+                         prefix + "ghost_labels"),
+      n_ghost);
+  Kokkos::deep_copy(
+      space, ghost_labels,
+      Kokkos::subview(labels, Kokkos::make_pair(n_local, n_local + n_ghost)));
+  Kokkos::resize(space, labels, n_local);
+  // Avoid communicating noise points
+  int num_compressed;
+  Kokkos::parallel_scan(
+      prefix + "compress", Kokkos::RangePolicy(space, 0, n_ghost),
+      KOKKOS_LAMBDA(int i, int &update, bool is_final) {
+        if (ghost_labels(i) != -1)
+        {
+          if (is_final)
+          {
+            ghost_ranks(update) = ghost_ranks(i);
+            ghost_ids(update) = ghost_ids(i);
+            ghost_labels(update) = ghost_labels(i);
+          }
+          ++update;
+        }
+      },
+      num_compressed);
+  Kokkos::resize(space, ghost_ranks, num_compressed);
+  Kokkos::resize(space, ghost_ids, num_compressed);
+  Kokkos::resize(space, ghost_labels, num_compressed);
+  Details::communicateNeighborDataBack(comm, space, ghost_ranks, ghost_ids,
+                                       ghost_labels);
+
+  // Step 5: process multi-labeled indices
+  Kokkos::View<Details::MergePair *, MemorySpace> local_merge_pairs(
+      prefix + "local_merge_pairs", 0);
+  Details::computeMergePairs(space, labels, ghost_ids, ghost_labels,
+                             local_merge_pairs);
+  sortAndFilterMergePairs(space, local_merge_pairs);
+
+  // Step 6: communicate merge pairs (all-to-all)
+  Kokkos::View<Details::MergePair *, MemorySpace> global_merge_pairs(
+      prefix + "global_merge_pairs", 0);
+  communicateMergePairs(comm, space, local_merge_pairs, global_merge_pairs);
+  Details::sortAndFilterMergePairs(space, global_merge_pairs);
+
+  // Step 7: flatten the labels
+  Details::relabel(space, global_merge_pairs, labels);
 }
 
 } // namespace ArborX::Experimental
