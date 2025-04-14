3f433728ffeae9988bec46de8df8fcbf98cbc47d
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-04-14 17:51:12 -0400

    Disable CUDA+Clang for now

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -98,12 +98,12 @@ pipeline {
                         }
                     }
                 }
-                stage('CUDA-12.8.0-NVCC') {
+                stage('CUDA-12.8.1-NVCC') {
                     agent {
                         dockerfile {
                             filename "Dockerfile.nvcc"
                             dir "docker"
-                            additionalBuildArgs '--build-arg BASE=nvidia/cuda:12.8.0-devel-ubuntu22.04 --build-arg KOKKOS_VERSION=4.6.00 --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_EXTENSIONS=OFF -DKokkos_ENABLE_SERIAL=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON"'
+                            additionalBuildArgs '--build-arg BASE=nvidia/cuda:12.8.1-devel-ubuntu24.04 --build-arg KOKKOS_VERSION=4.6.00 --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_EXTENSIONS=OFF -DKokkos_ENABLE_SERIAL=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON"'
                             args '-v /tmp/ccache:/tmp/ccache --env NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES}'
                             label 'NVIDIA_Tesla_V100-PCIE-32GB && nvidia-docker'
                         }
@@ -158,70 +158,70 @@ pipeline {
                         }
                     }
                 }
-                stage('CUDA-12.5.1-Clang') {
-                    agent {
-                        dockerfile {
-                            filename "Dockerfile"
-                            dir "docker"
-                            additionalBuildArgs '--build-arg BASE=nvidia/cuda:12.5.1-devel-ubuntu24.04 --build-arg KOKKOS_VERSION="4.5.00" --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_EXTENSIONS=OFF -DKokkos_ENABLE_THREADS=ON -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu"'
-                            args '-v /tmp/ccache:/tmp/ccache --env NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES}'
-                            label 'NVIDIA_Tesla_V100-PCIE-32GB && nvidia-docker'
-                        }
-                    }
-                    steps {
-                        sh 'ccache --zero-stats'
-                        sh 'rm -rf build && mkdir -p build'
-                        dir('build') {
-                            sh '''
-                                cmake \
-                                    -D CMAKE_INSTALL_PREFIX=$ARBORX_DIR \
-                                    -D CMAKE_BUILD_TYPE=Debug \
-                                    -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
-                                    -D CMAKE_CXX_COMPILER=clang++ \
-                                    -D CMAKE_CXX_EXTENSIONS=OFF \
-                                    -D CMAKE_CXX_FLAGS="-Wpedantic -Wall -Wextra" \
-                                    -D CMAKE_PREFIX_PATH="$KOKKOS_DIR;$BOOST_DIR;$BENCHMARK_DIR" \
-                                    -D ARBORX_ENABLE_MPI=OFF \
-                                    -D ARBORX_ENABLE_TESTS=ON \
-                                    -D ARBORX_ENABLE_EXAMPLES=ON \
-                                    -D ARBORX_ENABLE_BENCHMARKS=ON \
-                                ..
-                            '''
-                            sh 'make -j8 VERBOSE=1'
-                            sh 'ctest $CTEST_OPTIONS'
-                        }
-                    }
-                    post {
-                        always {
-                            sh 'ccache --show-stats'
-                            xunit reduceLog: false, tools:[CTest(deleteOutputFiles: true, failIfNotNew: true, pattern: 'build/Testing/**/Test.xml', skipNoTestFiles: false, stopProcessingIfError: true)]
-                        }
-                        success {
-                            sh 'cd build && make install'
-                            sh 'rm -rf test_install && mkdir -p test_install'
-                            dir('test_install') {
-                                sh 'cp -r ../examples .'
-                                sh '''
-                                    cmake \
-                                        -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
-                                        -D CMAKE_CXX_COMPILER=clang++ \
-                                        -D CMAKE_CXX_EXTENSIONS=OFF \
-                                        -D CMAKE_PREFIX_PATH="$KOKKOS_DIR;$ARBORX_DIR" \
-                                    examples \
-                                '''
-                                sh 'make VERBOSE=1'
-                                sh 'make test'
-                            }
-                        }
-                    }
-                }
+                // stage('CUDA-12.8.1-Clang') {
+                    // agent {
+                        // dockerfile {
+                            // filename "Dockerfile"
+                            // dir "docker"
+                            // additionalBuildArgs '--build-arg BASE=nvidia/cuda:12.8.1-devel-ubuntu24.04 --build-arg ADDITIONAL_PACKAGES="clang-19" --build-arg KOKKOS_VERSION="4.5.00" --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_COMPILER=clang++-19 -DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_EXTENSIONS=OFF -DKokkos_ENABLE_THREADS=ON -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu"'
+                            // args '-v /tmp/ccache:/tmp/ccache --env NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES}'
+                            // label 'NVIDIA_Tesla_V100-PCIE-32GB && nvidia-docker'
+                        // }
+                    // }
+                    // steps {
+                        // sh 'ccache --zero-stats'
+                        // sh 'rm -rf build && mkdir -p build'
+                        // dir('build') {
+                            // sh '''
+                                // cmake \
+                                    // -D CMAKE_INSTALL_PREFIX=$ARBORX_DIR \
+                                    // -D CMAKE_BUILD_TYPE=Debug \
+                                    // -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
+                                    // -D CMAKE_CXX_COMPILER=clang++-19 \
+                                    // -D CMAKE_CXX_EXTENSIONS=OFF \
+                                    // -D CMAKE_CXX_FLAGS="-Wpedantic -Wall -Wextra" \
+                                    // -D CMAKE_PREFIX_PATH="$KOKKOS_DIR;$BOOST_DIR;$BENCHMARK_DIR" \
+                                    // -D ARBORX_ENABLE_MPI=OFF \
+                                    // -D ARBORX_ENABLE_TESTS=ON \
+                                    // -D ARBORX_ENABLE_EXAMPLES=ON \
+                                    // -D ARBORX_ENABLE_BENCHMARKS=ON \
+                                // ..
+                            // '''
+                            // sh 'make -j8 VERBOSE=1'
+                            // sh 'ctest $CTEST_OPTIONS'
+                        // }
+                    // }
+                    // post {
+                        // always {
+                            // sh 'ccache --show-stats'
+                            // xunit reduceLog: false, tools:[CTest(deleteOutputFiles: true, failIfNotNew: true, pattern: 'build/Testing/**/Test.xml', skipNoTestFiles: false, stopProcessingIfError: true)]
+                        // }
+                        // success {
+                            // sh 'cd build && make install'
+                            // sh 'rm -rf test_install && mkdir -p test_install'
+                            // dir('test_install') {
+                                // sh 'cp -r ../examples .'
+                                // sh '''
+                                    // cmake \
+                                        // -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
+                                        // -D CMAKE_CXX_COMPILER=clang++-19 \
+                                        // -D CMAKE_CXX_EXTENSIONS=OFF \
+                                        // -D CMAKE_PREFIX_PATH="$KOKKOS_DIR;$ARBORX_DIR" \
+                                    // examples \
+                                // '''
+                                // sh 'make VERBOSE=1'
+                                // sh 'make test'
+                            // }
+                        // }
+                    // }
+                // }
 
                 stage('Clang') {
                     agent {
                         dockerfile {
                             filename "Dockerfile"
                             dir "docker"
-                            additionalBuildArgs '--build-arg BASE=ubuntu:22.04 --build-arg KOKKOS_VERSION=4.6.00 --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_STANDARD=20 -DKokkos_ENABLE_OPENMP=ON -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu"'
+                            additionalBuildArgs '--build-arg BASE=ubuntu:22.04 --build-arg ADDITIONAL_PACKAGES="clang clang-tidy" --build-arg KOKKOS_VERSION=4.6.00 --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_STANDARD=20 -DKokkos_ENABLE_OPENMP=ON -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu"'
                             args '-v /tmp/ccache:/tmp/ccache'
                             label 'docker'
                         }
@@ -281,7 +281,10 @@ pipeline {
                         dockerfile {
                             filename "Dockerfile"
                             dir "docker"
-                            additionalBuildArgs '--build-arg BASE=gcc:13.3 --build-arg KOKKOS_VERSION=4.6.00 --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_EXTENSIONS=OFF -DKokkos_ENABLE_OPENMP=ON -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu"'
+                            // We use gfortran- to avoid problems with openmpi
+                            // when using gcc images. See
+                            // https://github.com/docker-library/gcc/issues/57
+                            additionalBuildArgs '--build-arg BASE=gcc:13.3 --build-arg ADDITIONAL_PACKAGES="gfortran-" --build-arg KOKKOS_VERSION=4.6.00 --build-arg KOKKOS_OPTIONS="-DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_EXTENSIONS=OFF -DKokkos_ENABLE_OPENMP=ON -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu"'
                             args '-v /tmp/ccache:/tmp/ccache'
                             label 'docker'
                         }
