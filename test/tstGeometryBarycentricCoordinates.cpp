4aa79a93ea9aec0e914476bd2327aa08d2e71c9e
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-06-16 16:25:37 -0400

    Test barycentricCoordinate() for both float and double

--- test/tstGeometryBarycentricCoordinates.cpp
+++ test/tstGeometryBarycentricCoordinates.cpp
@@ -22,53 +22,56 @@
 
 namespace tt = boost::test_tools;
 
-template <int N>
-using Array = Kokkos::Array<float, N>;
+using CoordinatesList = std::tuple<float, double>;
 
-BOOST_AUTO_TEST_CASE(barycentric_triangle)
+BOOST_AUTO_TEST_CASE_TEMPLATE(barycentric_triangle, Coordinate, CoordinatesList)
 {
-  using ArborX::Point;
-  using ArborX::Triangle;
   using ArborX::Experimental::barycentricCoordinates;
+  using Point = ArborX::Point<2, Coordinate>;
+  using Triangle = ArborX::Triangle<2, Coordinate>;
+  using Array = Kokkos::Array<Coordinate, 3>;
 
-  Triangle<2> tri{{-1, -1}, {1, -1}, {-1, 1}};
+  Triangle tri{{-1, -1}, {1, -1}, {-1, 1}};
   // clang-format off
   // vertices
-  BOOST_TEST(barycentricCoordinates(tri, Point{-1.f, -1.f}) == (Array<3>{1, 0, 0}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tri, Point{1.f, -1.f}) == (Array<3>{0, 1, 0}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tri, Point{-1.f, 1.f}) == (Array<3>{0, 0, 1}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{-1, -1}) == (Array{1, 0, 0}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{1, -1}) == (Array{0, 1, 0}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{-1, 1}) == (Array{0, 0, 1}), tt::per_element());
   // mid edges
-  BOOST_TEST(barycentricCoordinates(tri, Point{0.f, -1.f}) == (Array<3>{0.5f, 0.5f, 0}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tri, Point{-1.f, 0.f}) == (Array<3>{0.5f, 0, 0.5f}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tri, Point{0.f, 0.f}) == (Array<3>{0, 0.5f, 0.5f}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{0, -1}) == (Array{0.5, 0.5, 0}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{-1, 0}) == (Array{0.5, 0, 0.5}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{0, 0}) == (Array{0, 0.5, 0.5}), tt::per_element());
   // center
-  BOOST_TEST(barycentricCoordinates(tri, Point{-1.f/3, -1.f/3}) == (Array<3>{1.f/3, 1.f/3, 1.f/3}), tt::tolerance(1e-7f) << tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{-1./3, -1./3}) == (Array{1./3, 1./3, 1./3}), tt::tolerance((Coordinate)1e-7) << tt::per_element());
   // off-center
-  BOOST_TEST(barycentricCoordinates(tri, Point{-0.4f, 0.2f}) == (Array<3>{0.1f, 0.3f, 0.6f}), tt::tolerance(1e-6f) << tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tri, Point{-0.4, 0.2}) == (Array{0.1, 0.3, 0.6}), tt::tolerance((Coordinate)1e-6) << tt::per_element());
   // clang-format on
 }
 
-BOOST_AUTO_TEST_CASE(barycentric_tetrahedron)
+BOOST_AUTO_TEST_CASE_TEMPLATE(barycentric_tetrahedron, Coordinate,
+                              CoordinatesList)
 {
-  using ArborX::Point;
   using ArborX::Experimental::barycentricCoordinates;
-  using ArborX::ExperimentalHyperGeometry::Tetrahedron;
+  using Point = ArborX::Point<3, Coordinate>;
+  using Tetrahedron =
+      ArborX::ExperimentalHyperGeometry::Tetrahedron<Coordinate>;
+  using Array = Kokkos::Array<Coordinate, 4>;
 
-  Tetrahedron<> tet{{0, 0, 0}, {0, 2, 0}, {2, 0, 0}, {0, 0, 2}};
+  Tetrahedron tet{{0, 0, 0}, {0, 2, 0}, {2, 0, 0}, {0, 0, 2}};
   // clang-format off
   // vertices
-  BOOST_TEST(barycentricCoordinates(tet, Point{0.f, 0.f, 0.f}) == (Array<4>{1, 0, 0, 0}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tet, Point{0.f, 2.f, 0.f}) == (Array<4>{0, 1, 0, 0}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tet, Point{2.f, 0.f, 0.f}) == (Array<4>{0, 0, 1, 0}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tet, Point{0.f, 0.f, 2.f}) == (Array<4>{0, 0, 0, 1}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{0, 0, 0}) == (Array{1, 0, 0, 0}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{0, 2, 0}) == (Array{0, 1, 0, 0}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{2, 0, 0}) == (Array{0, 0, 1, 0}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{0, 0, 2}) == (Array{0, 0, 0, 1}), tt::per_element());
   // (some) mid edges
-  BOOST_TEST(barycentricCoordinates(tet, Point{0.f, 1.f, 0.f}) == (Array<4>{0.5f, 0.5f, 0, 0}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tet, Point{0.f, 1.f, 1.f}) == (Array<4>{0, 0.5f, 0, 0.5f}), tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tet, Point{1.f, 0.f, 1.f}) == (Array<4>{0, 0, 0.5f, 0.5f}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{0, 1, 0}) == (Array{0.5, 0.5, 0, 0}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{0, 1, 1}) == (Array{0, 0.5, 0, 0.5}), tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{1, 0, 1}) == (Array{0, 0, 0.5, 0.5}), tt::per_element());
   // (some) mid faces
-  BOOST_TEST(barycentricCoordinates(tet, Point{2.f/3, 2.f/3, 0.f}) == (Array<4>{1.f/3, 1.f/3, 1.f/3, 0}), tt::tolerance(1e-6f) << tt::per_element());
-  BOOST_TEST(barycentricCoordinates(tet, Point{2.f/3, 2.f/3, 2.f/3}) == (Array<4>{0, 1.f/3, 1.f/3, 1.f/3}), tt::tolerance(1e-7f) << tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{2./3, 2./3, 0}) == (Array{1./3, 1./3, 1./3, 0}), tt::tolerance((Coordinate)1e-6) << tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{2./3, 2./3, 2./3}) == (Array{0, 1./3, 1./3, 1./3}), tt::tolerance((Coordinate)1e-7) << tt::per_element());
   // center
-  BOOST_TEST(barycentricCoordinates(tet, Point{0.5f, 0.5f, 0.5f}) == (Array<4>{0.25f, 0.25f, 0.25f, 0.25f}), tt::tolerance(1e-7f) << tt::per_element());
+  BOOST_TEST(barycentricCoordinates(tet, Point{0.5, 0.5, 0.5}) == (Array{0.25, 0.25, 0.25, 0.25}), tt::tolerance((Coordinate)1e-7) << tt::per_element());
   // clang-format on
 }
