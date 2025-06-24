73006e7fbcab90cff8a370dd0acd8517f7044865
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-06-24 10:19:41 -0400

    Add Iota header

---
+++ src/spatial/detail/ArborX_Iota.hpp
@@ -0,0 +1,61 @@
+/****************************************************************************
+ * Copyright (c) 2025, ArborX authors                                       *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ArborX library. ArborX is                       *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef ARBORX_IOTA_HPP
+#define ARBORX_IOTA_HPP
+
+#include "ArborX_AccessTraits.hpp"
+
+#include <Kokkos_Core.hpp>
+
+namespace ArborX
+{
+namespace Details
+{
+
+template <typename MemorySpace>
+struct Iota
+{
+  static_assert(Kokkos::is_memory_space_v<MemorySpace>);
+  using memory_space = MemorySpace;
+
+  KOKKOS_FUNCTION
+  Iota(size_t n)
+      : _n(n)
+  {}
+
+  KOKKOS_FUNCTION
+  auto size() const { return _n; }
+
+  template <std::integral T>
+  KOKKOS_FUNCTION T operator()(T i) const
+  {
+    return i;
+  }
+
+  size_t _n;
+};
+
+} // namespace Details
+
+template <typename MemorySpace>
+struct AccessTraits<Details::Iota<MemorySpace>>
+{
+  using Self = Details::Iota<MemorySpace>;
+
+  using memory_space = typename Self::memory_space;
+  static KOKKOS_FUNCTION size_t size(Self const &self) { return self._n; }
+  static KOKKOS_FUNCTION auto get(Self const &, int i) { return i; }
+};
+
+} // namespace ArborX
+
+#endif
