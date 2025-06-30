431c6264eec27c28cc16defced024337ad23209a
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-06-30 14:09:59 -0400

    Update minimum clang version for aggregate initialization guide

--- src/spatial/detail/ArborX_IndexableGetter.hpp
+++ src/spatial/detail/ArborX_IndexableGetter.hpp
@@ -77,10 +77,11 @@ struct Indexables
   KOKKOS_FUNCTION auto size() const { return _values.size(); }
 };
 
-// FIXME_CLANG(Clang<17): Clang 16 or earlier does not support aggregate
-// initialization type deduction
+// FIXME_CLANG(Clang<=17): Clang 16 and some Clang 17 based compilers do not
+// support aggregate initialization type deduction
 // https://github.com/llvm/llvm-project/issues/54050
-#if defined(__clang__) && (__clang_major__ < 17)
+// Reproducer: https://godbolt.org/z/nEdjb5rP4
+#if defined(__clang__) && KOKKOS_COMPILER_CLANG < 17001
 template <typename Values, typename IndexableGetter>
 KOKKOS_DEDUCTION_GUIDE Indexables(Values, IndexableGetter)
     -> Indexables<Values, IndexableGetter>;
