9f762c1caf5cc7063c069a3ffe214a3586afd786
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-11-06 14:25:56 -0500

    Rename variables

--- src/misc/ArborX_SymmetricSVD.hpp
+++ src/misc/ArborX_SymmetricSVD.hpp
@@ -23,23 +23,23 @@ namespace ArborX::Details
 
 template <typename Matrix>
 KOKKOS_INLINE_FUNCTION void
-ensureIsSquareMatrix([[maybe_unused]] Matrix const &mat)
+ensureIsSquareMatrix([[maybe_unused]] Matrix const &A)
 {
   static_assert(Kokkos::is_view_v<Matrix>, "Matrix must be a view");
   static_assert(Matrix::rank() == 2, "Matrix must be 2D");
-  KOKKOS_ASSERT(mat.extent(0) == mat.extent(1));
+  KOKKOS_ASSERT(A.extent(0) == A.extent(1));
 }
 
 template <typename Matrix>
-KOKKOS_INLINE_FUNCTION void ensureIsSquareSymmetricMatrix(Matrix const &mat)
+KOKKOS_INLINE_FUNCTION void ensureIsSquareSymmetricMatrix(Matrix const &A)
 {
-  ensureIsSquareMatrix(mat);
+  ensureIsSquareMatrix(A);
 
   [[maybe_unused]] auto is_symmetric = [&]() {
-    int const size = mat.extent(0);
-    for (int i = 0; i < size; i++)
-      for (int j = i + 1; j < size; j++)
-        if (mat(i, j) != mat(j, i))
+    int const n = A.extent(0);
+    for (int i = 0; i < n; i++)
+      for (int j = i + 1; j < n; j++)
+        if (A(i, j) != A(j, i))
           return false;
     return true;
   };
@@ -49,9 +49,9 @@ KOKKOS_INLINE_FUNCTION void ensureIsSquareSymmetricMatrix(Matrix const &mat)
 
 // Gets the argmax from the upper triangle part of a matrix
 template <typename Matrix>
-KOKKOS_FUNCTION auto argmaxUpperTriangle(Matrix const &mat)
+KOKKOS_FUNCTION auto argmaxUpperTriangle(Matrix const &A)
 {
-  ensureIsSquareMatrix(mat);
+  ensureIsSquareMatrix(A);
   using Value = typename Matrix::non_const_value_type;
 
   struct
@@ -61,11 +61,11 @@ KOKKOS_FUNCTION auto argmaxUpperTriangle(Matrix const &mat)
     int col = 0;
   } result;
 
-  int const size = mat.extent(0);
-  for (int i = 0; i < size; i++)
-    for (int j = i + 1; j < size; j++)
+  int const n = A.extent(0);
+  for (int i = 0; i < n; i++)
+    for (int j = i + 1; j < n; j++)
     {
-      Value val = Kokkos::abs(mat(i, j));
+      Value val = Kokkos::abs(A(i, j));
       if (result.max < val)
       {
         result.max = val;
@@ -119,43 +119,43 @@ KOKKOS_FUNCTION void svd2x2(Value a, Value b, Value c, Value &x, Value &y,
 // a sign matrix (only 1 or -1 on the diagonal, 0 elsewhere).
 // Thus A = U.ES.U^T.
 //
-// mat <=> initial ES
-// diag <=> final ES
-// unit <=> U
-template <typename Matrix, typename Diag, typename Unit>
-KOKKOS_FUNCTION void symmetricSVDKernel(Matrix &mat, Diag &diag, Unit &unit)
+// A <=> initial ES
+// D <=> final ES
+// U <=> U
+template <typename Matrix, typename Diagonal, typename Unitary>
+KOKKOS_FUNCTION void symmetricSVDKernel(Matrix &A, Diagonal &D, Unitary &U)
 {
-  ensureIsSquareSymmetricMatrix(mat);
+  ensureIsSquareSymmetricMatrix(A);
   static_assert(!std::is_const_v<typename Matrix::value_type>,
-                "mat must be writable");
+                "A must be writable");
 
-  static_assert(Kokkos::is_view_v<Diag>, "diag must be a view");
-  static_assert(Diag::rank() == 1, "diag must be 1D");
-  static_assert(!std::is_const_v<typename Diag::value_type>,
-                "diag must be writable");
+  static_assert(Kokkos::is_view_v<Diagonal>, "D must be a view");
+  static_assert(Diagonal::rank() == 1, "D must be 1D");
+  static_assert(!std::is_const_v<typename Diagonal::value_type>,
+                "D must be writable");
 
-  ensureIsSquareMatrix(unit);
-  static_assert(!std::is_const_v<typename Unit::value_type>,
-                "unit must be writable");
-  static_assert(
-      std::is_same_v<typename Matrix::value_type, typename Diag::value_type> &&
-          std::is_same_v<typename Diag::value_type, typename Unit::value_type>,
-      "All input matrices must have the same value type");
-  KOKKOS_ASSERT(mat.extent(0) == diag.extent(0) &&
-                diag.extent(0) == unit.extent(0));
+  ensureIsSquareMatrix(U);
+  static_assert(!std::is_const_v<typename Unitary::value_type>,
+                "U must be writable");
+  static_assert(std::is_same_v<typename Matrix::value_type,
+                               typename Diagonal::value_type> &&
+                    std::is_same_v<typename Diagonal::value_type,
+                                   typename Unitary::value_type>,
+                "All input matrices must have the same value type");
+  KOKKOS_ASSERT(A.extent(0) == D.extent(0) && D.extent(0) == U.extent(0));
   using Value = typename Matrix::non_const_value_type;
-  int const size = mat.extent(0);
+  int const n = A.extent(0);
 
-  // We first initialize 'unit' as the identity matrix
-  for (int i = 0; i < size; i++)
-    for (int j = 0; j < size; j++)
-      unit(i, j) = Value(i == j);
+  // We first initialize 'U' as the identity matrix
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      U(i, j) = Value(i == j);
 
   static constexpr auto epsilon = Kokkos::Experimental::epsilon_v<Value>;
   while (true)
   {
     // We have a guarantee that p < q
-    auto const [max_val, p, q] = argmaxUpperTriangle(mat);
+    auto const [max_val, p, q] = argmaxUpperTriangle(A);
     if (max_val <= epsilon)
       break;
 
@@ -163,51 +163,51 @@ KOKKOS_FUNCTION void symmetricSVDKernel(Matrix &mat, Diag &diag, Unit &unit)
     Value sin_theta;
     Value x;
     Value y;
-    svd2x2(mat(p, p), mat(p, q), mat(q, q), x, y, cos_theta, sin_theta);
+    svd2x2(A(p, p), A(p, q), A(q, q), x, y, cos_theta, sin_theta);
 
-    // Now let's compute the following new values for 'unit' and 'mat'
-    // mat  <- R'(theta)^T . mat . R'(theta)
-    // unit <- unit . R'(theta)
+    // Now let's compute the following new values for 'U' and 'A'
+    // A  <- R'(theta)^T . A . R'(theta)
+    // U <- U . R'(theta)
 
-    // R'(theta)^T . mat . R'(theta)
+    // R'(theta)^T . A . R'(theta)
     for (int i = 0; i < p; i++)
     {
-      auto const es_ip = mat(i, p);
-      auto const es_iq = mat(i, q);
-      mat(i, p) = cos_theta * es_ip + sin_theta * es_iq;
-      mat(i, q) = -sin_theta * es_ip + cos_theta * es_iq;
+      auto const es_ip = A(i, p);
+      auto const es_iq = A(i, q);
+      A(i, p) = cos_theta * es_ip + sin_theta * es_iq;
+      A(i, q) = -sin_theta * es_ip + cos_theta * es_iq;
     }
-    mat(p, p) = x;
-    mat(p, q) = 0;
+    A(p, p) = x;
+    A(p, q) = 0;
     for (int i = p + 1; i < q; i++)
     {
-      auto const es_pi = mat(p, i);
-      auto const es_iq = mat(i, q);
-      mat(p, i) = cos_theta * es_pi + sin_theta * es_iq;
-      mat(i, q) = -sin_theta * es_pi + cos_theta * es_iq;
+      auto const es_pi = A(p, i);
+      auto const es_iq = A(i, q);
+      A(p, i) = cos_theta * es_pi + sin_theta * es_iq;
+      A(i, q) = -sin_theta * es_pi + cos_theta * es_iq;
     }
-    mat(q, q) = y;
-    for (int i = q + 1; i < size; i++)
+    A(q, q) = y;
+    for (int i = q + 1; i < n; i++)
     {
-      auto const es_pi = mat(p, i);
-      auto const es_qi = mat(q, i);
-      mat(p, i) = cos_theta * es_pi + sin_theta * es_qi;
-      mat(q, i) = -sin_theta * es_pi + cos_theta * es_qi;
+      auto const es_pi = A(p, i);
+      auto const es_qi = A(q, i);
+      A(p, i) = cos_theta * es_pi + sin_theta * es_qi;
+      A(q, i) = -sin_theta * es_pi + cos_theta * es_qi;
     }
 
-    // unit . R'(theta)
-    for (int i = 0; i < size; i++)
+    // U . R'(theta)
+    for (int i = 0; i < n; i++)
     {
-      auto const u_ip = unit(i, p);
-      auto const u_iq = unit(i, q);
-      unit(i, p) = cos_theta * u_ip + sin_theta * u_iq;
-      unit(i, q) = -sin_theta * u_ip + cos_theta * u_iq;
+      auto const u_ip = U(i, p);
+      auto const u_iq = U(i, q);
+      U(i, p) = cos_theta * u_ip + sin_theta * u_iq;
+      U(i, q) = -sin_theta * u_ip + cos_theta * u_iq;
     }
   }
 
   // Extract eigenvalues from the diagonal of the resulting A
-  for (int i = 0; i < size; i++)
-    diag(i) = mat(i, i);
+  for (int i = 0; i < n; i++)
+    D(i) = A(i, i);
 }
 
 // Pseudo-inverse of symmetric matrices using SVD
@@ -216,39 +216,39 @@ KOKKOS_FUNCTION void symmetricSVDKernel(Matrix &mat, Diag &diag, Unit &unit)
 // a sign matrix (only 1 or -1 on the diagonal, 0 elsewhere).
 // Thus A = U.ES.U^T and A^-1 = U.[ ES^-1 ].U^T
 //
-// mat <=> initial ES
-// diag <=> final ES
-// unit <=> U
-template <typename Matrix, typename Diag, typename Unit>
-KOKKOS_FUNCTION void symmetricPseudoInverseSVDKernel(Matrix &mat, Diag &diag,
-                                                     Unit &unit)
+// A <=> initial ES
+// D <=> final ES
+// U <=> U
+template <typename Matrix, typename Diagonal, typename Unitary>
+KOKKOS_FUNCTION void symmetricPseudoInverseSVDKernel(Matrix &A, Diagonal &D,
+                                                     Unitary &U)
 {
-  symmetricSVDKernel(mat, diag, unit);
+  symmetricSVDKernel(A, D, U);
 
-  int const size = mat.extent(0);
+  int const n = A.extent(0);
 
   using Value = typename Matrix::non_const_value_type;
 
   // Compute the max to get a range of the invertible eigenvalues
   Value max_eigen = 0;
-  for (int i = 0; i < size; i++)
-    max_eigen = Kokkos::max(Kokkos::abs(diag(i)), max_eigen);
+  for (int i = 0; i < n; i++)
+    max_eigen = Kokkos::max(Kokkos::abs(D(i)), max_eigen);
 
   // The standard tolerance for forming pseudo-inverse is to only invert
   // singular values that are max(m,n) * \epsilon * ||A||_2.
   // ||A||_2 is equal to the max singular value (max abs diagonal).
   constexpr auto epsilon = Kokkos::Experimental::epsilon_v<Value>;
-  auto const tolerance = size * max_eigen * epsilon;
-  for (int i = 0; i < size; i++)
-    diag(i) = (Kokkos::abs(diag(i)) <= tolerance) ? 0 : 1 / diag(i);
+  auto const tolerance = n * max_eigen * epsilon;
+  for (int i = 0; i < n; i++)
+    D(i) = (Kokkos::abs(D(i)) <= tolerance) ? 0 : 1 / D(i);
 
-  // Then we fill out 'mat' as the pseudo inverse
-  for (int i = 0; i < size; i++)
-    for (int j = 0; j < size; j++)
+  // Then we fill out 'A' as the pseudo inverse
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
     {
-      mat(i, j) = 0;
-      for (int k = 0; k < size; k++)
-        mat(i, j) += unit(i, k) * diag(k) * unit(j, k);
+      A(i, j) = 0;
+      for (int k = 0; k < n; k++)
+        A(i, j) += U(i, k) * D(k) * U(j, k);
     }
 }
 
