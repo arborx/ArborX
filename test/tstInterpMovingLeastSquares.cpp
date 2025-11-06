b94a509d98bdac283c31f944e6011f05a2e50af1
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-11-06 12:40:08 -0500

    Add convergence test
    Co-authored-by: Daniel Arndt <arndtd@ornl.com>

--- test/tstInterpMovingLeastSquares.cpp
+++ test/tstInterpMovingLeastSquares.cpp
@@ -182,3 +182,63 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(moving_least_squares_edge_cases, DeviceType,
   mls1.interpolate(space, srcv1, eval1);
   ARBORX_MDVIEW_TEST_TOL(eval1, tgtv1, Kokkos::Experimental::epsilon_v<float>);
 }
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(moving_least_square_cartesian_convergence,
+                              DeviceType, ARBORX_DEVICE_TYPES)
+{
+  // Test interpolation on a cartesian-type grid where using the minimal number
+  // of neighbors (6 for quadratic polynomials in 2d) wasn't sufficient for an
+  // exact interpolation for a function contained in the ansatz space.
+  using ExecutionSpace = typename DeviceType::execution_space;
+  using MemorySpace = typename DeviceType::memory_space;
+  ExecutionSpace space{};
+
+  using Point = ArborX::Point<2, double>;
+
+  auto f = [](double x, double y) {
+    return Kokkos::sin(4 * x) + Kokkos::sin(2 * y);
+  };
+
+  constexpr int num_targets = 4;
+  Kokkos::View<Point *, MemorySpace> target_coords("target_coords",
+                                                   num_targets);
+  Kokkos::View<double *, MemorySpace> target_values("target_values",
+                                                    num_targets);
+  Kokkos::parallel_for(
+      Kokkos::RangePolicy(space, 0, 1), KOKKOS_LAMBDA(int) {
+        target_coords(0) = {.788675, .788675};
+        target_coords(1) = {.211325, .788675};
+        target_coords(2) = {.788675, .211325};
+        target_coords(3) = {.211325, .211325};
+        for (int i = 0; i < num_targets; ++i)
+          target_values(i) = f(target_coords(i)[0], target_coords(i)[1]);
+      });
+
+  constexpr int num_refinements = 9;
+  constexpr int n = (1 << num_refinements) + 1;
+  constexpr double h = 2. / (n - 1);
+  constexpr int num_sources = n * n;
+
+  std::cout << "h = " << h << std::endl;
+
+  Kokkos::View<Point *, MemorySpace> source_coords("source_coords",
+                                                   num_sources);
+  Kokkos::View<double *, MemorySpace> source_values("source_values",
+                                                    num_sources);
+  Kokkos::parallel_for(
+      Kokkos::MDRangePolicy(space, {0, 0}, {n, n}),
+      KOKKOS_LAMBDA(int const i, int const j) {
+        int index = i * n + j;
+        source_coords(index) = {-1 + i * h, -1 + j * h};
+        source_values(index) =
+            f(source_coords(index)[0], source_coords(index)[1]);
+      });
+
+  ArborX::Interpolation::MovingLeastSquares<MemorySpace, double> mls(
+      space, source_coords, target_coords);
+  Kokkos::View<double *, MemorySpace> interpolated_values("interpolated_values",
+                                                          num_targets);
+  mls.interpolate(space, source_values, interpolated_values);
+
+  ARBORX_MDVIEW_TEST_TOL(interpolated_values, target_values, 2e-7);
+}
