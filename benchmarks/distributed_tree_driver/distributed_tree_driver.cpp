4cc857d8c44fcae6fddf915238344767a7c9d28c
Andrey Prokopenko <prokopenkoav@ornl.gov>
2025-12-03 14:06:13 -0500

    Allow different precisions

--- benchmarks/distributed_tree_driver/distributed_tree_driver.cpp
+++ benchmarks/distributed_tree_driver/distributed_tree_driver.cpp
@@ -25,15 +25,26 @@
 
 #include <mpi.h>
 
-using Point = ArborX::Point<3>;
-
 namespace bpo = boost::program_options;
 
-template <typename MemorySpace>
+struct Parameters
+{
+  int n_values;
+  int n_queries;
+  int n_neighbors;
+  float shift;
+  int partition_dim;
+  bool perform_knn_search;
+  bool perform_radius_search;
+  bool shift_queries;
+};
+
+template <typename Point, typename MemorySpace>
 void buildProblem(MPI_Comm comm, Kokkos::View<Point *, MemorySpace> &values,
                   Kokkos::View<Point *, MemorySpace> &queries,
                   int partition_dim, bool shift_queries, float shift)
 {
+  using Coordinate = ArborX::GeometryTraits::coordinate_type_t<Point>;
   int comm_rank;
   MPI_Comm_rank(comm, &comm_rank);
   int comm_size;
@@ -42,10 +53,10 @@ void buildProblem(MPI_Comm comm, Kokkos::View<Point *, MemorySpace> &values,
   int const n_queries = queries.size();
   int const n_values = values.size();
 
-  float a = 0.;
-  float offset_x = 0.;
-  float offset_y = 0.;
-  float offset_z = 0.;
+  Coordinate a = 0;
+  Coordinate offset_x = 0;
+  Coordinate offset_y = 0;
+  Coordinate offset_z = 0;
   int i_max = 0;
   // Change the geometry of the problem. In 1D, all the point clouds are
   // aligned on a line. In 2D, the point clouds create a board and in 3D,
@@ -89,7 +100,7 @@ void buildProblem(MPI_Comm comm, Kokkos::View<Point *, MemorySpace> &values,
   }
 
   // Generate random points uniformly distributed within a box.
-  std::uniform_real_distribution<float> distribution(-1, 1);
+  std::uniform_real_distribution<Coordinate> distribution(-1, 1);
   std::default_random_engine generator;
   auto random = [&distribution, &generator]() {
     return distribution(generator);
@@ -136,97 +147,25 @@ void buildProblem(MPI_Comm comm, Kokkos::View<Point *, MemorySpace> &values,
   }
 }
 
-int main(int argc, char *argv[])
+template <typename Coordinate>
+void main_(MPI_Comm comm, Parameters const &params)
 {
-  MPI_Init(&argc, &argv);
-
-  MPI_Comm const comm = MPI_COMM_WORLD;
-  int comm_rank;
-  MPI_Comm_rank(comm, &comm_rank);
-  if (comm_rank == 0)
-  {
-    std::cout << "ArborX version: " << ArborX::version() << std::endl;
-    std::cout << "ArborX hash   : " << ArborX::gitCommitHash() << std::endl;
-    std::cout << "Kokkos version: " << ArborX::Details::KokkosExt::version()
-              << std::endl;
-  }
-
-  // Strip "--help" and "--kokkos-help" from the flags passed to Kokkos if we
-  // are not on MPI rank 0 to prevent Kokkos from printing the help message
-  // multiply.
-  if (comm_rank != 0)
-  {
-    auto *help_it = std::find_if(argv, argv + argc, [](std::string const &x) {
-      return x == "--help" || x == "--kokkos-help";
-    });
-    if (help_it != argv + argc)
-    {
-      std::swap(*help_it, *(argv + argc - 1));
-      --argc;
-    }
-  }
-
-  Kokkos::ScopeGuard guard(argc, argv);
-
   using ExecutionSpace = Kokkos::DefaultExecutionSpace;
   using MemorySpace = typename ExecutionSpace::memory_space;
 
-  int n_values;
-  int n_queries;
-  int n_neighbors;
-  float shift;
-  int partition_dim;
+  using Point = ArborX::Point<3, Coordinate>;
 
-  bpo::options_description desc("Allowed options");
-  // clang-format off
-  desc.add_options()
-      ( "help", "produce help message" )
-      ( "values", bpo::value<int>(&n_values)->default_value(20000), "Number of indexable values (source) per MPI rank." )
-      ( "queries", bpo::value<int>(&n_queries)->default_value(5000), "Number of queries (target) per MPI rank." )
-      ( "neighbors", bpo::value<int>(&n_neighbors)->default_value(10), "Desired number of results per query." )
-      ( "shift", bpo::value<float>(&shift)->default_value(1.f), "Shift of the point clouds. '0' means the clouds are built "
-                                                                "at the same place, while '1' places the clouds next to each"
-                                                                "other. Negative values and values larger than one "
-                                                                "mean that the clouds are separated." )
-      ( "partition_dim", bpo::value<int>(&partition_dim)->default_value(3), "Number of dimension used by the partitioning of the global "
-                                                                            "point cloud. 1 -> local clouds are aligned on a line, 2 -> "
-                                                                            "local clouds form a board, 3 -> local clouds form a box." )
-      ( "do-not-perform-knn-search", "skip kNN search" )
-      ( "do-not-perform-radius-search", "skip radius search" )
-      ( "shift-queries" , "By default, points are reused for the queries. Enabling this option shrinks the local box queries are created "
-                          "in to a third of its size and moves it to the center of the global box. The result is a huge imbalance for the "
-                          "number of queries that need to be processed by each processor.")
-      ;
-  // clang-format on
-  bpo::variables_map vm;
-  bpo::store(bpo::command_line_parser(argc, argv).options(desc).run(), vm);
-  bpo::notify(vm);
+  auto const n_values = params.n_values;
+  auto const n_queries = params.n_queries;
+  auto const n_neighbors = params.n_neighbors;
+  auto const shift = params.shift;
+  auto const partition_dim = params.partition_dim;
+  auto const perform_knn_search = params.perform_knn_search;
+  auto const perform_radius_search = params.perform_radius_search;
+  auto const shift_queries = params.shift_queries;
 
-  auto const shift_queries = !vm.count("shift-queries");
-
-  if (vm.count("help") > 0)
-  {
-    if (comm_rank == 0)
-      std::cout << desc << '\n';
-    return 0;
-  }
-
-  auto const perform_knn_search = !vm.count("do-not-perform-knn-search");
-  auto const perform_radius_search = !vm.count("do-not-perform-radius-search");
-
-  if (comm_rank == 0)
-  {
-    std::cout << std::boolalpha;
-    std::cout << "\nRunning with arguments:\n"
-              << "perform knn search      : " << perform_knn_search << '\n'
-              << "perform radius search   : " << perform_radius_search << '\n'
-              << "#points/MPI process     : " << n_values << '\n'
-              << "#queries/MPI process    : " << n_queries << '\n'
-              << "size of shift           : " << shift << '\n'
-              << "dimension               : " << partition_dim << '\n'
-              << "shift-queries           : " << shift_queries << '\n'
-              << '\n';
-  }
+  int comm_rank;
+  MPI_Comm_rank(comm, &comm_rank);
 
   Kokkos::View<Point *, MemorySpace> random_values(
       Kokkos::view_alloc(Kokkos::WithoutInitializing, "Benchmark::values"),
@@ -276,20 +215,20 @@ int main(int argc, char *argv[])
     // Radius is computed so that the number of results per query for a
     // uniformly distributed primitives in a [-a,a]^d box is approximately
     // n_neighbors.
-    float r = 0;
+    Coordinate r = 0;
     switch (partition_dim)
     {
     case 1:
       // Derivation: n_values*(2*r)/(2a) = n_neighbors
-      r = static_cast<float>(n_neighbors);
+      r = static_cast<Coordinate>(n_neighbors);
       break;
     case 2:
       // Derivation: n_values*(pi*r^2)/(2a)^2 = n_neighbors
-      r = std::sqrt(n_neighbors * 4.f / Kokkos::numbers::pi_v<float>);
+      r = std::sqrt(n_neighbors * 4.f / Kokkos::numbers::pi_v<Coordinate>);
       break;
     case 3:
       // Derivation: n_values*(4/3*pi*r^3)/(2a)^3 = n_neighbors
-      r = std::cbrt(n_neighbors * 6.f / Kokkos::numbers::pi_v<float>);
+      r = std::cbrt(n_neighbors * 6.f / Kokkos::numbers::pi_v<Coordinate>);
       break;
     }
 
@@ -306,6 +245,96 @@ int main(int argc, char *argv[])
       os << "radius done\n";
   }
   time_monitor.summarize(comm);
+}
+
+int main(int argc, char *argv[])
+{
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm const comm = MPI_COMM_WORLD;
+  int comm_rank;
+  MPI_Comm_rank(comm, &comm_rank);
+  if (comm_rank == 0)
+  {
+    std::cout << "ArborX version: " << ArborX::version() << std::endl;
+    std::cout << "ArborX hash   : " << ArborX::gitCommitHash() << std::endl;
+    std::cout << "Kokkos version: " << ArborX::Details::KokkosExt::version()
+              << std::endl;
+  }
+
+  // Strip "--help" and "--kokkos-help" from the flags passed to Kokkos if we
+  // are not on MPI rank 0 to prevent Kokkos from printing the help message
+  // multiply.
+  if (comm_rank != 0)
+  {
+    auto *help_it = std::find_if(argv, argv + argc, [](std::string const &x) {
+      return x == "--help" || x == "--kokkos-help";
+    });
+    if (help_it != argv + argc)
+    {
+      std::swap(*help_it, *(argv + argc - 1));
+      --argc;
+    }
+  }
+
+  Parameters params;
+  std::string precision;
+  bpo::options_description desc("Allowed options");
+  // clang-format off
+  desc.add_options()
+      ( "help", "produce help message" )
+      ( "values", bpo::value<int>(&params.n_values)->default_value(20000), "Number of indexable values (source) per MPI rank." )
+      ( "queries", bpo::value<int>(&params.n_queries)->default_value(5000), "Number of queries (target) per MPI rank." )
+      ( "neighbors", bpo::value<int>(&params.n_neighbors)->default_value(10), "Desired number of results per query." )
+      ( "shift", bpo::value<float>(&params.shift)->default_value(1.f), "Shift of the point clouds. '0' means the clouds are built "
+                                                                       "at the same place, while '1' places the clouds next to each"
+                                                                       "other. Negative values and values larger than one "
+                                                                       "mean that the clouds are separated." )
+      ( "partition_dim", bpo::value<int>(&params.partition_dim)->default_value(3), "Number of dimension used by the partitioning of the global "
+                                                                                   "point cloud. 1 -> local clouds are aligned on a line, 2 -> "
+                                                                                   "local clouds form a board, 3 -> local clouds form a box." )
+      ( "precision", bpo::value<std::string>(&precision)->default_value("float"), "Precision (float | double)" )
+      ( "do-not-perform-knn-search", "skip kNN search" )
+      ( "do-not-perform-radius-search", "skip radius search" )
+      ( "shift-queries" , "By default, points are reused for the queries. Enabling this option shrinks the local box queries are created "
+                          "in to a third of its size and moves it to the center of the global box. The result is a huge imbalance for the "
+                          "number of queries that need to be processed by each processor.")
+      ;
+  // clang-format on
+  bpo::variables_map vm;
+  bpo::store(bpo::command_line_parser(argc, argv).options(desc).run(), vm);
+  bpo::notify(vm);
+
+  if (vm.count("help") > 0)
+  {
+    if (comm_rank == 0)
+      std::cout << desc << '\n';
+    return 0;
+  }
+
+  params.perform_knn_search = !vm.count("do-not-perform-knn-search");
+  params.perform_radius_search = !vm.count("do-not-perform-radius-search");
+  params.shift_queries = !vm.count("shift-queries");
+
+  if (comm_rank == 0)
+  {
+    std::cout << std::boolalpha;
+    std::cout << "\nRunning with arguments:"
+              << "\nperform knn search      : " << params.perform_knn_search
+              << "\nperform radius search   : " << params.perform_radius_search
+              << "\n#points/MPI process     : " << params.n_values
+              << "\n#queries/MPI process    : " << params.n_queries
+              << "\nsize of shift           : " << params.shift
+              << "\ndimension               : " << params.partition_dim
+              << "\nshift-queries           : " << params.shift_queries << '\n';
+  }
+
+  Kokkos::ScopeGuard guard(argc, argv);
+
+  if (precision == "float")
+    main_<float>(comm, params);
+  else
+    main_<double>(comm, params);
 
   MPI_Finalize();
 
