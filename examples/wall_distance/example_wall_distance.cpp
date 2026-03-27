3dfa791463a92df235b53189a31c06cfb86c4aa0
Andrey Prokopenko <prokopenkoav@ornl.gov>
2026-03-27 09:51:45 -0400

    Implement STK mesh wall distance calculations

---
+++ examples/wall_distance/example_wall_distance.cpp
@@ -0,0 +1,352 @@
+/****************************************************************************
+ * Copyright (c) 2025, ArborX authors                                       *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ArborX library. ArborX is                       *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#include "../../benchmarks/utils/ArborXBenchmark_TimeMonitor.hpp"
+#include "ArborX_WallDistance.hpp"
+#include <ArborX_Version.hpp>
+
+#include <boost/program_options.hpp>
+
+#include <string>
+#include <vector>
+
+#include "helpers.hpp"
+#include <Panzer_IntegrationRule.hpp>
+#include <Panzer_STK_Interface.hpp>
+#include <mpi.h>
+
+template <int DIM, typename Coordinate, bool ReplicateSides,
+          typename ExecutionSpace, typename WallDistances>
+void main_(MPI_Comm comm, ExecutionSpace const &space,
+           panzer_stk::STK_Interface const &mesh,
+           std::vector<std::string> const &wall_names,
+           std::string const &distance_type,
+           std::vector<std::string> const &block_names,
+           std::vector<panzer::Workset> const &worksets,
+           panzer::IntegrationRule const &ir, WallDistances &wall_distances,
+           bool verbose)
+{
+  using MemorySpace = typename ExecutionSpace::memory_space;
+
+  int comm_rank;
+  MPI_Comm_rank(comm, &comm_rank);
+
+  ArborXBenchmark::TimeMonitor time_monitor;
+
+  auto construction_time = time_monitor.getNewTimer("construction");
+  construction_time->start();
+  ArborX::Experimental::WallDistance<MemorySpace, DIM, Coordinate,
+                                     ReplicateSides>
+      wall_distance(space, mesh, wall_names);
+  space.fence();
+  construction_time->stop();
+
+  if (distance_type == "node")
+  {
+    auto meta = mesh.getMetaData();
+
+    stk::mesh::Selector selector;
+    for (auto const &block_name : block_names)
+      selector |= *meta->get_part(block_name);
+    selector &= meta->locally_owned_part() | meta->globally_shared_part();
+
+    auto query_time = time_monitor.getNewTimer("query");
+    query_time->start();
+    wall_distance.distance(space, mesh, selector, wall_distances);
+    space.fence();
+    query_time->stop();
+  }
+  else if (distance_type == "cell")
+  {
+    int const num_worksets = (worksets).size();
+    int const max_num_cells_per_workset = ir.dl_scalar->extent(0);
+    int const num_int_points_per_cell = ir.dl_scalar->extent(1);
+
+    Kokkos::View<Coordinate ***, MemorySpace> workset_distances(
+        Kokkos::view_alloc(space, Kokkos::WithoutInitializing,
+                           "Example::workset_distances"),
+        num_worksets, max_num_cells_per_workset, num_int_points_per_cell);
+
+    space.fence();
+
+    auto query_time = time_monitor.getNewTimer("query");
+    query_time->start();
+    wall_distance.distance(space, worksets, ir, workset_distances);
+    space.fence();
+    query_time->stop();
+
+    // Copy workset distances to a flat array
+    Kokkos::resize(Kokkos::view_alloc(space, Kokkos::WithoutInitializing),
+                   wall_distances, num_worksets * max_num_cells_per_workset);
+    for (int workset_id = 0; workset_id < num_worksets; ++workset_id)
+    {
+      auto const &workset = (worksets)[workset_id];
+      auto num_cells = workset.num_cells;
+      if (num_cells == 0)
+        continue;
+
+      auto local_cell_ids = workset.getLocalCellIDs();
+      Kokkos::parallel_for(
+          "Example::copy_distances", Kokkos::RangePolicy(space, 0, num_cells),
+          KOKKOS_LAMBDA(int i) {
+            Coordinate avg = 0;
+            for (int p = 0; p < num_int_points_per_cell; ++p)
+              avg += workset_distances(workset_id, i, p);
+            wall_distances(local_cell_ids(i)) = avg / num_int_points_per_cell;
+          });
+    }
+  }
+
+  if (verbose)
+    time_monitor.summarize(comm);
+}
+
+int main(int argc, char *argv[])
+{
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm const comm = MPI_COMM_WORLD;
+  int comm_rank;
+  MPI_Comm_rank(comm, &comm_rank);
+  int comm_size;
+  MPI_Comm_size(comm, &comm_size);
+
+  using Coordinate = double;
+
+  namespace bpo = boost::program_options;
+
+  // Strip "--help" and "--kokkos-help" from the flags passed to Kokkos if
+  // we are not on MPI rank 0 to prevent Kokkos from printing the help
+  // message multiply.
+  auto *help_it = std::find_if(argv, argv + argc, [](std::string const &x) {
+    return x == "--help" || x == "--kokkos-help";
+  });
+  bool is_help_present = (help_it != argv + argc);
+  if (is_help_present && comm_rank != 0)
+  {
+    std::swap(*help_it, *(argv + argc - 1));
+    --argc;
+  }
+
+  Kokkos::initialize(argc, argv);
+
+  std::string basis_type;
+  std::string filename;
+  std::string out_filename;
+  int basis_order;
+  int int_order;
+  std::vector<std::string> block_names;
+  std::vector<std::string> wall_names;
+  std::string distance_type;
+  int restart_index;
+  bool verbose;
+  std::string distance_field_name;
+
+  bpo::options_description desc("Allowed options");
+  // clang-format off
+  desc.add_options()
+    ("help", "help message" )
+    ("basis-order", bpo::value<int>(&basis_order)->default_value(1), "basis order")
+    ("basis-distance_type", bpo::value<std::string>(&basis_type)->default_value("HGrad"), "basis distance_type")
+    ("block-names", bpo::value<std::vector<std::string>>(&block_names)->multitoken(), "block names to compute distances for")
+    ("filename", bpo::value<std::string>(&filename)->default_value("mesh.exo"), "mesh filename")
+    ("field-name", bpo::value<std::string>(&distance_field_name)->default_value("wall_distance"), "wall distance field name in Exodus")
+    ("int-order", bpo::value<int>(&int_order)->default_value(2), "integration order")
+    ("output-filename", bpo::value<std::string>(&out_filename)->default_value("output.exo"), "output filename")
+    ("restart-index", bpo::value<int>(&restart_index)->default_value(-1), "restart index")
+    ("type", bpo::value<std::string>(&distance_type)->default_value("node"), "type of field to write (node or cell)")
+    ("verbose", bpo::bool_switch(&verbose), "verbose")
+    ("wall-names", bpo::value<std::vector<std::string>>(&wall_names)->multitoken(), "names of walls")
+    ;
+  // clang-format on
+  bpo::variables_map vm;
+  bpo::store(bpo::command_line_parser(argc, argv)
+                 .options(desc)
+                 .positional(bpo::positional_options_description{})
+                 .run(),
+             vm);
+  bpo::notify(vm);
+
+  if (comm_rank == 0)
+  {
+    std::cout << "ArborX version    : " << ArborX::version() << std::endl;
+    std::cout << "ArborX hash       : " << ArborX::gitCommitHash() << std::endl;
+    std::cout << "Kokkos version    : " << ArborX::Details::KokkosExt::version()
+              << std::endl;
+    std::cout << "#MPI ranks        : " << comm_size << std::endl;
+  }
+
+  if (is_help_present)
+  {
+    if (comm_rank == 0)
+      std::cout << desc << '\n';
+    Kokkos::finalize();
+    MPI_Finalize();
+    return 0;
+  }
+
+  if (wall_names.empty())
+  {
+    if (comm_rank == 0)
+      std::cerr << "At least one wall name must be provided\n";
+    Kokkos::finalize();
+    MPI_Finalize();
+    return 0;
+  }
+
+  if (distance_type != "node" && distance_type != "cell")
+  {
+    if (comm_rank == 0)
+      std::cerr << "Invalid distance_type: " << distance_type
+                << ". Must be \"node\" or \"cell\".\n";
+    Kokkos::finalize();
+    MPI_Finalize();
+    return 0;
+  }
+
+  if (!check_names(comm, filename, block_names, wall_names))
+  {
+    Kokkos::finalize();
+    MPI_Finalize();
+    return 0;
+  }
+
+  auto vec2string = [](std::vector<std::string> const &names) {
+    if (names.empty())
+      return std::string("(none)");
+    std::string result = names[0];
+    for (size_t i = 1; i < names.size(); ++i)
+      result += ", " + names[i];
+    return result;
+  };
+
+  // Print out the runtime parameters
+  if (comm_rank == 0)
+  {
+    printf("basis order       : %d\n", basis_order);
+    printf("basis type        : %s\n", basis_type.c_str());
+    printf("block names       : %s\n", vec2string(block_names).c_str());
+    printf("field name        : %s\n", distance_field_name.c_str());
+    printf("filename          : %s\n", filename.c_str());
+    printf("integration order : %d\n", int_order);
+    printf("distance type     : %s\n", distance_type.c_str());
+    printf("restart index     : %d%s\n", restart_index,
+           (restart_index == -1 ? " (last)" : ""));
+    printf("verbose           : %s\n", (verbose ? "true" : "false"));
+    printf("wall names        : %s\n", vec2string(wall_names).c_str());
+  }
+
+  constexpr bool ReplicateSides = true;
+
+  {
+    using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+    using MemorySpace = typename ExecutionSpace::memory_space;
+
+    // Note: when running in parallel, use
+    //   IOSS_PROPERTIES="DECOMPOSITION_METHOD=RIB:MAXIMUM_NAME_LENGTH=64"
+    // for automatic mesh decomposition. This requires NetCDF-C compiled
+    // with parallel support.
+    Kokkos::Timer timer;
+    auto mesh = build_mesh(
+        comm, filename, block_names, distance_field_name,
+        (distance_type == "node" ? DistanceType::NODE : DistanceType::CELL),
+        restart_index);
+    if (comm_rank == 0)
+      std::cout << "Mesh construction time: " << timer.seconds()
+                << " seconds\n";
+    std::vector<panzer::Workset> worksets;
+    if (distance_type == "cell")
+    {
+      timer.reset();
+      worksets =
+          build_worksets(mesh, block_names, basis_type, basis_order, int_order);
+      if (comm_rank == 0)
+        std::cout << "Worksets construction time: " << timer.seconds()
+                  << " seconds\n";
+    }
+
+    panzer::CellData cell_data(workset_size,
+                               mesh->getCellTopology(block_names[0]));
+    panzer::IntegrationRule ir(int_order, cell_data);
+
+    ExecutionSpace space;
+
+    Kokkos::View<Coordinate *, MemorySpace> wall_distances;
+    if (mesh->getDimension() == 2)
+      main_<2, Coordinate, ReplicateSides>(comm, space, *mesh, wall_names,
+                                           distance_type, block_names, worksets,
+                                           ir, wall_distances, verbose);
+    else
+      main_<3, Coordinate, ReplicateSides>(comm, space, *mesh, wall_names,
+                                           distance_type, block_names, worksets,
+                                           ir, wall_distances, verbose);
+    space.fence();
+
+    auto wall_distances_host = Kokkos::create_mirror_view_and_copy(
+        Kokkos::HostSpace{}, wall_distances);
+
+    // Store wall distances in the output file
+    if (distance_type == "node")
+    {
+      auto meta = mesh->getMetaData();
+      auto bulk = mesh->getBulkData();
+
+      // The field is the same on all blocks. But the panzer's interface only
+      // allows to specify one block.
+      auto *field = mesh->getSolutionField(distance_field_name, block_names[0]);
+
+      stk::mesh::Selector selector;
+      for (auto const &block_name : block_names)
+        selector |= *meta->get_part(block_name);
+      selector &= meta->locally_owned_part() | meta->globally_shared_part();
+
+      stk::mesh::EntityVector nodes;
+      stk::mesh::get_selected_entities(
+          selector, mesh->getBulkData()->buckets(stk::topology::NODE_RANK),
+          nodes);
+      int const num_nodes = nodes.size();
+
+      for (int i = 0; i < num_nodes; ++i)
+      {
+        auto const &node = nodes[i];
+        auto *field_data = stk::mesh::field_data(*field, node);
+        KOKKOS_ASSERT(field_data != nullptr); // sanity check
+        *field_data = wall_distances_host(i);
+      }
+    }
+    else
+    {
+      for (auto const &block_name : block_names)
+      {
+        // The field is the same on all blocks. But the panzer's interface
+        // only allows to specify one block.
+        auto *field = mesh->getCellField(distance_field_name, block_name);
+
+        std::vector<stk::mesh::Entity> elements;
+        mesh->getMyElements(block_name, elements);
+        for (std::size_t el = 0; el < elements.size(); el++)
+        {
+          std::size_t localId = mesh->elementLocalId(elements[el]);
+          double *field_data = stk::mesh::field_data(*field, elements[el]);
+          KOKKOS_ASSERT(field_data != nullptr); // sanity check
+          field_data[0] = wall_distances_host(localId);
+        }
+      }
+    }
+
+    mesh->writeToExodus(out_filename);
+  }
+
+  Kokkos::finalize();
+  MPI_Finalize();
+
+  return 0;
+}
---
+++ src/adapters/ArborX_WallDistance.hpp
@@ -0,0 +1,290 @@
+/****************************************************************************
+ * Copyright (c) 2025, ArborX authors                                       *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ArborX library. ArborX is                       *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef ARBORX_WALL_DISTANCE_HPP
+#define ARBORX_WALL_DISTANCE_HPP
+
+#include <ArborX_Config.hpp>
+
+#ifdef ARBORX_ENABLE_TRILINOS
+
+#include "ArborX_WallDistanceHelpers.hpp"
+#include <ArborX_DistributedTree.hpp>
+#include <ArborX_LinearBVH.hpp>
+#include <ArborX_Segment.hpp>
+
+#include <Kokkos_DynRankView.hpp>
+#include <Kokkos_Profiling_ScopedRegion.hpp>
+
+#include <string>
+
+#include <Panzer_IntegrationRule.hpp>
+#include <Panzer_STK_Interface.hpp>
+#include <Panzer_Workset.hpp>
+#include <Panzer_Workset_Utilities.hpp> // getIntegrationRuleIndex
+#include <mpi.h>
+
+namespace ArborX::Experimental
+{
+
+template <typename MemorySpace, int DIM, typename Coordinate = double,
+          bool ReplicateSides = true>
+class WallDistance
+{
+  static_assert(DIM == 2 || DIM == 3, "ArborX::Experimental::WallDistance is "
+                                      "only implemented for 2D and 3D meshes");
+
+  using Geometry = std::conditional_t<DIM == 2, Segment<DIM, Coordinate>,
+                                      Triangle<DIM, Coordinate>>;
+  using Index =
+      std::conditional_t<ReplicateSides,
+                         BoundingVolumeHierarchy<MemorySpace, Geometry>,
+                         DistributedTree<MemorySpace, Geometry>>;
+
+  Index _index;
+
+public:
+  KOKKOS_DEFAULTED_FUNCTION
+  WallDistance() = default;
+
+  template <typename ExecutionSpace>
+  WallDistance(ExecutionSpace const &space,
+               panzer_stk::STK_Interface const &mesh,
+               std::vector<std::string> const &wall_names);
+
+  // Distances from mesh nodes
+  template <typename ExecutionSpace, typename Distances>
+  void distance(ExecutionSpace const &space,
+                panzer_stk::STK_Interface const &mesh, Distances &distances);
+
+  template <typename ExecutionSpace, typename Distances>
+  void distance(ExecutionSpace const &space,
+                panzer_stk::STK_Interface const &mesh,
+                stk::mesh::Selector const &selector, Distances &distances);
+
+  // Distances from integration points
+  template <typename ExecutionSpace, typename WorksetDistances>
+  void distance(ExecutionSpace const &space,
+                std::vector<panzer::Workset> const &worksets,
+                panzer::IntegrationRule const &ir,
+                WorksetDistances &workset_distances);
+
+  // Distances from any set of ArborX points
+  template <typename ExecutionSpace, typename Points, typename Distances>
+  void distance(ExecutionSpace const &space, Points const &points,
+                Distances &distances);
+};
+
+template <typename MemorySpace, int DIM, typename Coordinate,
+          bool ReplicateSides>
+template <typename ExecutionSpace>
+WallDistance<MemorySpace, DIM, Coordinate, ReplicateSides>::WallDistance(
+    ExecutionSpace const &space, panzer_stk::STK_Interface const &mesh,
+    std::vector<std::string> const &wall_names)
+{
+  std::string prefix = "ArborX::WallDistance::WallDistance";
+  Kokkos::Profiling::ScopedRegion guard(prefix);
+  prefix += "::";
+
+  Kokkos::DynRankView<Coordinate, MemorySpace> local_sides;
+  Details::getLocalSides(mesh, wall_names, local_sides);
+
+  MPI_Comm comm = Teuchos::getRawMpiComm(*mesh.getComm());
+  if constexpr (ReplicateSides)
+  {
+    Kokkos::View<Coordinate ***, MemorySpace> global_sides(
+        prefix + "global_sides", 0, 0, 0);
+    Details::gatherGlobalSides(comm, space, local_sides, global_sides);
+
+    _index = BoundingVolumeHierarchy(
+        space, Details::Geometries<DIM, decltype(global_sides)>{global_sides});
+  }
+  else
+  {
+    _index = DistributedTree(
+        comm, space,
+        Details::Geometries<DIM, decltype(local_sides)>{local_sides});
+  }
+}
+
+template <typename MemorySpace, int DIM, typename Coordinate,
+          bool ReplicateSides>
+template <typename ExecutionSpace, typename Points, typename Distances>
+void WallDistance<MemorySpace, DIM, Coordinate, ReplicateSides>::distance(
+    ExecutionSpace const &space, Points const &points, Distances &distances)
+{
+  std::string prefix = "ArborX::WallDistance::distance [points]";
+  Kokkos::Profiling::ScopedRegion guard(prefix);
+  prefix += "::";
+
+  auto queries = ArborX::Experimental::make_nearest(points, 1);
+
+  Kokkos::View<int *, MemorySpace> offset(prefix + "offset", 0);
+  if constexpr (ReplicateSides)
+    _index.query(space, queries, Details::WallDistanceCallback{}, distances,
+                 offset);
+  else
+    _index.query(space, queries,
+                 declare_callback_constrained(Details::WallDistanceCallback{}),
+                 distances, offset);
+}
+
+template <typename MemorySpace, int DIM, typename Coordinate,
+          bool ReplicateSides>
+template <typename ExecutionSpace, typename Distances>
+void WallDistance<MemorySpace, DIM, Coordinate, ReplicateSides>::distance(
+    ExecutionSpace const &space, panzer_stk::STK_Interface const &mesh,
+    stk::mesh::Selector const &selector, Distances &distances)
+{
+  std::string prefix = "ArborX::WallDistance::distance [nodes]";
+  Kokkos::Profiling::ScopedRegion guard(prefix);
+  prefix += "::";
+
+  Kokkos::Profiling::pushRegion(prefix + "get_stk_points");
+  auto bulk = mesh.getBulkData();
+
+  std::vector<stk::mesh::Entity> nodes;
+  stk::mesh::get_selected_entities(
+      selector, bulk->buckets(stk::topology::NODE_RANK), nodes);
+
+  int const num_nodes = nodes.size();
+
+  Kokkos::View<ArborX::Point<DIM, Coordinate> *, MemorySpace> points(
+      Kokkos::view_alloc(space, Kokkos::WithoutInitializing, prefix + "points"),
+      num_nodes);
+  auto points_host = Kokkos::create_mirror_view(points);
+
+  auto const &coords = mesh.getCoordinatesField();
+  for (int i = 0; i < num_nodes; ++i)
+  {
+    auto const &node = nodes[i];
+    auto const *x = stk::mesh::field_data(coords, node);
+    if constexpr (DIM == 2)
+      points_host(i) = {x[0], x[1]};
+    else
+      points_host(i) = {x[0], x[1], x[2]};
+  }
+  Kokkos::deep_copy(space, points, points_host);
+
+  Kokkos::Profiling::popRegion();
+
+  Kokkos::resize(Kokkos::view_alloc(space, Kokkos::WithoutInitializing),
+                 distances, num_nodes);
+  distance(space, points, distances);
+}
+
+template <typename MemorySpace, int DIM, typename Coordinate,
+          bool ReplicateSides>
+template <typename ExecutionSpace, typename Distances>
+void WallDistance<MemorySpace, DIM, Coordinate, ReplicateSides>::distance(
+    ExecutionSpace const &space, panzer_stk::STK_Interface const &mesh,
+    Distances &distances)
+{
+  std::string prefix = "ArborX::WallDistance::distance [all nodes]";
+
+  auto meta = mesh.getMetaData();
+  auto selector = meta->locally_owned_part() | meta->globally_shared_part();
+  distance(space, mesh, selector, distances);
+}
+
+template <typename MemorySpace, int DIM, typename Coordinate,
+          bool ReplicateSides>
+template <typename ExecutionSpace, typename WorksetDistances>
+void WallDistance<MemorySpace, DIM, Coordinate, ReplicateSides>::distance(
+    ExecutionSpace const &space, std::vector<panzer::Workset> const &worksets,
+    panzer::IntegrationRule const &ir, WorksetDistances &workset_distances)
+{
+  std::string prefix = "ArborX::WallDistance::distance [integration points]";
+  Kokkos::Profiling::ScopedRegion guard(prefix);
+  prefix += "::";
+
+  // FIXME: do we require that workset_distances is already allocated with the
+  // right size?
+  int const num_worksets = worksets.size();
+  int const num_int_points_per_cell = workset_distances.extent(2);
+
+  KOKKOS_ASSERT((int)workset_distances.extent(0) == num_worksets);
+  KOKKOS_ASSERT((int)workset_distances.extent(2) == num_int_points_per_cell);
+
+  auto ir_index = panzer::getIntegrationRuleIndex(ir.order(), worksets[0]);
+
+  int num_queries = 0;
+  std::vector<int> workset_sizes(num_worksets);
+  for (int workset_id = 0; workset_id < num_worksets; ++workset_id)
+  {
+    auto const &workset = worksets[workset_id];
+    auto const num_cells = workset.num_cells;
+    workset_sizes[workset_id] = num_cells;
+    num_queries += num_cells * num_int_points_per_cell;
+  }
+
+  using Point = ArborX::Point<DIM, Coordinate>;
+  Kokkos::View<Point *, MemorySpace> points(
+      Kokkos::view_alloc(space, Kokkos::WithoutInitializing, prefix + "points"),
+      num_queries);
+  for (int workset_id = 0, queries_offset = 0; workset_id < num_worksets;
+       ++workset_id)
+  {
+    auto const &workset = worksets[workset_id];
+    auto const num_cells = workset.num_cells;
+
+    if (num_cells == 0)
+      continue;
+
+    auto const &ip_coords = workset.int_rules[ir_index]->ip_coordinates;
+
+    Kokkos::parallel_for(
+        prefix + "create_queries", Kokkos::RangePolicy(space, 0, num_cells),
+        KOKKOS_LAMBDA(int cell) {
+          auto offset = queries_offset + cell * num_int_points_per_cell;
+          Point p;
+          for (int int_point = 0; int_point < num_int_points_per_cell;
+               ++int_point)
+          {
+            for (int d = 0; d < DIM; ++d)
+              p[d] = ip_coords(cell, int_point, d);
+            points(offset++) = p;
+          }
+        });
+    queries_offset += num_cells * num_int_points_per_cell;
+  }
+
+  Kokkos::View<Coordinate *, MemorySpace> distances(
+      Kokkos::view_alloc(space, Kokkos::WithoutInitializing,
+                         prefix + "distances"),
+      0);
+  distance(space, points, distances);
+
+  for (int workset_id = 0, workset_offset = 0; workset_id < num_worksets;
+       ++workset_id)
+  {
+    auto const num_cells = workset_sizes[workset_id];
+    if (num_cells == 0)
+      continue;
+
+    Kokkos::parallel_for(
+        "ArborX::WallDistance::reshape_distances",
+        Kokkos::RangePolicy(space, 0, num_cells), KOKKOS_LAMBDA(int cell) {
+          auto offset = workset_offset + cell * num_int_points_per_cell;
+          for (int int_point = 0; int_point < num_int_points_per_cell;
+               ++int_point)
+            workset_distances(workset_id, cell, int_point) =
+                distances(offset++);
+        });
+    workset_offset += num_cells * num_int_points_per_cell;
+  }
+}
+
+} // namespace ArborX::Experimental
+
+#endif
+
+#endif
