275b792b8b3a80977a3462f86aee25fa39c3a49a
Andrey Prokopenko <prokopenkoav@ornl.gov>
2026-06-07 10:00:36 -0400

    Revert to using old AccessTraits in user-facing code

--- src/spatial/ArborX_LinearBVH.hpp
+++ src/spatial/ArborX_LinearBVH.hpp
@@ -65,7 +65,7 @@ public:
 
   BoundingVolumeHierarchy() = default; // build an empty tree
 
-  template <typename ExecutionSpace, Details::Concepts::Primitives Values,
+  template <typename ExecutionSpace, typename Values,
             typename SpaceFillingCurve = Experimental::Morton64>
   BoundingVolumeHierarchy(
       ExecutionSpace const &space, Values const &values,
@@ -81,15 +81,13 @@ public:
   KOKKOS_FUNCTION
   bounding_volume_type bounds() const noexcept { return _bounds; }
 
-  template <typename ExecutionSpace, Details::Concepts::Predicates Predicates,
-            typename Callback>
+  template <typename ExecutionSpace, typename Predicates, typename Callback>
   void query(ExecutionSpace const &space, Predicates const &predicates,
              Callback const &callback,
              Experimental::TraversalPolicy const &policy =
                  Experimental::TraversalPolicy()) const;
 
-  template <typename ExecutionSpace,
-            Details::Concepts::Predicates UserPredicates,
+  template <typename ExecutionSpace, typename UserPredicates,
             typename CallbackOrView, typename View, typename... Args>
     requires(Kokkos::is_view_v<std::decay_t<View>>)
   void query(ExecutionSpace const &space, UserPredicates const &user_predicates,
@@ -143,14 +141,13 @@ private:
   IndexableGetter _indexable_getter;
 };
 
-template <typename ExecutionSpace, Details::Concepts::Primitives Values>
+template <typename ExecutionSpace, typename Values>
 KOKKOS_DEDUCTION_GUIDE BoundingVolumeHierarchy(ExecutionSpace, Values)
     -> BoundingVolumeHierarchy<
         typename Details::AccessValues<Values>::memory_space,
         typename Details::AccessValues<Values>::value_type>;
 
-template <typename ExecutionSpace, Details::Concepts::Primitives Values,
-          typename IndexableGetter>
+template <typename ExecutionSpace, typename Values, typename IndexableGetter>
 KOKKOS_DEDUCTION_GUIDE BoundingVolumeHierarchy(ExecutionSpace, Values,
                                                IndexableGetter)
     -> BoundingVolumeHierarchy<
@@ -169,7 +166,7 @@ using BVH = BoundingVolumeHierarchy<MemorySpace, Value, IndexableGetter,
 
 template <typename MemorySpace, typename Value, typename IndexableGetter,
           typename BoundingVolume>
-template <typename ExecutionSpace, Details::Concepts::Primitives UserValues,
+template <typename ExecutionSpace, typename UserValues,
           typename SpaceFillingCurve>
 BoundingVolumeHierarchy<MemorySpace, Value, IndexableGetter, BoundingVolume>::
     BoundingVolumeHierarchy(ExecutionSpace const &space,
@@ -187,6 +184,8 @@ BoundingVolumeHierarchy<MemorySpace, Value, IndexableGetter, BoundingVolume>::
 {
   static_assert(Details::KokkosExt::is_accessible_from<MemorySpace,
                                                        ExecutionSpace>::value);
+  Details::check_valid_access_traits(user_values);
+
   using Values = Details::AccessValues<UserValues>;
   Values values{user_values}; // NOLINT
 
@@ -258,8 +257,7 @@ BoundingVolumeHierarchy<MemorySpace, Value, IndexableGetter, BoundingVolume>::
 
 template <typename MemorySpace, typename Value, typename IndexableGetter,
           typename BoundingVolume>
-template <typename ExecutionSpace, Details::Concepts::Predicates UserPredicates,
-          typename Callback>
+template <typename ExecutionSpace, typename UserPredicates, typename Callback>
 void BoundingVolumeHierarchy<
     MemorySpace, Value, IndexableGetter,
     BoundingVolume>::query(ExecutionSpace const &space,
@@ -269,6 +267,8 @@ void BoundingVolumeHierarchy<
 {
   static_assert(Details::KokkosExt::is_accessible_from<MemorySpace,
                                                        ExecutionSpace>::value);
+  Details::check_valid_access_traits(user_predicates,
+                                     Details::CheckReturnTypeTag{});
   Details::check_valid_callback<value_type>(callback, user_predicates);
 
   using Predicates = Details::AccessValues<UserPredicates>;
